8525693850df6bf8a6226a49141d8609d5118f34
razykov <v.razykov@gmail.com>
2014-09-05 18:20:24 +0400

    Implement to_24_hour_clock

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -8,6 +8,7 @@ using namespace std;
 double seconds_difference(double time_1, double time_2);
 double hours_difference(double time_1, double time_2);
 double to_float_hours(int hours, int minutes, int seconds);
+double to_24_hour_clock(double hours);
 
 int main(){
   
@@ -25,7 +26,7 @@ int main(){
   assert(fabs(to_float_hours(0, 15, 0) - 0.25) <  DBL_EPSILON);
   assert(fabs(to_float_hours(2, 45, 9) - 2.7525) <  DBL_EPSILON);
   assert(fabs(to_float_hours(1, 0, 36) - 1.01) <  DBL_EPSILON);
-  /*
+  
   assert(fabs(to_24_hour_clock(24) - 0) <  DBL_EPSILON);
   assert(fabs(to_24_hour_clock(48) - 0) <  DBL_EPSILON);
   assert(fabs(to_24_hour_clock(25) - 1) <  DBL_EPSILON);
@@ -74,33 +75,11 @@ double to_float_hours(int hours, int minutes, int seconds){
   return (3600 * hours + 60 * minutes + seconds) / 3600.0;
 }
 
-double to_24_hour_clock(double hours)
-{
-    /*
-        hours is a number of hours since midnight. Return the
-        hour as seen on a 24-hour clock.
-
-        Precondition: hours >= 0
-
-        >>> to_24_hour_clock(24)
-        0
-        
-        >>> to_24_hour_clock(48)
-        0
-        
-        >>> to_24_hour_clock(25)
-        1
-        
-        >>> to_24_hour_clock(4)
-        4
-        
-        >>> to_24_hour_clock(28.5)
-        4.5
-        
-        You may wish to inspect various function in <cmath> to work
-        with integer and fractional part of a hours separately.
-        
-    */
+double to_24_hour_clock(double hours){
+  
+  if(hours < 0) {cerr << "incorect hours"; return -1;}
+  
+  return fmod(hours, 24.0);
 }
 
 /*
