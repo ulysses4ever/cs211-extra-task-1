b7a9861efa039eb45a841cb86b43e5da000a7ac5
razykov <v.razykov@gmail.com>
2014-09-05 18:31:53 +0400

    Implement get_hours, get_minutes, get_seconds

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -9,6 +9,10 @@ double seconds_difference(double time_1, double time_2);
 double hours_difference(double time_1, double time_2);
 double to_float_hours(int hours, int minutes, int seconds);
 double to_24_hour_clock(double hours);
+unsigned get_hours(unsigned seconds);
+unsigned get_minutes(unsigned seconds);
+unsigned get_seconds(unsigned seconds);
+
 
 int main(){
   
@@ -32,7 +36,7 @@ int main(){
   assert(fabs(to_24_hour_clock(25) - 1) <  DBL_EPSILON);
   assert(fabs(to_24_hour_clock(4) - 4) <  DBL_EPSILON);
   assert(fabs(to_24_hour_clock(28.5) - 4.5) <  DBL_EPSILON);
-  /*
+  
   assert(fabs(get_hours(3800) - 1) <  DBL_EPSILON);
   assert(fabs(get_minutes(3800) - 3) <  DBL_EPSILON);
   assert(fabs(get_seconds(3800) - 20) <  DBL_EPSILON);
@@ -82,26 +86,20 @@ double to_24_hour_clock(double hours){
   return fmod(hours, 24.0);
 }
 
-/*
-    Implement three functions
-        * get_hours
-        * get_minutes
-        * get_seconds
-    They are used to determine the hours part, minutes part and seconds part 
-    of a time in seconds. E.g.:
+unsigned get_hours(unsigned seconds){
+  
+  return seconds/3600.0;  
+}
 
-    >>> get_hours(3800)
-    1
+unsigned get_minutes(unsigned seconds){
+  
+  return (seconds%3600)/60;  
+}
 
-    >>> get_minutes(3800)
-    3
-
-    >>> get_seconds(3800)
-    20
-
-    In other words, if 3800 seconds have elapsed since midnight, 
-    it is currently 01:03:20 (hh:mm:ss).
-*/
+unsigned get_seconds(unsigned seconds){
+  
+  return (seconds%3600)%60;  
+}
 
 double time_to_utc(int utc_offset, double time)
 {
