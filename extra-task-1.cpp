5935099e787cf828ca89f3d9d795a3595f32e451
Rasie1 <rasielll@gmail.com>
2014-09-06 00:54:45 +0400

    time_to_utc

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -139,6 +139,8 @@ int get_seconds(double time_in_seconds)
 
 double time_to_utc(int utc_offset, double time)
 {
+    return fmod(24 + time - utc_offset, 24.0);
+    
     /*
         Return time at UTC+0, where utc_offset is the number of hours away from
         UTC+0.
@@ -245,6 +247,14 @@ int main()
     assert(get_seconds(3600.0) == 3600);
     assert(get_seconds(60.0) == 60);
     assert(get_seconds(8888.0) == 8888);
+
+    //time_to_utc
+    assert_compare(time_to_utc(+0, 12.0), 12.0);
+    assert_compare(time_to_utc(+1, 12.0), 11.0);
+    assert_compare(time_to_utc(-1, 12.0), 13.0);
+    assert_compare(time_to_utc(-11, 18.0), 5.0);
+    assert_compare(time_to_utc(-1, 0.0), 1.0);
+    assert_compare(time_to_utc(-1, 23.0), 0.0);
 
 
     return 0;
