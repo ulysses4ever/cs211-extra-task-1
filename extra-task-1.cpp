c49d8e1ffed3f371e94038bfb840eba2af55117e
razykov <v.razykov@gmail.com>
2014-09-05 23:38:32 +0400

    Added comments

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -18,31 +18,40 @@ double time_from_utc(int utc_offset, double time);
 
 int main(){
   
-  
+  // seconds_difference
   assert(fabs(seconds_difference(1800.0, 3600.0) - 1800.0) <  DBL_EPSILON);
   assert(fabs(seconds_difference(3600.0, 1800.0) - -1800.0) <  DBL_EPSILON);
   assert(fabs(seconds_difference(1800.0, 2160.0) - 360.0) <  DBL_EPSILON);
   assert(fabs(seconds_difference(1800.0, 1800.0) - 0.0) <  DBL_EPSILON);
   
+  // hours_difference
   assert(fabs(hours_difference(1800.0, 3600.0) - 0.5) <  DBL_EPSILON);
   assert(fabs(hours_difference(3600.0, 1800.0) - -0.5) <  DBL_EPSILON);
   assert(fabs(hours_difference(1800.0, 2160.0) - 0.1) <  DBL_EPSILON);
   assert(fabs(hours_difference(1800.0, 1800.0) - 0.0) <  DBL_EPSILON);
   
+  // to_float_hours
   assert(fabs(to_float_hours(0, 15, 0) - 0.25) <  DBL_EPSILON);
   assert(fabs(to_float_hours(2, 45, 9) - 2.7525) <  DBL_EPSILON);
   assert(fabs(to_float_hours(1, 0, 36) - 1.01) <  DBL_EPSILON);
   
+  // to_24_hour_clock
   assert(fabs(to_24_hour_clock(24) - 0) <  DBL_EPSILON);
   assert(fabs(to_24_hour_clock(48) - 0) <  DBL_EPSILON);
   assert(fabs(to_24_hour_clock(25) - 1) <  DBL_EPSILON);
   assert(fabs(to_24_hour_clock(4) - 4) <  DBL_EPSILON);
   assert(fabs(to_24_hour_clock(28.5) - 4.5) <  DBL_EPSILON);
   
+  // get_hours
   assert(fabs(get_hours(3800) - 1) <  DBL_EPSILON);
+  
+  // get_minutes
   assert(fabs(get_minutes(3800) - 3) <  DBL_EPSILON);
+  
+  // get_seconds
   assert(fabs(get_seconds(3800) - 20) <  DBL_EPSILON);
-          
+  
+  // time_to_utc        
   assert(fabs(time_to_utc(+0, 12.0) - 12.0) <  DBL_EPSILON);       
   assert(fabs(time_to_utc(+1, 12.0) - 11.0) <  DBL_EPSILON);
   assert(fabs(time_to_utc(-1, 12.0) - 13.0) <  DBL_EPSILON);
@@ -50,6 +59,7 @@ int main(){
   assert(fabs(time_to_utc(-1, 0.0) - 1.0) <  DBL_EPSILON);
   assert(fabs(time_to_utc(-1, 23.0) - 0.0) <  DBL_EPSILON);
   
+  // time_from_utc
   assert(fabs(time_from_utc(+0, 12.0) - 12.0) <  DBL_EPSILON);
   assert(fabs(time_from_utc(+1, 12.0) - 13.0) <  DBL_EPSILON);
   assert(fabs(time_from_utc(-1, 12.0) - 11.0) <  DBL_EPSILON);
@@ -62,6 +72,7 @@ int main(){
   
   return 0;        
 }
+
 
 double seconds_difference(double time_1, double time_2){
   
