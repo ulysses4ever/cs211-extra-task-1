d6005f30086d916c7a5f2cd93c8efefc02af0c8c
Xamgore <xamgore@ya.ru>
2014-09-05 18:53:28 +0400

    hours_difference() function bug fix

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -13,12 +13,12 @@ double seconds_difference(double time_1, double time_2)
 }
 
 /**
- * Return the number of hours later that a time 
- * in seconds time_2 is than a time in seconds time_1.
+	Return the number of hours later that a time 
+	in seconds time_2 is than a time in seconds time_1.
  */
 double hours_difference(double time_1, double time_2)
 {
-	return (time_2 - time_1) / 2;
+	return (time_2 - time_1) / 3600;
 }
 
 /**
