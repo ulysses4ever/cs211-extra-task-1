701458f9f28cc84315db069411df5f8358abd960
razykov <v.razykov@gmail.com>
2014-09-05 19:01:22 +0400

    Implement time_from_utc

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -13,6 +13,7 @@ unsigned get_hours(unsigned seconds);
 unsigned get_minutes(unsigned seconds);
 unsigned get_seconds(unsigned seconds);
 double time_to_utc(int utc_offset, double time);
+double time_from_utc(int utc_offset, double time);
 
 
 int main(){
@@ -48,7 +49,7 @@ int main(){
   assert(fabs(time_to_utc(-11, 18.0) - 5.0) <  DBL_EPSILON);
   assert(fabs(time_to_utc(-1, 0.0) - 1.0) <  DBL_EPSILON);
   assert(fabs(time_to_utc(-1, 23.0) - 0.0) <  DBL_EPSILON);
-  /*
+  
   assert(fabs(time_from_utc(+0, 12.0) - 12.0) <  DBL_EPSILON);
   assert(fabs(time_from_utc(+1, 12.0) - 13.0) <  DBL_EPSILON);
   assert(fabs(time_from_utc(-1, 12.0) - 11.0) <  DBL_EPSILON);
@@ -57,7 +58,7 @@ int main(){
   assert(fabs(time_from_utc(-1, 0.0) - 23.0) <  DBL_EPSILON);
   assert(fabs(time_from_utc(-1, 23.0) - 22.0) <  DBL_EPSILON);
   assert(fabs(time_from_utc(+1, 23.0) - 0.0) <  DBL_EPSILON);
-  */
+  
   
   return 0;        
 }
@@ -102,41 +103,20 @@ unsigned get_seconds(unsigned seconds){
   return (seconds%3600)%60;  
 }
 
-double time_to_utc(int utc_offset, double time)
-{
+double time_to_utc(int utc_offset, double time){
+  
   if(utc_offset < -13 || utc_offset > 13) {cerr << "incorect utc_offset"; return -1;}
   if(time < 0.0 || time >= 24.0) {cerr << "incorect time"; return -1;}
   
   return fmod(time - utc_offset, 24.0);
 }
 
-double time_from_utc(int utc_offset, double time)
-{
-    /*
-        Return UTC time in time zone utc_offset.
-
-        >>> time_from_utc(+0, 12.0)
-        12.0
- 
-        >>> time_from_utc(+1, 12.0)
-        13.0
- 
-        >>> time_from_utc(-1, 12.0)
-        11.0
- 
-        >>> time_from_utc(+6, 6.0)
-        12.0
- 
-        >>> time_from_utc(-7, 6.0)
-        23.0
- 
-        >>> time_from_utc(-1, 0.0)
-        23.0
- 
-        >>> time_from_utc(-1, 23.0)
-        22.0
- 
-        >>> time_from_utc(+1, 23.0)
-        0.0
-    */
+double time_from_utc(int utc_offset, double time){
+  
+  if(utc_offset < -13 || utc_offset > 13) {cerr << "incorect utc_offset"; return -1;}
+  if(time < 0.0 || time >= 24.0) {cerr << "incorect time"; return -1;}
+  
+  double reg = fmod(time + utc_offset, 24.0);
+  
+  return (reg < 0.0)? reg + 24.0 : reg;
 }
