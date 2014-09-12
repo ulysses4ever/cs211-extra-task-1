56eafa5d2ba7c7f7e02ad075074d934aa3d838ed
lunawyll <lunawyll@mail.ru>
2014-09-12 17:25:01 +0400

    implemented time_to_utc

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -41,30 +41,9 @@ Precondition: hours >= 0
 */
 double to_24_hour_clock(double hours)
 {
-	int temp = trunc(hours);
-	return temp % 24 + hours - temp;
+	return fmod(hours, 24.0);
 }
 
-/*
-    Implement three functions
-        * get_hours
-        * get_minutes
-        * get_seconds
-    They are used to determine the hours part, minutes part and seconds part 
-    of a time in seconds. E.g.:
-
-    >>> get_hours(3800)
-    1
-
-    >>> get_minutes(3800)
-    3
-
-    >>> get_seconds(3800)
-    20
-
-    In other words, if 3800 seconds have elapsed since midnight, 
-    it is currently 01:03:20 (hh:mm:ss).
-*/
 int get_hours(int seconds){
 	return seconds / 3600;
 }
@@ -77,65 +56,47 @@ int get_seconds(int seconds){
 	return seconds % 60;
 }
 
+/*
+Return time at UTC+0, where utc_offset is the number of hours away from
+UTC+0.
+*/
 double time_to_utc(int utc_offset, double time)
 {
-	return 0;
-	/*
-        Return time at UTC+0, where utc_offset is the number of hours away from
-        UTC+0.
-        You may be interested in:
-        https://en.wikipedia.org/wiki/Coordinated_Universal_Time
-
-        >>> time_to_utc(+0, 12.0)
-        12.0
- 
-        >>> time_to_utc(+1, 12.0)
-        11.0
- 
-        >>> time_to_utc(-1, 12.0)
-        13.0
- 
-        >>> time_to_utc(-11, 18.0)
-        5.0
- 
-        >>> time_to_utc(-1, 0.0)
-        1.0
- 
-        >>> time_to_utc(-1, 23.0)
-        0.0
-    */
+	return fmod(time - utc_offset, 24.0);
+	
 }
 
+/*
+Return UTC time in time zone utc_offset.
+
+>>> time_from_utc(+0, 12.0)
+12.0
+
+>>> time_from_utc(+1, 12.0)
+13.0
+
+>>> time_from_utc(-1, 12.0)
+11.0
+
+>>> time_from_utc(+6, 6.0)
+12.0
+
+>>> time_from_utc(-7, 6.0)
+23.0
+
+>>> time_from_utc(-1, 0.0)
+23.0
+
+>>> time_from_utc(-1, 23.0)
+22.0
+
+>>> time_from_utc(+1, 23.0)
+0.0
+*/
 double time_from_utc(int utc_offset, double time)
 {
 	return 0;
-	/*
-        Return UTC time in time zone utc_offset.
-
-        >>> time_from_utc(+0, 12.0)
-        12.0
- 
-        >>> time_from_utc(+1, 12.0)
-        13.0
- 
-        >>> time_from_utc(-1, 12.0)
-        11.0
- 
-        >>> time_from_utc(+6, 6.0)
-        12.0
- 
-        >>> time_from_utc(-7, 6.0)
-        23.0
- 
-        >>> time_from_utc(-1, 0.0)
-        23.0
- 
-        >>> time_from_utc(-1, 23.0)
-        22.0
- 
-        >>> time_from_utc(+1, 23.0)
-        0.0
-    */
+	
 }
 
 int main(){
@@ -173,31 +134,28 @@ int main(){
 	assert(abs(to_24_hour_clock(4) - 4) < DBL_EPSILON);
 	assert(abs(to_24_hour_clock(28.5) - 4.5) < DBL_EPSILON);
 	cout << "Testing is complete\n";
-	/*
-	Implement three functions
-	* get_hours
-	* get_minutes
-	* get_seconds
-	They are used to determine the hours part, minutes part and seconds part
-	of a time in seconds. E.g.:
+	
+	cout << endl;
 
-	>>> get_hours(3800)
-	1
-
-	>>> get_minutes(3800)
-	3
-
-	>>> get_seconds(3800)
-	20
-
-	In other words, if 3800 seconds have elapsed since midnight,
-	it is currently 01:03:20 (hh:mm:ss).
-	*/
 	cout << "Start testing get_hors, get_minutes, get_seconds...\n";
 	assert(get_hours(3800) == 1);
 	assert(get_minutes(3800) == 3);
 	assert(get_seconds(3800) == 20);
 	cout << "Testing is complete\n";
+
+	cout << endl;
+
+	cout << "Start testing time_to_utc...\n";
+	assert(abs(time_to_utc(+0, 12.0) - 12.0) < DBL_EPSILON);
+	assert(abs(time_to_utc(+1, 12.0) - 11.0) < DBL_EPSILON);
+	assert(abs(time_to_utc(-1, 12.0) - 13.0) < DBL_EPSILON);
+	assert(abs(time_to_utc(-11, 18.0) - 5.0) < DBL_EPSILON);
+	assert(abs(time_to_utc(-1, 0.0) - 1.0) < DBL_EPSILON);
+	assert(abs(time_to_utc(-1, 23.0) - 0.0) < DBL_EPSILON);
+	cout << "Testing is complete\n";
+
+	cout << endl;
+	
 
 	return 0;
 }
