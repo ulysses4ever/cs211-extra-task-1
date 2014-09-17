a596dd1bf066ca05c6cd2f26af489b27cf4b93b7
Elizaveta21 <elizaveta_pshenichnaya21@mail.ru>
2014-09-18 00:00:03 +0400

    Update extra-task-1.cpp

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -1,3 +1,7 @@
+#include <iostream>
+#include <cmath>
+#include <locale>
+using namespace std;
 double seconds_difference(double time_1, double time_2)
 {
     // your implementation goes here...
@@ -18,6 +22,7 @@ double seconds_difference(double time_1, double time_2)
         >>> seconds_difference(1800.0, 1800.0)
         0.0
     */
+    return time_2 - time_1;
 }
 
 double hours_difference(double time_1, double time_2)
@@ -38,6 +43,8 @@ double hours_difference(double time_1, double time_2)
         >>> hours_difference(1800.0, 1800.0)
         0.0
     */
+    return (time_2 - time_1) / 3600.0;
+    
 }
 
 double to_float_hours(int hours, int minutes, int seconds)
@@ -57,6 +64,9 @@ double to_float_hours(int hours, int minutes, int seconds)
         >>> to_float_hours(1, 0, 36)
         1.01
     */
+    assert((0 <= minutes) && (minutes < 60));
+    assert((0 <= seconds) && (seconds < 60));
+    return hours + minutes / 60.0 + seconds / 3600.0;
 }
 
 double to_24_hour_clock(double hours)
@@ -86,6 +96,9 @@ double to_24_hour_clock(double hours)
         with integer and fractional part of a hours separately.
         
     */
+    assert(hours >= 0);
+	return fmod(hours, 24);
+    
 }
 
 /*
@@ -108,6 +121,21 @@ double to_24_hour_clock(double hours)
     In other words, if 3800 seconds have elapsed since midnight, 
     it is currently 01:03:20 (hh:mm:ss).
 */
+int get_hours(int seconds)
+{
+    return seconds / 3600 % 24;
+}
+
+
+int get_minutes(int seconds)
+{
+    return seconds % 3600 / 60;
+}
+
+int get_seconds(int seconds)
+{
+    return seconds % 3600 % 60;
+}
 
 double time_to_utc(int utc_offset, double time)
 {
@@ -135,6 +163,10 @@ double time_to_utc(int utc_offset, double time)
         >>> time_to_utc(-1, 23.0)
         0.0
     */
+    double d;
+	d = (time - utc_offset);
+	if (d >= 24)  d = d - 24;
+	return d;
 }
 
 double time_from_utc(int utc_offset, double time)
@@ -166,4 +198,72 @@ double time_from_utc(int utc_offset, double time)
         >>> time_from_utc(+1, 23.0)
         0.0
     */
+    double d;
+	d = (time + utc_offset);
+	if (d < 0)  d = d + 24;
+	if (d == 24) d = d - 24;
+	return d;
+}
+
+int main()
+{
+    std::locale::global(std::locale(""));
+    
+    assert(fabs(seconds_difference(1800.0, 3600.0) - 1800.0) < DBL_EPSILON);
+    assert(fabs(seconds_difference(3600.0, 1800.0) - (-1800.0)) < DBL_EPSILON);
+    assert(fabs(seconds_difference(1800.0, 2160.0) - 360.0) < DBL_EPSILON);
+    assert(fabs(seconds_difference(1800.0, 1800.0) - 0.0) < DBL_EPSILON);
+    cout << "Tests 1" << endl;
+    
+    assert(fabs(hours_difference(1800.0, 3600.0) - 0.5) < DBL_EPSILON);
+    assert(fabs(hours_difference(3600.0, 1800.0) - (- 0.5)) < DBL_EPSILON);
+    assert(fabs(hours_difference(1800.0, 2160.0) - 0.1) < DBL_EPSILON);
+    assert(fabs(hours_difference(1800.0, 1800.0) - 0.0) < DBL_EPSILON);
+    cout << "Tests 2" << endl;
+ 
+ 
+    assert(fabs(to_float_hours(0, 15, 0) - 0.25) < DBL_EPSILON);
+    assert(fabs(to_float_hours(2, 45, 9) - 2.7525) < DBL_EPSILON);
+    assert(fabs(to_float_hours(1, 0, 36) - 1.01) < DBL_EPSILON);
+    cout << "Tests 3" << endl;
+    
+    
+    assert(fabs(to_24_hour_clock(24) - 0.0) < DBL_EPSILON);
+    assert(fabs(to_24_hour_clock(48) - 0.0) < DBL_EPSILON);
+    assert(fabs(to_24_hour_clock(25) - 1.0) < DBL_EPSILON);
+    assert(fabs(to_24_hour_clock(4) - 4.0) < DBL_EPSILON);
+    assert(fabs(to_24_hour_clock(28.5) - 4.5) < DBL_EPSILON);
+    cout << "Tests 4" << endl;
+    
+    
+    assert(fabs(time_to_utc(+0, 12.0) - 12.0) < DBL_EPSILON);
+    assert(fabs(time_to_utc(+1, 12.0) - 11.0) < DBL_EPSILON);
+    assert(fabs(time_to_utc(-1, 12.0) - 13.0) < DBL_EPSILON);
+    assert(fabs(time_to_utc(-11, 18.0) - 5.0) < DBL_EPSILON);
+    assert(fabs(time_to_utc(-1, 0.0) - 1.0) < DBL_EPSILON);
+    assert(fabs(time_to_utc(-1, 23.0) - 0.0) < DBL_EPSILON);
+    cout << "Tests 5" << endl;
+    
+    
+    assert(fabs(time_from_utc(0, 12.0) - 12.0) < DBL_EPSILON);
+    assert(fabs(time_from_utc(1, 12.0) - 13.0) < DBL_EPSILON);
+    assert(fabs(time_from_utc(-1, 12.0) - 11.0) < DBL_EPSILON);
+    assert(fabs(time_from_utc(6, 6.0) - 12.0) < DBL_EPSILON);
+    assert(fabs(time_from_utc(-7, 6.0) - 23.0) < DBL_EPSILON);
+    assert(fabs(time_from_utc(-1, 0.0) - 23.0) < DBL_EPSILON);
+    assert(fabs(time_from_utc(-1, 23.0) - 22.0) < DBL_EPSILON);
+    assert(fabs(time_from_utc(1, 23.0) - 0.0) < DBL_EPSILON);
+    cout << "Tests 6" << endl;
+    
+    
+    assert(get_hours(3800) == 1);
+    cout << "Test (get_hours)" << endl;
+    
+    
+    assert(get_minutes(3800) == 3);
+    cout << "Test(get_minutes)" << endl;
+    
+    
+    assert(get_seconds(3800) == 20);
+    cout << "Test(get_seconds)" << endl;
 }
