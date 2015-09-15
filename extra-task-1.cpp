5d9236b6505d86701188a63607e106219509c85f
max1119 <max1119@yandex.ru>
2015-09-15 23:39:55 +0400

    hours_difference

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -17,24 +17,23 @@ int main() {
 	assert(fabs(seconds_difference(1800.0, 1800.0) - 0.0) < DBL_EPSILON && "test-2");
 }
 
+//Return the number of hours later that a time in seconds
+//time_2 is than a time in seconds time_1.
+#include <iostream>
+#include <cassert>
+#include <math.h>
+using namespace std;
+
 double hours_difference(double time_1, double time_2)
 {
-    /*
-        Return the number of hours later that a time in seconds
-        time_2 is than a time in seconds time_1.
-            
-        >>> hours_difference(1800.0, 3600.0)
-        0.5
+	return (time_2 - time_1)/3600.0;
+}
 
-        >>> hours_difference(3600.0, 1800.0)
-        -0.5
-
-        >>> hours_difference(1800.0, 2160.0)
-        0.1
-
-        >>> hours_difference(1800.0, 1800.0)
-        0.0
-    */
+int main() {
+	assert(fabs(hours_difference(1800.0, 3600.0) - 0.5) < DBL_EPSILON && "test-1");
+	assert(fabs(hours_difference(3600.0, 1800.0) + 0.5) < DBL_EPSILON && "test-2");
+	assert(fabs(hours_difference(1800.0, 2160.0) - 0.1) < DBL_EPSILON && "test-2");
+	assert(fabs(hours_difference(1800.0, 1800.0) - 0.0) < DBL_EPSILON && "test-2");
 }
 
 double to_float_hours(int hours, int minutes, int seconds)
