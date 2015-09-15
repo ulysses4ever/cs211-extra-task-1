4f16350d18726a7814d833ab61c189dc9c42bd96
max1119 <max1119@yandex.ru>
2015-09-15 23:31:52 +0400

    seconds_difference

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -1,23 +1,20 @@
+// Return the number of seconds later that a time in seconds
+// time_2 is than a time in seconds time_1.
+#include <iostream>
+#include <cassert>
+#include <math.h>
+using namespace std;
+
 double seconds_difference(double time_1, double time_2)
 {
-    // your implementation goes here...
-    
-    /*    
-        Return the number of seconds later that a time in seconds
-        time_2 is than a time in seconds time_1.
-            
-        >>> seconds_difference(1800.0, 3600.0)
-        1800.0
+	return time_2 - time_1;
+}
 
-        >>> seconds_difference(3600.0, 1800.0)
-        -1800.0
-
-        >>> seconds_difference(1800.0, 2160.0)
-        360.0
-
-        >>> seconds_difference(1800.0, 1800.0)
-        0.0
-    */
+int main() {
+	assert(fabs(seconds_difference(1800.0, 3600.0) - 1800.0) < DBL_EPSILON && "test-1");
+	assert(fabs(seconds_difference(3600.0, 1800.0) + 1800.0) < DBL_EPSILON && "test-2");
+	assert(fabs(seconds_difference(1800.0, 2160.0) - 360.0) < DBL_EPSILON && "test-2");
+	assert(fabs(seconds_difference(1800.0, 1800.0) - 0.0) < DBL_EPSILON && "test-2");
 }
 
 double hours_difference(double time_1, double time_2)
