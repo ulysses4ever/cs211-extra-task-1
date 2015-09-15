d1e086dc873a24e637cc51c5b62f08b155660ba4
artking4815162342 <artur_96@bk.ru>
2015-09-15 22:34:56 +0300

    реализация hours_difference

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -11,22 +11,11 @@ double seconds_difference(double time_1, double time_2)
 
 double hours_difference(double time_1, double time_2)
 {
-    /*
-        Return the number of hours later that a time in seconds
-        time_2 is than a time in seconds time_1.
-            
-        >>> hours_difference(1800.0, 3600.0)
-        0.5
-
-        >>> hours_difference(3600.0, 1800.0)
-        -0.5
-
-        >>> hours_difference(1800.0, 2160.0)
-        0.1
-
-        >>> hours_difference(1800.0, 1800.0)
-        0.0
-    */
+	/*
+	Return the number of hours later that a time in seconds
+	time_2 is than a time in seconds time_1.
+	*/
+	return seconds_difference(time_1, time_2) / 3600.0;
 }
 
 double to_float_hours(int hours, int minutes, int seconds)
