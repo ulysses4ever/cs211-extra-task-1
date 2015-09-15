dacb89e3f943acfe00bed10bb5f57f9aa17d2c67
max1119 <max1119@yandex.ru>
2015-09-16 00:03:56 +0400

    Update extra-task-1.cpp

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -54,35 +54,28 @@ int main() {
 	assert(fabs(to_float_hours(1, 0, 36) - 1.01) < DBL_EPSILON && "test-3");
 }
 
+// hours is a number of hours since midnight. Return the
+//hour as seen on a 24 - hour clock.
+#include <iostream>
+#include <cassert>
+#include <math.h>
+using namespace std;
+
 double to_24_hour_clock(double hours)
 {
-    /*
-        hours is a number of hours since midnight. Return the
-        hour as seen on a 24-hour clock.
-
-        Precondition: hours >= 0
-
-        >>> to_24_hour_clock(24)
-        0
-        
-        >>> to_24_hour_clock(48)
-        0
-        
-        >>> to_24_hour_clock(25)
-        1
-        
-        >>> to_24_hour_clock(4)
-        4
-        
-        >>> to_24_hour_clock(28.5)
-        4.5
-        
-        You may wish to inspect various function in <cmath> to work
-        with integer and fractional part of a hours separately.
-        
-    */
+	while (hours >= 24) {
+		hours -= 24;
+	}
+	return hours;
 }
 
+int main() {
+	assert(fabs(to_24_hour_clock(24) - 0.0) < DBL_EPSILON && "test-1");
+	assert(fabs(to_24_hour_clock(48) - 0.0) < DBL_EPSILON && "test-2");
+	assert(fabs(to_24_hour_clock(25) - 1.0) < DBL_EPSILON && "test-3");
+	assert(fabs(to_24_hour_clock(4) - 4.0) < DBL_EPSILON && "test-4");
+	assert(fabs(to_24_hour_clock(28.5) - 4.5) < DBL_EPSILON && "test-5");
+}
 /*
     Implement three functions
         * get_hours
