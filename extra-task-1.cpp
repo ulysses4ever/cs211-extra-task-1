96de69e78fee8b4b896fd303ffa79bc9c9bc7df4
max1119 <max1119@yandex.ru>
2015-09-15 23:49:48 +0400

    to_float_hours

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -13,8 +13,8 @@ double seconds_difference(double time_1, double time_2)
 int main() {
 	assert(fabs(seconds_difference(1800.0, 3600.0) - 1800.0) < DBL_EPSILON && "test-1");
 	assert(fabs(seconds_difference(3600.0, 1800.0) + 1800.0) < DBL_EPSILON && "test-2");
-	assert(fabs(seconds_difference(1800.0, 2160.0) - 360.0) < DBL_EPSILON && "test-2");
-	assert(fabs(seconds_difference(1800.0, 1800.0) - 0.0) < DBL_EPSILON && "test-2");
+	assert(fabs(seconds_difference(1800.0, 2160.0) - 360.0) < DBL_EPSILON && "test-3");
+	assert(fabs(seconds_difference(1800.0, 1800.0) - 0.0) < DBL_EPSILON && "test-4");
 }
 
 //Return the number of hours later that a time in seconds
@@ -32,27 +32,26 @@ double hours_difference(double time_1, double time_2)
 int main() {
 	assert(fabs(hours_difference(1800.0, 3600.0) - 0.5) < DBL_EPSILON && "test-1");
 	assert(fabs(hours_difference(3600.0, 1800.0) + 0.5) < DBL_EPSILON && "test-2");
-	assert(fabs(hours_difference(1800.0, 2160.0) - 0.1) < DBL_EPSILON && "test-2");
-	assert(fabs(hours_difference(1800.0, 1800.0) - 0.0) < DBL_EPSILON && "test-2");
+	assert(fabs(hours_difference(1800.0, 2160.0) - 0.1) < DBL_EPSILON && "test-3");
+	assert(fabs(hours_difference(1800.0, 1800.0) - 0.0) < DBL_EPSILON && "test-4");
 }
+
+//Return the total number of hours in the specified number
+//of hours, minutes, and seconds.
+#include <iostream>
+#include <cassert>
+#include <math.h>
+using namespace std;
 
 double to_float_hours(int hours, int minutes, int seconds)
 {
-    /*
-        Return the total number of hours in the specified number
-        of hours, minutes, and seconds.
+	return hours + minutes/ 60.0 + seconds/3600.0;
+}
 
-        Precondition: 0 <= minutes < 60  and  0 <= seconds < 60
-
-        >>> to_float_hours(0, 15, 0)
-        0.25
-
-        >>> to_float_hours(2, 45, 9)
-        2.7525
-
-        >>> to_float_hours(1, 0, 36)
-        1.01
-    */
+int main() {
+	assert(fabs(to_float_hours(0, 15, 0) - 0.25) < DBL_EPSILON && "test-1");
+	assert(fabs(to_float_hours(2, 45, 9) - 2.7525) < DBL_EPSILON && "test-2");
+	assert(fabs(to_float_hours(1, 0, 36) - 1.01) < DBL_EPSILON && "test-3");
 }
 
 double to_24_hour_clock(double hours)
