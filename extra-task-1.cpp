f0c42556db26d4b041b12e2b82c4010151fd339c
artking4815162342 <artur_96@bk.ru>
2015-09-15 22:22:46 +0300

    реализация second_difference

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -1,23 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
 double seconds_difference(double time_1, double time_2)
 {
-    // your implementation goes here...
-    
-    /*    
-        Return the number of seconds later that a time in seconds
-        time_2 is than a time in seconds time_1.
-            
-        >>> seconds_difference(1800.0, 3600.0)
-        1800.0
-
-        >>> seconds_difference(3600.0, 1800.0)
-        -1800.0
-
-        >>> seconds_difference(1800.0, 2160.0)
-        360.0
-
-        >>> seconds_difference(1800.0, 1800.0)
-        0.0
-    */
+	//Return the number of seconds later that a time in seconds
+	//time_2 is than a time in seconds time_1.
+	return time_2 - time_1;
 }
 
 double hours_difference(double time_1, double time_2)
@@ -166,4 +155,9 @@ double time_from_utc(int utc_offset, double time)
         >>> time_from_utc(+1, 23.0)
         0.0
     */
+}
+
+int main()
+{	
+	system("pause");
 }
