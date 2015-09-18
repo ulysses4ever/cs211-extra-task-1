d46161d745a79574bd8e2dcd41b2d8d3160c9f2d
Your Name <you@example.com>
2015-09-19 00:13:47 +0300

    realisation_6_function_last

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -81,36 +81,14 @@ double time_to_utc(int utc_offset, double time)
 		return (time - utc_offset < 0) ? time - utc_offset + 24 : fmod(time - utc_offset, 24);
 }
 
-//double time_from_utc(int utc_offset, double time)
-//{
+double time_from_utc(int utc_offset, double time)
+{
     /*
-        Return UTC time in time zone utc_offset.
-
-        >>> time_from_utc(+0, 12.0)
-        12.0
- 
-        >>> time_from_utc(+1, 12.0)
-        13.0
- 
-        >>> time_from_utc(-1, 12.0)
-        11.0
- 
-        >>> time_from_utc(+6, 6.0)
-        12.0
- 
-        >>> time_from_utc(-7, 6.0)
-        23.0
- 
-        >>> time_from_utc(-1, 0.0)
-        23.0
- 
-        >>> time_from_utc(-1, 23.0)
-        22.0
- 
-        >>> time_from_utc(+1, 23.0)
-        0.0
-    */
-//}
+        Return UTC time in time zone utc_offset.   */
+		assert(time >= 0);
+		return (time + utc_offset < 0) ? time + utc_offset + 24 : fmod(time + utc_offset, 24);
+}
+       
 
 int main()
 {
@@ -145,4 +123,15 @@ int main()
 	assert(time_to_utc(-11, 18.0) == 5.0);
 	assert(time_to_utc(-1, 0.0) == 1.0);
 	assert( time_to_utc(-1, 23.0) == 0.0);
+	
+	//time_from_utc
+	assert(time_from_utc(+0, 12.0) == 12.0);
+	assert(time_from_utc(+1, 12.0) == 13.0);
+	assert(time_from_utc(-1, 12.0) == 11.0);
+	assert(time_from_utc(+6, 6.0) == 12.0);
+	assert(time_from_utc(-7, 6.0) == 23.0);
+	assert(time_from_utc(-1, 0.0) == 23.0);
+	assert(time_from_utc(-1, 23.0) == 22.0);
+	assert(time_from_utc(+1, 23.0) == 0.0);
+
 }
