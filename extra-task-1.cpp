40cbcbb7e0bef85682d16eaa36cb0f6e06e7f88b
Tyan Vyacheslav <chapaevez@yabdex.ru>
2015-09-20 18:57:25 +0400

    time_from_utc

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -181,7 +181,7 @@ double time_to_utc(int utc_offset, double time)
 
 double time_from_utc(int utc_offset, double time)
 {
-	return 0.0;
+	return (double)((24 + (int)(time + utc_offset)) % 24);
 	/*
 	Return UTC time in time zone utc_offset.
 
@@ -252,4 +252,14 @@ int main()
 	assert(AreEqual(time_to_utc(-11, 18.0), 5.0));
 	assert(AreEqual(time_to_utc(-1, 0.0), 1.0));
 	assert(AreEqual(time_to_utc(-1, 23.0), 0.0));
+
+
+	assert(AreEqual(time_from_utc(+0, 12.0), 12.0));
+	assert(AreEqual(time_from_utc(+1, 12.0), 13.0));
+	assert(AreEqual(time_from_utc(-1, 12.0), 11.0));
+	assert(AreEqual(time_from_utc(+6, 6.0), 12.0));
+	assert(AreEqual(time_from_utc(-7, 6.0), 23.0));
+	assert(AreEqual(time_from_utc(-1, 0.0), 23.0));
+	assert(AreEqual(time_from_utc(-1, 23.0), 22.0));
+    assert(AreEqual(time_from_utc(+1, 23.0), 0.0));
 }
