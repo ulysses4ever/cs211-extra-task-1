80bccb4b327516bd064dd65de6bdcc7922074a83
Tyan Vyacheslav <chapaevez@yabdex.ru>
2015-09-20 16:55:51 +0400

    hours_difference

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -34,7 +34,9 @@ double seconds_difference(double time_1, double time_2)
 
 double hours_difference(double time_1, double time_2)
 {
-	return 0.0;
+	assert(time_1 >= 0.0);
+	assert(time_2 >= 0.0);
+	return (time_2 - time_1) / 3600;
 	/*
 	Return the number of hours later that a time in seconds
 	time_2 is than a time in seconds time_1.
@@ -194,4 +196,9 @@ int main()
 	assert(AreEqual(seconds_difference(1800.0, 2160.0), 360.0));
 	assert(AreEqual(seconds_difference(1800.0, 1800.0), 0.0));
 
+
+	assert(AreEqual(hours_difference(1800.0, 3600.0), 0.5));
+	assert(AreEqual(hours_difference(3600.0, 1800.0), -0.5));
+	assert(AreEqual(hours_difference(1800.0, 2160.0), 0.1));
+	assert(AreEqual(hours_difference(1800.0, 1800.0), 0.0));
 }
