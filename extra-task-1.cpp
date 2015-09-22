d65e3dcd56c95b795f36beed917a90c6bb964eac
root <root@Boch>
2015-09-22 22:49:40 +0400

    imp of seconds_difference

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -1,7 +1,6 @@
 double seconds_difference(double time_1, double time_2)
 {
-    // your implementation goes here...
-    
+    return time_2 - time_1;
     /*    
         Return the number of seconds later that a time in seconds
         time_2 is than a time in seconds time_1.
@@ -21,7 +20,8 @@ double seconds_difference(double time_1, double time_2)
 }
 
 double hours_difference(double time_1, double time_2)
-{
+{   
+    return (time_2 - time_1) / 3600;
     /*
         Return the number of hours later that a time in seconds
         time_2 is than a time in seconds time_1.
@@ -42,6 +42,7 @@ double hours_difference(double time_1, double time_2)
 
 double to_float_hours(int hours, int minutes, int seconds)
 {
+    return 
     /*
         Return the total number of hours in the specified number
         of hours, minutes, and seconds.
