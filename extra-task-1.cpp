7bfa49e80d71f8fd7575a27b162a3bee3a287886
Evgeny Dorofeev <giraffe_1996@mail.ru>
2015-09-24 20:39:35 +0300

    function hours_difference was implemented.

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -29,7 +29,7 @@ double seconds_difference(double time_1, double time_2)
 
 double hours_difference(double time_1, double time_2)
 {  
-	return 0;
+	return (time_2 - time_1) / 3600;
 	/*
         Return the number of hours later that a time in seconds
         time_2 is than a time in seconds time_1.
@@ -187,6 +187,12 @@ int main()
 	assert(fabs(seconds_difference(3600.0, 1800.0) + 1800.0) < DBL_EPSILON);
 	assert(fabs(seconds_difference(1800.0, 2160.0) - 360.0) < DBL_EPSILON);
 	assert(fabs(seconds_difference(1800.0, 1800.0)) < DBL_EPSILON);
+
+	// Test hours_difference
+	assert(fabs(hours_difference(1800.0, 3600.0) - 0.5) < DBL_EPSILON);
+	assert(fabs(hours_difference(3600.0, 1800.0) + 0.5) < DBL_EPSILON);
+	assert(fabs(hours_difference(1800.0, 2160.0) - 0.1) < DBL_EPSILON);
+	assert(fabs(hours_difference(1800.0, 1800.0) - 0.0) < DBL_EPSILON);
 
 	cout << "Tests successfully completed!" << endl;
 }
