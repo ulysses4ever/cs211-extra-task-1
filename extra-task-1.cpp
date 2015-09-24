45c1f4d26110a16b7fad4f246f385fe8c4926e2c
Evgeny Dorofeev <giraffe_1996@mail.ru>
2015-09-24 20:40:54 +0300

    function to_float_hours was implemented.

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -50,7 +50,7 @@ double hours_difference(double time_1, double time_2)
 
 double to_float_hours(int hours, int minutes, int seconds)
 {
-	return 0;
+	return hours + minutes / 60.0 + seconds / 3600.0;
 	/*
         Return the total number of hours in the specified number
         of hours, minutes, and seconds.
@@ -193,6 +193,11 @@ int main()
 	assert(fabs(hours_difference(3600.0, 1800.0) + 0.5) < DBL_EPSILON);
 	assert(fabs(hours_difference(1800.0, 2160.0) - 0.1) < DBL_EPSILON);
 	assert(fabs(hours_difference(1800.0, 1800.0) - 0.0) < DBL_EPSILON);
+
+	// Test to_float_hours
+	assert(fabs(to_float_hours(0, 15, 0) - 0.25) < DBL_EPSILON);
+	assert(fabs(to_float_hours(2, 45, 9) - 2.7525) < DBL_EPSILON);
+	assert(fabs(to_float_hours(1, 0, 36) - 1.01) < DBL_EPSILON);
 
 	cout << "Tests successfully completed!" << endl;
 }
