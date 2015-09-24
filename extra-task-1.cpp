af9be9919412b77d4b65154496d6491d5ef08807
Evgeny Dorofeev <giraffe_1996@mail.ru>
2015-09-24 21:12:39 +0300

    Necessary asserts were added in each function. Unnecesarry comments were deleted.

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -7,6 +7,7 @@ using namespace std;
 
 double seconds_difference(double time_1, double time_2)
 {
+	assert((time_1 >= 0) && (time_2 >= 0));
 	return time_2 - time_1;
     
     /*    
@@ -29,6 +30,7 @@ double seconds_difference(double time_1, double time_2)
 
 double hours_difference(double time_1, double time_2)
 {  
+	assert((time_1 >= 0) && (time_2 >= 0));
 	return (time_2 - time_1) / 3600;
 	/*
         Return the number of hours later that a time in seconds
@@ -50,6 +52,7 @@ double hours_difference(double time_1, double time_2)
 
 double to_float_hours(int hours, int minutes, int seconds)
 {
+	assert((hours >= 0) && (minutes >= 0) && (seconds >= 0));
 	return hours + minutes / 60.0 + seconds / 3600.0;
 	/*
         Return the total number of hours in the specified number
@@ -70,6 +73,7 @@ double to_float_hours(int hours, int minutes, int seconds)
 
 double to_24_hour_clock(double hours)
 {
+	assert(hours >= 0);
 	return fmod(hours, 24);
 	/*
         hours is a number of hours since midnight. Return the
@@ -92,57 +96,35 @@ double to_24_hour_clock(double hours)
         >>> to_24_hour_clock(28.5)
         4.5
         
-        You may wish to inspect various function in <cmath> to work
-        with integer and fractional part of a hours separately.
-        
     */
 }
 
-/*
-    Implement three functions
-        * get_hours
-        * get_minutes
-        * get_seconds
-    They are used to determine the hours part, minutes part and seconds part 
-    of a time in seconds. E.g.:
-
-    >>> get_hours(3800)
-    1
-
-    >>> get_minutes(3800)
-    3
-
-    >>> get_seconds(3800)
-    20
-
-    In other words, if 3800 seconds have elapsed since midnight, 
-    it is currently 01:03:20 (hh:mm:ss).
-*/
-
 int get_hours(int seconds)
 {
+	assert(seconds >= 0);
 	return seconds / 3600;
 }
 
 int get_minutes(int seconds)
 {
+	assert(seconds >= 0);
 	return (seconds / 60) % 60;
 }
 
 int get_seconds(int seconds)
 {
+	assert(seconds >= 0);
 	return seconds % 60;
 }
 
 double time_to_utc(int utc_offset, double time)
 {
+	assert(time >= 0);
 	double res = fmod(time - utc_offset, 24);
 	return (res < 0) ? 24 + res : res;
 	/*
         Return time at UTC+0, where utc_offset is the number of hours away from
         UTC+0.
-        You may be interested in:
-        https://en.wikipedia.org/wiki/Coordinated_Universal_Time
 
         >>> time_to_utc(+0, 12.0)
         12.0
@@ -166,7 +148,9 @@ double time_to_utc(int utc_offset, double time)
 
 double time_from_utc(int utc_offset, double time)
 {
-	return 0;
+	assert(time >= 0);
+	double res = fmod(utc_offset + time, 24);
+	return (res < 0) ? 24 + res : res;
 	/*
         Return UTC time in time zone utc_offset.
 
@@ -234,6 +218,16 @@ int main()
 	assert(fabs(time_to_utc(-11, 18.0) - 5.0) < DBL_EPSILON);
 	assert(fabs(time_to_utc(-1, 0.0) - 1.0) < DBL_EPSILON);
 	assert(fabs(time_to_utc(-1, 23.0)) < DBL_EPSILON);
+
+	// Test time_from_utc
+	assert(fabs(time_from_utc(+0, 12.0) - 12) < DBL_EPSILON);
+	assert(fabs(time_from_utc(+1, 12.0) - 13) < DBL_EPSILON);
+	assert(fabs(time_from_utc(-1, 12.0) - 11) < DBL_EPSILON);
+	assert(fabs(time_from_utc(+6, 6.0) - 12) < DBL_EPSILON);
+	assert(fabs(time_from_utc(-7, 6.0) - 23) < DBL_EPSILON);
+	assert(fabs(time_from_utc(-1, 0.0) - 23) < DBL_EPSILON);
+	assert(fabs(time_from_utc(-1, 23.0) - 22) < DBL_EPSILON);
+	assert(fabs(time_from_utc(+1, 23.0)) < DBL_EPSILON);
 
 	cout << "Tests successfully completed!" << endl;
 }
