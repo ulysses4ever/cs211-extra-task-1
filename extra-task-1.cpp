abe982f77e54feb52c476eea5d20a0b12f447a7e
Evgeny Dorofeev <giraffe_1996@mail.ru>
2015-09-24 20:48:55 +0300

    function time_to_utc was implemented.

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -136,7 +136,8 @@ int get_seconds(int seconds)
 
 double time_to_utc(int utc_offset, double time)
 {
-	return 0;
+	double res = fmod(time - utc_offset, 24);
+	return (res < 0) ? 24 + res : res;
 	/*
         Return time at UTC+0, where utc_offset is the number of hours away from
         UTC+0.
@@ -225,6 +226,14 @@ int main()
 	assert(get_hours(3800) == 1);
 	assert(get_minutes(3800) == 3);
 	assert(get_seconds(3800) == 20);
+
+	// Test time_to_utc
+	assert(fabs(time_to_utc(+0, 12.0) - 12.0) < DBL_EPSILON);
+	assert(fabs(time_to_utc(+1, 12.0) - 11.0) < DBL_EPSILON);
+	assert(fabs(time_to_utc(-1, 12.0) - 13.0) < DBL_EPSILON);
+	assert(fabs(time_to_utc(-11, 18.0) - 5.0) < DBL_EPSILON);
+	assert(fabs(time_to_utc(-1, 0.0) - 1.0) < DBL_EPSILON);
+	assert(fabs(time_to_utc(-1, 23.0)) < DBL_EPSILON);
 
 	cout << "Tests successfully completed!" << endl;
 }
