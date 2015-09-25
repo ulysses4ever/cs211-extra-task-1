c0752e70a45d4d8bbe6e083713f424cd45d631ae
sarieved <sarieved@gmail.com>
2015-09-25 20:39:39 +0300

    time_from_utc function + time_to_utc bug fixes

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -2,6 +2,8 @@
 #include <cmath>
 #include <cfloat>
 
+const double day = 24.0;
+
 bool areEquals(double x, double y) {
 	return fabs(x - y) < DBL_EPSILON;
 }
@@ -22,7 +24,6 @@ double to_float_hours(int hours, int minutes, int seconds)
 	assert((seconds >= 0) && (seconds < 60));
 	return hours + (double(minutes) / 60) + (double(seconds) / 3600);
 }
-
 
 double to_24_hour_clock(double hours)
 {
@@ -47,40 +48,20 @@ int get_seconds(int seconds) {
 
 double time_to_utc(int utc_offset, double time)
 {
-	assert((uts_offset <= 14) && (utc_offset >= -12));
+	assert((utc_offset <= 14) && (utc_offset >= -12));
 	assert((trunc(time) >= 0) && (trunc(time) <= 23));
-	return (int)(time - utc_offset) % 24;
+	double mins = time - trunc(time);
+	double newT = trunc(time) - utc_offset;
+	return newT > 0 ? (int)newT % 24 + mins : day + (int)newT % 24 + mins;
 }
 
 double time_from_utc(int utc_offset, double time)
 {
-	/*
-	Return UTC time in time zone utc_offset.
-
-	>>> time_from_utc(+0, 12.0)
-	12.0
-
-	>>> time_from_utc(+1, 12.0)
-	13.0
-
-	>>> time_from_utc(-1, 12.0)
-	11.0
-
-	>>> time_from_utc(+6, 6.0)
-	12.0
-
-	>>> time_from_utc(-7, 6.0)
-	23.0
-
-	>>> time_from_utc(-1, 0.0)
-	23.0
-
-	>>> time_from_utc(-1, 23.0)
-	22.0
-
-	>>> time_from_utc(+1, 23.0)
-	0.0
-	*/
+	assert((utc_offset <= 14) && (utc_offset >= -12));
+	assert((trunc(time) >= 0) && (trunc(time) <= 23));
+	double mins = time - trunc(time);
+	double newT = trunc(time) + utc_offset;
+	return newT > 0 ? (int)newT % 24 + mins : day + (int)newT % 24 + mins;
 }
 
 int main() {
@@ -130,4 +111,14 @@ int main() {
 	assert(areEquals(time_to_utc(-11, 18.0), 5.0) && ("test #8.4"));
 	assert(areEquals(time_to_utc(-1, 0.0), 1.0) && ("test #8.5"));
 	assert(areEquals(time_to_utc(-1, 23.0), 0.0) && ("test #8.6"));
+
+	//time_from_utc tests
+	assert(areEquals(time_from_utc(0, 12.0), 12.0) && ("test #9.1"));
+	assert(areEquals(time_from_utc(1, 12.0), 13.0) && ("test #9.2"));
+	assert(areEquals(time_from_utc(-1, 12.0), 11.0) && ("test #9.3"));
+	assert(areEquals(time_from_utc(6, 6.0), 12.0) && ("test #9.4"));
+	assert(areEquals(time_from_utc(-7, 6.0), 23.0) && ("test #9.5"));
+	assert(areEquals(time_from_utc(-1, 0.0), 23.0) && ("test #9.6"));
+	assert(areEquals(time_from_utc(-1, 23.0), 22.0) && ("test #9.7"));
+	assert(areEquals(time_from_utc(1, 23.0), 0.0) && ("test #9.8"));
 }
