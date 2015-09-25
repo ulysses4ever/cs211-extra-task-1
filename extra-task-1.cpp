1a474e56db575ac6f46ee48d43561c33b790553f
StKseniya <ksenya15-92@mail.ru>
2015-09-25 18:51:55 +0300

    реализованы функции seconds_difference и hours_difference

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -1,22 +1,11 @@
+#include <cassert>
+#include <cfloat>
 double seconds_difference(double time_1, double time_2)
 {
     // your implementation goes here...
-    
+       return time_2 - time_1;
     /*    
-        Return the number of seconds later that a time in seconds
-        time_2 is than a time in seconds time_1.
-            
-        >>> seconds_difference(1800.0, 3600.0)
-        1800.0
-
-        >>> seconds_difference(3600.0, 1800.0)
-        -1800.0
-
-        >>> seconds_difference(1800.0, 2160.0)
-        360.0
-
-        >>> seconds_difference(1800.0, 1800.0)
-        0.0
+ 
     */
 }
 
@@ -167,3 +156,12 @@ double time_from_utc(int utc_offset, double time)
         0.0
     */
 }
+ int main()
+ {
+            
+        assert(seconds_difference(1800.0, 3600.0) - 1800.0 < DBL_EPSILON); 
+        assert(seconds_difference(3600.0, 1800.0) - -1800.0 < DBL_EPSILON );
+        assert(seconds_difference(1800.0, 2160.0) - 360.0 < DBL_EPSILON);
+        assert(seconds_difference(1800.0, 1800.0) -  0.00 < DBL_EPSILON);
+
+ }
