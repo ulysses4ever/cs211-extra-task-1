1b1ed3cd46fe5d4bd9a6ab33e9b1a75e455bc618
vladimikh <vladimikh97@yandex.ru>
2015-09-25 18:34:34 +0300

    implement hours_difference

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -12,27 +12,13 @@ double seconds_difference(double time_1, double time_2)
 	return time_2 - time_1;
 }
 
-
+/*
+Return the number of hours later that a time in seconds
+time_2 is than a time in seconds time_1.
+*/
 double hours_difference(double time_1, double time_2)
 {
-    /*
-        Return the number of hours later that a time in seconds
-        time_2 is than a time in seconds time_1.
-            
-        >>> hours_difference(1800.0, 3600.0)
-        0.5
-
-        >>> hours_difference(3600.0, 1800.0)
-        -0.5
-
-        >>> hours_difference(1800.0, 2160.0)
-        0.1
-
-        >>> hours_difference(1800.0, 1800.0)
-        0.0
-    */
-	return 0;
-
+	return seconds_difference(time_1, time_2) / 3600;
 }
 
 double to_float_hours(int hours, int minutes, int seconds)
@@ -182,5 +168,17 @@ int main() {
 	assert(fabs(seconds_difference(1800.0, 1800.0)) < DBL_EPSILON);
 
 	cout << "Tests for the function 'seconds_difference' are completed successfully" << endl;
+
+	/*
+	Return the number of hours later that a time in seconds
+	time_2 is than a time in seconds time_1.
+	*/
+
+	assert(fabs(hours_difference(1800.0, 3600.0) - 0.5) < DBL_EPSILON);
+	assert(fabs(hours_difference(3600.0, 1800.0) + 0.5) < DBL_EPSILON);
+	assert(fabs(hours_difference(1800.0, 2160.0) - 0.1) < DBL_EPSILON);
+	assert(fabs(hours_difference(1800.0, 1800.0)) < DBL_EPSILON);
+
+	cout << "Tests for the function 'hours_difference' are completed successfully" << endl;
 
 }
