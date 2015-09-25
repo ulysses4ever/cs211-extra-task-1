ce92f2f7ca322823664b33335dd647964126438b
StKseniya <ksenya15-92@mail.ru>
2015-09-25 18:57:05 +0300

    реализована функиция to_float_hours

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -2,50 +2,19 @@
 #include <cfloat>
 double seconds_difference(double time_1, double time_2)
 {
-    // your implementation goes here...
        return time_2 - time_1;
-    /*    
- 
-    */
 }
 
 double hours_difference(double time_1, double time_2)
 {
-    /*
-        Return the number of hours later that a time in seconds
-        time_2 is than a time in seconds time_1.
-            
-        >>> hours_difference(1800.0, 3600.0)
-        0.5
-
-        >>> hours_difference(3600.0, 1800.0)
-        -0.5
-
-        >>> hours_difference(1800.0, 2160.0)
-        0.1
-
-        >>> hours_difference(1800.0, 1800.0)
-        0.0
-    */
+   return seconds_difference(time_1, time_2) / 3600;
+   
 }
 
 double to_float_hours(int hours, int minutes, int seconds)
 {
-    /*
-        Return the total number of hours in the specified number
-        of hours, minutes, and seconds.
-
-        Precondition: 0 <= minutes < 60  and  0 <= seconds < 60
-
-        >>> to_float_hours(0, 15, 0)
-        0.25
-
-        >>> to_float_hours(2, 45, 9)
-        2.7525
-
-        >>> to_float_hours(1, 0, 36)
-        1.01
-    */
+    return hours + minutes / 60.0 + seconds / 3600.0;
+    
 }
 
 double to_24_hour_clock(double hours)
@@ -163,5 +132,15 @@ double time_from_utc(int utc_offset, double time)
         assert(seconds_difference(3600.0, 1800.0) - -1800.0 < DBL_EPSILON );
         assert(seconds_difference(1800.0, 2160.0) - 360.0 < DBL_EPSILON);
         assert(seconds_difference(1800.0, 1800.0) -  0.00 < DBL_EPSILON);
+
+        assert(hours_difference(1800.0, 3600.0) - 0.5 < DBL_EPSILON); 
+        assert(hours_difference(3600.0, 1800.0) - -0.5 < DBL_EPSILON);
+        assert(hours_difference(1800.0, 2160.0) -  0.1 < DBL_EPSILON);
+        assert(hours_difference(1800.0, 1800.0) -  0.0 < DBL_EPSILON);
+
+        assert(to_float_hours(0, 15, 0) - 0.25 < DBL_EPSILON);
+        assert(to_float_hours(2, 45, 9) - 2.7525 < DBL_EPSILON);
+        assert(to_float_hours(1, 0, 36) - 1.01 < DBL_EPSILON);
+        
 
  }
