55943d5f28e70df51fd8a6bd11d3e5fc6a952f34
calstream <calstream.lgn@gmail.com>
2015-09-25 17:31:51 +0300

    added: time_to_utc

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -59,6 +59,15 @@ int get_seconds(int seconds)
 	return((seconds % 3600) % 60);
 }
 
+// Returns time at UTC+0, where utc_offset is the number of hours away from UTC + 0
+        
+double time_to_utc(int utc_offset, double time)
+{
+	assert(time >= 0.0 && time < 24.00, "TIME_TO_UTC: time out of range");
+	double t = time - utc_offset;
+	return (int)trunc(t) % 24 + t - trunc(t);
+}
+
 int main()
 {
 
@@ -106,41 +115,19 @@ int main()
 	assert(get_seconds(9032) == 32, "get_seconds: test #3");
 	assert(get_seconds(73) == 13, "get_seconds: test #4");
 	assert(get_seconds(0) == 0, "get_seconds: test #5");
+
+	//TIME_TO_UTC TESTS:
+	assert(time_to_utc(+0, 12.0) == 12.0, "time_to_utc: test #1");
+	assert(time_to_utc(+1, 12.0) == 11.0, "time_to_utc: test #2");
+	assert(time_to_utc(-1, 12.0) == 13.0, "time_to_utc: test #3");
+	assert(time_to_utc(-11, 18.0) == 5.0, "time_to_utc: test #4");
+	assert(time_to_utc(-1, 0.0) == 1.0, "time_to_utc: test #5");
+	assert(time_to_utc(-1, 23.0) == 0.0, "time_to_utc: test #6");
+
 }
 
 
 
-
-
-
-
-
-
-
-
-/* 
-
-
-/*
-    Implement three functions
-        * get_hours
-        * get_minutes
-        * get_seconds
-    They are used to determine the hours part, minutes part and seconds part 
-    of a time in seconds. E.g.:
-
-    >>> get_hours(3800)
-    1
-
-    >>> get_minutes(3800)
-    3
-
-    >>> get_seconds(3800)
-    20
-
-    In other words, if 3800 seconds have elapsed since midnight, 
-    it is currently 01:03:20 (hh:mm:ss).
-*/
 
 //double time_to_utc(int utc_offset, double time)
 //{
