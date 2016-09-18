87716c219ed4b6770e1ef337991f193995b86796
Крылов <d.krylov@open-bs.ru>
2016-09-18 22:52:08 +0300

    seconds_difference hours_difference to_float_hours

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -1,6 +1,11 @@
+#include <iostream>
+#include <cassert>
+
+using namespace std;
+
 double seconds_difference(double time_1, double time_2)
 {
-    // your implementation goes here...
+	return time_2 - time_1;
     
     /*    
         Return the number of seconds later that a time in seconds
@@ -22,6 +27,7 @@ double seconds_difference(double time_1, double time_2)
 
 double hours_difference(double time_1, double time_2)
 {
+	return seconds_difference(time_1, time_2) / 3600.0;
     /*
         Return the number of hours later that a time in seconds
         time_2 is than a time in seconds time_1.
@@ -42,6 +48,7 @@ double hours_difference(double time_1, double time_2)
 
 double to_float_hours(int hours, int minutes, int seconds)
 {
+	return hours + minutes / 60.0 + seconds / 3600.0;
     /*
         Return the total number of hours in the specified number
         of hours, minutes, and seconds.
@@ -61,6 +68,7 @@ double to_float_hours(int hours, int minutes, int seconds)
 
 double to_24_hour_clock(double hours)
 {
+	return hours - ((hours / 24) * 24);
     /*
         hours is a number of hours since midnight. Return the
         hour as seen on a 24-hour clock.
@@ -135,6 +143,7 @@ double time_to_utc(int utc_offset, double time)
         >>> time_to_utc(-1, 23.0)
         0.0
     */
+	return 0.0;
 }
 
 double time_from_utc(int utc_offset, double time)
@@ -166,4 +175,59 @@ double time_from_utc(int utc_offset, double time)
         >>> time_from_utc(+1, 23.0)
         0.0
     */
+	return 0.0;
+}
+
+int main()
+{
+	assert(seconds_difference(1800.0, 3600.0) == 1800.0);
+	assert(seconds_difference(3600.0, 1800.0) == -1800.0);
+	assert(seconds_difference(1800.0, 2160.0) == 360.0);
+	assert(seconds_difference(1800.0, 1800.0) == 0.0);
+	cout << "seconds_difference tests passed!" << endl;
+
+	assert(hours_difference(1800.0, 3600.0) == 0.5);
+	assert(hours_difference(3600.0, 1800.0) == -0.5);
+	assert(hours_difference(1800.0, 2160.0) == 0.1);
+	assert(hours_difference(1800.0, 1800.0) == 0.0);
+	cout << "hours_difference tests passed!" << endl;
+
+	assert(to_float_hours(0, 15, 0) == 0.25);
+	assert(to_float_hours(2, 45, 9) == 2.7525);
+	assert(to_float_hours(1, 0, 36) == 1.01);
+	cout << "to_float_hours tests passed!" << endl;
+
+	/*
+	hours is a number of hours since midnight. Return the
+	hour as seen on a 24-hour clock.
+
+	Precondition: hours >= 0
+
+	>>> to_24_hour_clock(24)
+	0
+
+	>>> to_24_hour_clock(48)
+	0
+
+	>>> to_24_hour_clock(25)
+	1
+
+	>>> to_24_hour_clock(4)
+	4
+
+	>>> to_24_hour_clock(28.5)
+	4.5
+
+	You may wish to inspect various function in <cmath> to work
+	with integer and fractional part of a hours separately.
+
+	*/
+	assert(to_24_hour_clock(24) == 0);
+	assert(to_24_hour_clock(48) == 0);
+	cout << to_24_hour_clock(300) << endl;
+	//assert(to_24_hour_clock(25) == 1);
+	//assert(to_24_hour_clock(4) == 4);
+	//assert(to_24_hour_clock(28.5) == 4.5);
+	cout << "to_24_hour_clock tests passed!" << endl;
+
 }
