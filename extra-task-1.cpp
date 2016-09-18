8edb480ea4d3cf6beedda3a3ec8186407b8e10dc
Battler45 <ushiromiya7@gmail.com>
2016-09-18 16:08:52 +0300

    Update extra-task-1.cpp

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -39,6 +39,7 @@ double hours_difference(double time_1, double time_2)
         >>> hours_difference(1800.0, 1800.0)
         0.0
     */
+    return (time_2 - time_1)/3600.0;
 }
 
 double to_float_hours(int hours, int minutes, int seconds)
