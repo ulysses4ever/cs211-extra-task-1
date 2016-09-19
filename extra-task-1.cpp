d1c687f682530c3f530acf0657a2e130da2582ae
admin <rmansorokin@gmail.com>
2016-09-19 12:13:14 +0300

    hours_difference function with tests added.

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -206,6 +206,18 @@ int main()
 	assert(fabs(seconds_difference(1800.0, 2160.0) - 360.0) < DBL_EPS);
 	assert(fabs(seconds_difference(1800.0, 1800.0)) < DBL_EPS);
 
+	cout << "seconds_difference tests are completed succesfully" << endl;
+
+	//hours_difference test
+	assert(fabs(hours_difference(1800.0, 3600.0) - 0.5) < DBL_EPS);
+	assert(fabs(hours_difference(3600.0, 1800.0) + 0.5) < DBL_EPS);
+	assert(fabs(hours_difference(1800.0, 2160.0) - 0.1) < DBL_EPS);
+	assert(fabs(hours_difference(1800.0, 1800.0)) < DBL_EPS);
+
+	cout << "hours_difference tests are completed succesfully" << endl;
+
+
+
 
 
 }
