33b07d1b1838e8f6ed5744e622f03dbac8fcf45e
PolluxCastor <kumaritof@gmail.com>
2016-09-20 19:19:29 +0300

    Реализована и протестирована 3 функция

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -50,6 +50,7 @@ double hours_difference(double time_1, double time_2)
 
 double to_float_hours(int hours, int minutes, int seconds)
 {
+	return hours + (minutes / 60.0) + (seconds / 3600.0);
 	
     /*
         Return the total number of hours in the specified number
@@ -186,4 +187,7 @@ int main(){
 		assert(fabs(hours_difference(3600.0, 1800.0) + 0.5) < DBL_EPSILON && "test-2.2");
 		assert(fabs(hours_difference(1800.0, 2160.0) - 0.1) < DBL_EPSILON && "test-2.3");
 		assert(fabs(hours_difference(1800.0, 1800.0) - 0.0) < DBL_EPSILON && "test-2.4");
+		assert(fabs(to_float_hours(0, 15, 0) - 0.25) < DBL_EPSILON && "test-3.1");
+		assert(fabs(to_float_hours(2, 45, 9) - 2.7525) < DBL_EPSILON && "test-3.2");
+		assert(fabs(to_float_hours(1, 0, 36) - 1.01) < DBL_EPSILON && "test-3.3");
 }
