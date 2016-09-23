6c0db6bd75d88ea34acb7b7706616e87323c4dad
numumotik <numumotik@mail.ru>
2016-09-23 22:07:50 +0300

    to_float_hours

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -4,36 +4,26 @@ using namespace std;
 
 double seconds_difference(double time_1, double time_2)
 {
-/*	Return the number of seconds later that a time in seconds
-	time_2 is than a time in seconds time_1. */
-	return time_2 - time_1;
+    /*Return the number of seconds later that a time in seconds
+    time_2 is than a time in seconds time_1. */
+    return time_2 - time_1;
 }
 
 double hours_difference(double time_1, double time_2)
 {
-        /*Return the number of hours later that a time in seconds
-	time_2 is than a time in seconds time_1.*/
-	return seconds_difference(time_1, time_2) / 3600;
+    /*Return the number of hours later that a time in seconds
+    time_2 is than a time in seconds time_1.*/
+    return seconds_difference(time_1, time_2) / 3600;
 }
 
- /*double to_float_hours(int hours, int minutes, int seconds)
+ double to_float_hours(int hours, int minutes, int seconds)
 {
-   
-        Return the total number of hours in the specified number
-        of hours, minutes, and seconds.
-
-        Precondition: 0 <= minutes < 60  and  0 <= seconds < 60
-
-        >>> to_float_hours(0, 15, 0)
-        0.25
-
-        >>> to_float_hours(2, 45, 9)
-        2.7525
-
-        >>> to_float_hours(1, 0, 36)
-        1.01
-    
-}*/
+   /*Return the total number of hours in the specified number
+    of hours, minutes, and seconds.*/
+    assert((minutes >= 0) && (minutes < 60));
+    assert((seconds >= 0) && (seconds < 60));
+    return hours + minutes / 60.0 + seconds / 3600.0;
+}
 
  /*double to_24_hour_clock(double hours)
 {
@@ -146,14 +136,19 @@ double hours_difference(double time_1, double time_2)
 
 int main()
 {  /* seconds_difference */
-	assert(fabs(seconds_difference(1800.0, 3600.0) - 1800.0) < DBL_EPSILON);
-	assert(fabs(seconds_difference(3600.0, 1800.0) + 1800.0) < DBL_EPSILON);
-	assert(fabs(seconds_difference(1800.0, 2160.0) - 360.0) < DBL_EPSILON);
-	assert(fabs(seconds_difference(1800.0, 1800.0) - 0.0) < DBL_EPSILON);
+    assert(fabs(seconds_difference(1800.0, 3600.0) - 1800.0) < DBL_EPSILON);
+    assert(fabs(seconds_difference(3600.0, 1800.0) + 1800.0) < DBL_EPSILON);
+    assert(fabs(seconds_difference(1800.0, 2160.0) - 360.0) < DBL_EPSILON);
+    assert(fabs(seconds_difference(1800.0, 1800.0) - 0.0) < DBL_EPSILON);
 	
-	/* hours_difference */
-	assert(fabs(hours_difference(1800.0, 3600.0) - 0.5) < DBL_EPSILON);
-	assert(fabs(hours_difference(3600.0, 1800.0) + 0.5) < DBL_EPSILON);
-	assert(fabs(hours_difference(1800.0, 2160.0) - 0.1) < DBL_EPSILON);
-	assert(hours_difference(1800.0, 1800.0) < DBL_EPSILON);
+    /* hours_difference */
+    assert(fabs(hours_difference(1800.0, 3600.0) - 0.5) < DBL_EPSILON);
+    assert(fabs(hours_difference(3600.0, 1800.0) + 0.5) < DBL_EPSILON);
+    assert(fabs(hours_difference(1800.0, 2160.0) - 0.1) < DBL_EPSILON);
+    assert(hours_difference(1800.0, 1800.0) < DBL_EPSILON);
+	
+    /*to_float_hours*/
+    assert(fabs(to_float_hours(0, 15, 0) - 0.25) < DBL_EPSILON);
+    assert(fabs(to_float_hours(2, 45, 9) - 2.7525) < DBL_EPSILON);
+    assert(fabs(to_float_hours(1, 0, 36) - 1.01) < DBL_EPSILON);
 }
