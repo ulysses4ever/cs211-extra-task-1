037aecff2336de50bdc347c18fd929c1e03a1bcc
Beardier <vayanow@gmail.com>
2016-09-23 17:43:34 +0300

    Function to_float_hours is ready

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -48,6 +48,7 @@ double hours_difference(double time_1, double time_2)
 
 double to_float_hours(int hours, int minutes, int seconds)
 {
+	return hours + minutes*1.0 / 60 + seconds*1.0 / 3600;
 
     /*
         Return the total number of hours in the specified number
