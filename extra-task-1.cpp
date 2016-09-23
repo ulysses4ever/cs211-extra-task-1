31cb7dd4c3941c5f2a13581dd6d4869e6c16cdc4
Beardier <vayanow@gmail.com>
2016-09-23 18:24:02 +0300

    Function time_from_utc is ready!

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -172,6 +172,7 @@ double time_to_utc(int utc_offset, double time)
 
 double time_from_utc(int utc_offset, double time)
 {
+	return to_24_hour_clock(time + utc_offset);
     /*
         Return UTC time in time zone utc_offset.
 
