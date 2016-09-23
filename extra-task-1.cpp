6e9bdd5e8cb1a49ce7bebde8a705e7c4cd66ba72
Beardier <vayanow@gmail.com>
2016-09-23 17:40:33 +0300

    Function seconds_difference is ready!

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -1,6 +1,11 @@
+#include <iostream>
+#include <cassert>
+
+using namespace std;
+
 double seconds_difference(double time_1, double time_2)
 {
-    // your implementation goes here...
+	return time_2 - time_1;
     
     /*    
         Return the number of seconds later that a time in seconds
@@ -22,6 +27,7 @@ double seconds_difference(double time_1, double time_2)
 
 double hours_difference(double time_1, double time_2)
 {
+
     /*
         Return the number of hours later that a time in seconds
         time_2 is than a time in seconds time_1.
@@ -42,6 +48,7 @@ double hours_difference(double time_1, double time_2)
 
 double to_float_hours(int hours, int minutes, int seconds)
 {
+
     /*
         Return the total number of hours in the specified number
         of hours, minutes, and seconds.
@@ -61,6 +68,7 @@ double to_float_hours(int hours, int minutes, int seconds)
 
 double to_24_hour_clock(double hours)
 {
+
     /*
         hours is a number of hours since midnight. Return the
         hour as seen on a 24-hour clock.
