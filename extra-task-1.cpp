c3a9c73b5107bf6d6f39494c143cbafd1f1f527f
vanillaflame <deadgirlneversayno@gmail.com>
2016-09-23 20:41:36 +0300

    function time_from_utc complete. Function time_to_utc fixed

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -68,39 +68,17 @@ int get_seconds(int seconds) {
 //UTC + 0.
 double time_to_utc(int utc_offset, double time)
 {
-	return fmod(time - utc_offset, 24);
+	double curr_time = time - utc_offset;
+	return curr_time > 0 ? fmod(curr_time, 24) : curr_time + 24.0;
 }
-//
-//double time_from_utc(int utc_offset, double time)
-//{
-//    /*
-//        Return UTC time in time zone utc_offset.
-//
-//        >>> time_from_utc(+0, 12.0)
-//        12.0
-// 
-//        >>> time_from_utc(+1, 12.0)
-//        13.0
-// 
-//        >>> time_from_utc(-1, 12.0)
-//        11.0
-// 
-//        >>> time_from_utc(+6, 6.0)
-//        12.0
-// 
-//        >>> time_from_utc(-7, 6.0)
-//        23.0
-// 
-//        >>> time_from_utc(-1, 0.0)
-//        23.0
-// 
-//        >>> time_from_utc(-1, 23.0)
-//        22.0
-// 
-//        >>> time_from_utc(+1, 23.0)
-//        0.0
-//    */
-//}
+
+
+//Return UTC time in time zone utc_offset.
+double time_from_utc(int utc_offset, double time)
+{
+	double curr_time = time + utc_offset;
+	return curr_time > 0 ? fmod(curr_time, 24) : curr_time + 24.0;
+}
 
 
 int main() {
@@ -140,4 +118,14 @@ int main() {
 	assert(fabs(time_to_utc(-11, 18.0) - 5.0) < DBL_EPSILON);
 	assert(fabs(time_to_utc(-1, 0.0) - 1.0) < DBL_EPSILON);
 	assert(fabs(time_to_utc(-1, 23.0)) < DBL_EPSILON);
+
+//----------------Test time_from_utc-----------------------------
+	assert(fabs(time_from_utc(+0, 12.0) - 12.0) < DBL_EPSILON);
+	assert(fabs(time_from_utc(+1, 12.0) - 13.0) < DBL_EPSILON);
+	assert(fabs(time_from_utc(-1, 12.0) - 11.0) < DBL_EPSILON);
+	assert(fabs(time_from_utc(+6, 6.0) - 12.0) < DBL_EPSILON);
+	assert(fabs(time_from_utc(-7, 6.0) - 23.0) < DBL_EPSILON);
+	assert(fabs(time_from_utc(-1, 0.0) - 23.0) < DBL_EPSILON);
+	assert(fabs(time_from_utc(-1, 23.0) - 22.0) < DBL_EPSILON);
+	assert(fabs(time_from_utc(+1, 23.0)) < DBL_EPSILON);
 }
