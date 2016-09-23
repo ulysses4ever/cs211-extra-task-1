7a1d4f3b174d41287c5de3725f5c14c80ebf54fa
vanillaflame <deadgirlneversayno@gmail.com>
2016-09-23 19:53:26 +0300

    function to_24_hour_clock complete

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -1,5 +1,5 @@
 #include <cassert>
-
+#include <cmath>
 
 //Return the number of seconds later that a time in seconds
 //time_2 is than a time in seconds time_1.
@@ -24,35 +24,15 @@ double to_float_hours(int hours, int minutes, int seconds)
 	assert(0 <= minutes && minutes < 60 && 0 <= seconds && seconds < 60);
 	return hours + minutes / 60.0 + seconds / 3600.0;
 }
-//
-//double to_24_hour_clock(double hours)
-//{
-//    /*
-//        hours is a number of hours since midnight. Return the
-//        hour as seen on a 24-hour clock.
-//
-//        Precondition: hours >= 0
-//
-//        >>> to_24_hour_clock(24)
-//        0
-//        
-//        >>> to_24_hour_clock(48)
-//        0
-//        
-//        >>> to_24_hour_clock(25)
-//        1
-//        
-//        >>> to_24_hour_clock(4)
-//        4
-//        
-//        >>> to_24_hour_clock(28.5)
-//        4.5
-//        
-//        You may wish to inspect various function in <cmath> to work
-//        with integer and fractional part of a hours separately.
-//        
-//    */
-//}
+
+
+//hours is a number of hours since midnight. return the
+//hour as seen on a 24 - hour clock.
+double to_24_hour_clock(double hours)
+{
+	assert(hours >= 0);
+	return fmod(hours, 24);
+}
 //
 ///*
 //    Implement three functions
@@ -149,5 +129,11 @@ int main() {
 	assert(to_float_hours(0, 15, 0) == 0.25);
 	assert(to_float_hours(2, 45, 9) == 2.7525);
 	assert(to_float_hours(1, 0, 36) == 1.01);
+
+	assert(to_24_hour_clock(24) == 0);
+	assert(to_24_hour_clock(48) == 0);
+	assert(to_24_hour_clock(25) == 1);
+	assert(to_24_hour_clock(4) == 4);
+	assert(to_24_hour_clock(28.5) == 4.5);
 
 }
