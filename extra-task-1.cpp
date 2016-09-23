4db4d7f2bce6f784bd294bc6e81f207b8b34af99
Beardier <vayanow@gmail.com>
2016-09-23 17:41:52 +0300

    Function hours_difference is ready!

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -27,7 +27,7 @@ double seconds_difference(double time_1, double time_2)
 
 double hours_difference(double time_1, double time_2)
 {
-
+	return (time_2 - time_1) / 3600;
     /*
         Return the number of hours later that a time in seconds
         time_2 is than a time in seconds time_1.
