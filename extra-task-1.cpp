e30659cf58475dbc0f25e334f41b28be7db8093a
numumotik <numumotik@mail.ru>
2016-09-23 21:50:52 +0300

    hours_difference

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -9,25 +9,12 @@ double seconds_difference(double time_1, double time_2)
 	return time_2 - time_1;
 }
 
-/*double hours_difference(double time_1, double time_2)
+double hours_difference(double time_1, double time_2)
 {
-    
-        Return the number of hours later that a time in seconds
-        time_2 is than a time in seconds time_1.
-            
-        >>> hours_difference(1800.0, 3600.0)
-        0.5
-
-        >>> hours_difference(3600.0, 1800.0)
-        -0.5
-
-        >>> hours_difference(1800.0, 2160.0)
-        0.1
-
-        >>> hours_difference(1800.0, 1800.0)
-        0.0
-    
-}*/
+        /*Return the number of hours later that a time in seconds
+	time_2 is than a time in seconds time_1.*/
+	return seconds_difference(time_1, time_2) / 3600;
+}
 
  /*double to_float_hours(int hours, int minutes, int seconds)
 {
@@ -163,4 +150,10 @@ int main()
 	assert(fabs(seconds_difference(3600.0, 1800.0) + 1800.0) < DBL_EPSILON);
 	assert(fabs(seconds_difference(1800.0, 2160.0) - 360.0) < DBL_EPSILON);
 	assert(fabs(seconds_difference(1800.0, 1800.0) - 0.0) < DBL_EPSILON);
+	
+	/* hours_difference */
+	assert(fabs(hours_difference(1800.0, 3600.0) - 0.5) < DBL_EPSILON);
+	assert(fabs(hours_difference(3600.0, 1800.0) + 0.5) < DBL_EPSILON);
+	assert(fabs(hours_difference(1800.0, 2160.0) - 0.1) < DBL_EPSILON);
+	assert(hours_difference(1800.0, 1800.0) < DBL_EPSILON);
 }
