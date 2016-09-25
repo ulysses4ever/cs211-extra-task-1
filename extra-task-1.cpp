46516b2463d2054271a4e856833932a57e3613cc
admin <rmansorokin@gmail.com>
2016-09-25 21:34:52 +0300

    DBL_EPS replaced with DBL_EPSILON

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -210,40 +210,39 @@ double time_from_utc(int utc_offset, double time)
         0.0
     */
 }
-const double DBL_EPS{ 0.00001 };
 
 int main()
 {
 	//seconds_difference tests
-	assert(fabs(seconds_difference(1800.0, 3600.0) - 1800.0) < DBL_EPS);
-	assert(fabs(seconds_difference(3600.0, 1800.0) + 1800.0) < DBL_EPS);
-	assert(fabs(seconds_difference(1800.0, 2160.0) - 360.0) < DBL_EPS);
-	assert(fabs(seconds_difference(1800.0, 1800.0)) < DBL_EPS);
+	assert(fabs(seconds_difference(1800.0, 3600.0) - 1800.0) < DBL_EPSILON);
+	assert(fabs(seconds_difference(3600.0, 1800.0) + 1800.0) < DBL_EPSILON);
+	assert(fabs(seconds_difference(1800.0, 2160.0) - 360.0) < DBL_EPSILON);
+	assert(fabs(seconds_difference(1800.0, 1800.0)) < DBL_EPSILON);
 
 	cout << "seconds_difference tests are completed succesfully" << endl;
 
 	//hours_difference tests
-	assert(fabs(hours_difference(1800.0, 3600.0) - 0.5) < DBL_EPS);
-	assert(fabs(hours_difference(3600.0, 1800.0) + 0.5) < DBL_EPS);
-	assert(fabs(hours_difference(1800.0, 2160.0) - 0.1) < DBL_EPS);
-	assert(fabs(hours_difference(1800.0, 1800.0)) < DBL_EPS);
+	assert(fabs(hours_difference(1800.0, 3600.0) - 0.5) < DBL_EPSILON);
+	assert(fabs(hours_difference(3600.0, 1800.0) + 0.5) < DBL_EPSILON);
+	assert(fabs(hours_difference(1800.0, 2160.0) - 0.1) < DBL_EPSILON);
+	assert(fabs(hours_difference(1800.0, 1800.0)) < DBL_EPSILON);
 
 	cout << "hours_difference tests are completed succesfully" << endl;
 
 	//to_float_hours tests
-	assert(fabs(to_float_hours(0, 15, 0) - 0.25) < DBL_EPS);
-	assert(fabs(to_float_hours(2, 45, 9) - 2.7525) < DBL_EPS);
-	assert(fabs(to_float_hours(1, 0, 36) - 1.01) < DBL_EPS);
+	assert(fabs(to_float_hours(0, 15, 0) - 0.25) < DBL_EPSILON);
+	assert(fabs(to_float_hours(2, 45, 9) - 2.7525) < DBL_EPSILON);
+	assert(fabs(to_float_hours(1, 0, 36) - 1.01) < DBL_EPSILON);
 
 	cout << "to_float_hours tests are completed succesfully" << endl;
 
 
 	//to_24_hour_clock tests
-	assert(fabs(to_24_hour_clock(24)) < DBL_EPS);
-	assert(fabs(to_24_hour_clock(48)) < DBL_EPS);
-	assert(fabs(to_24_hour_clock(25)-1) < DBL_EPS);
-	assert(fabs(to_24_hour_clock(4)-4) < DBL_EPS);
-	assert(fabs(to_24_hour_clock(28.5)-4.5) < DBL_EPS);
+	assert(fabs(to_24_hour_clock(24)) < DBL_EPSILON);
+	assert(fabs(to_24_hour_clock(48)) < DBL_EPSILON);
+	assert(fabs(to_24_hour_clock(25)-1) < DBL_EPSILON);
+	assert(fabs(to_24_hour_clock(4)-4) < DBL_EPSILON);
+	assert(fabs(to_24_hour_clock(28.5)-4.5) < DBL_EPSILON);
 
 	cout << "to_24_hour_clock tests are completed succesfully" << endl;
 
@@ -254,29 +253,29 @@ int main()
 	/*"get_hours" and "get_minutes" tests do not use the "DBL_EPS" constant because
 	the type of their returning values is integer*/
 	
-	assert(fabs(get_seconds(3800) - 20) < DBL_EPS);
+	assert(fabs(get_seconds(3800) - 20) < DBL_EPSILON);
 
 	cout << "get_hours, get_minutes and get_seconds tests are completed succesfully" << endl;
 
 	//time_to_utc tests
-	assert(fabs(time_to_utc(+0, 12.0) - 12) < DBL_EPS);
-	assert(fabs(time_to_utc(+1, 12.0) - 11) < DBL_EPS);
-	assert(fabs(time_to_utc(-1, 12.0) - 13) < DBL_EPS);
-	assert(fabs(time_to_utc(+-11, 18.0) - 5) < DBL_EPS);
-	assert(fabs(time_to_utc(-1, 0.0) - 1) < DBL_EPS);
-	assert(fabs(time_to_utc(-1, 23.0)) < DBL_EPS);
+	assert(fabs(time_to_utc(+0, 12.0) - 12) < DBL_EPSILON);
+	assert(fabs(time_to_utc(+1, 12.0) - 11) < DBL_EPSILON);
+	assert(fabs(time_to_utc(-1, 12.0) - 13) < DBL_EPSILON);
+	assert(fabs(time_to_utc(+-11, 18.0) - 5) < DBL_EPSILON);
+	assert(fabs(time_to_utc(-1, 0.0) - 1) < DBL_EPSILON);
+	assert(fabs(time_to_utc(-1, 23.0)) < DBL_EPSILON);
 
 	cout << "time_to_utc tests are completed succesfully" << endl;
 
 	//time_from_utc tests
-	assert(fabs(time_from_utc(+0, 12.0) - 12.0) < DBL_EPS);
-	assert(fabs(time_from_utc(+1, 12.0) - 13.0) < DBL_EPS);
-	assert(fabs(time_from_utc(-1, 12.0) - 11.0) < DBL_EPS);
-	assert(fabs(time_from_utc(+6, 6.0) - 12.0) < DBL_EPS);
-	assert(fabs(time_from_utc(-7, 6.0) - 23.0) < DBL_EPS);
-	assert(fabs(time_from_utc(-1, 0.0) - 23.0) < DBL_EPS);
-	assert(fabs(time_from_utc(-1, 23.0) - 22.0) < DBL_EPS);
-	assert(fabs(time_from_utc(+1, 23.0)) < DBL_EPS);
+	assert(fabs(time_from_utc(+0, 12.0) - 12.0) < DBL_EPSILON);
+	assert(fabs(time_from_utc(+1, 12.0) - 13.0) < DBL_EPSILON);
+	assert(fabs(time_from_utc(-1, 12.0) - 11.0) < DBL_EPSILON);
+	assert(fabs(time_from_utc(+6, 6.0) - 12.0) < DBL_EPSILON);
+	assert(fabs(time_from_utc(-7, 6.0) - 23.0) < DBL_EPSILON);
+	assert(fabs(time_from_utc(-1, 0.0) - 23.0) < DBL_EPSILON);
+	assert(fabs(time_from_utc(-1, 23.0) - 22.0) < DBL_EPSILON);
+	assert(fabs(time_from_utc(+1, 23.0)) < DBL_EPSILON);
 
 	cout << "time_from_utc tests are completed succesfully" << endl;
 }
