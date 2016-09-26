0dde0c172356ffd33a2db0a6aad733349c6b91ca
BlueMadWolf <sasha_kulishova@mail.ru>
2016-09-26 12:24:17 +0300

    time_to_utc

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -51,34 +51,12 @@ int get_seconds(int time)
 	return time % 60;
 }
 
-
-/*double time_to_utc(int utc_offset, double time)
+//	Return time at UTC+0, where utc_offset is the number of hours away from UTC + 0.
+double time_to_utc(int utc_offset, double time)
 {
-    /*
-        Return time at UTC+0, where utc_offset is the number of hours away from
-        UTC+0.
-        You may be interested in:
-        https://en.wikipedia.org/wiki/Coordinated_Universal_Time
-
-        >>> time_to_utc(+0, 12.0)
-        12.0
- 
-        >>> time_to_utc(+1, 12.0)
-        11.0
- 
-        >>> time_to_utc(-1, 12.0)
-        13.0
- 
-        >>> time_to_utc(-11, 18.0)
-        5.0
- 
-        >>> time_to_utc(-1, 0.0)
-        1.0
- 
-        >>> time_to_utc(-1, 23.0)
-        0.0
-    */
-//}
+	int x = ceil(time); //используется, чтобы сохранить дробную часть
+	return (x - utc_offset) % 24 + time - x;
+}
 
 /*double time_from_utc(int utc_offset, double time)
 {
@@ -142,7 +120,11 @@ int main()
 	assert((get_minutes(3800) - 3) < DBL_EPSILON && "test-18");
 	assert((get_seconds(3800) - 120) < DBL_EPSILON && "test-19");
 
-
-
-
+	//проверяет работу time_to_utc
+	assert((time_to_utc(+0, 12.0) - 12) < DBL_EPSILON && "test-20");
+	assert((time_to_utc(+1, 12.0) - 11) < DBL_EPSILON && "test-21");
+	assert((time_to_utc(-1, 12.0) - 13) < DBL_EPSILON && "test-22");
+	assert((time_to_utc(-11, 18.0) - 5) < DBL_EPSILON && "test-23");
+	assert((time_to_utc(-1, 0.0) - 1) < DBL_EPSILON && "test-24");
+	assert((time_to_utc(-1, 23.0) - 0.0) < DBL_EPSILON && "test-25");
 }
