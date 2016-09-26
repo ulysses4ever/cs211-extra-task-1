72c2b07516f35e2de41640d68548a1a0388d7e44
NoName19871 <smersh197@mail.ru>
2016-09-26 14:16:00 +0300

    Реализована функция 'to_24_hour_clock' и тесты для нее

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -23,35 +23,18 @@ double to_float_hours(int hours, int minutes, int seconds)
 	return hours + minutes / 60.0 + seconds / 3600.0;
 }
 
-//double to_24_hour_clock(double hours)
-//{
-//    /*
-//        hours is a number of hours since midnight. Return the
-//        hour as seen on a 24-hour clock.
-//
-//        Precondition: hours >= 0
-//
-//        >>> to_24_hour_clock(24)
-//        0
-//        
-//        >>> to_24_hour_clock(48)
-//        0
-//        
-//        >>> to_24_hour_clock(25)
-//        1
-//        
-//        >>> to_24_hour_clock(4)
-//        4
-//        
-//        >>> to_24_hour_clock(28.5)
-//        4.5
-//        
-//        You may wish to inspect various function in <cmath> to work
-//        with integer and fractional part of a hours separately.
-//        
-//    */
-//}
-//
+///Return the hour as seen on a 24-hour clock.
+double to_24_hour_clock(double hours)
+{
+	assert(hours >= 0);
+
+	double x,y;
+	y = modf(hours, &x);
+	int n = x;
+
+	return (n % 24) + y;
+}
+
 ///*
 //    Implement three functions
 //        * get_hours
@@ -133,6 +116,13 @@ int main()
 	assert(fabs(to_float_hours(1, 0, 36) - 1.01)    <= DBL_EPSILON && "Test3 'to_float_hours'");
 	assert(fabs(to_float_hours(20, 15, 36) - 20.26) <= DBL_EPSILON && "Test4 'to_float_hours'");
 	assert(fabs(to_float_hours(10, 30, 0) - 10.5)   <= DBL_EPSILON && "Test5 'to_float_hours'");
+
+	//Tests for 'to_24_hour_clock' function
+	assert(fabs(to_24_hour_clock(24) - 0.0)   <= DBL_EPSILON && "Test1 'to_24_hour_clock'");
+	assert(fabs(to_24_hour_clock(48) - 0.0)   <= DBL_EPSILON && "Test2 'to_24_hour_clock'");
+	assert(fabs(to_24_hour_clock(25) - 1.0)   <= DBL_EPSILON && "Test3 'to_24_hour_clock'");
+	assert(fabs(to_24_hour_clock(4) - 4.0)    <= DBL_EPSILON && "Test4 'to_24_hour_clock'");
+	assert(fabs(to_24_hour_clock(28.5) - 4.5) <= DBL_EPSILON && "Test5 'to_24_hour_clock'");
 
 	//Tests for 'time_from_utc' function
 	assert(fabs(time_from_utc(0, 12.0) - 12.0)  <= DBL_EPSILON  && "Test 1 'time_from_utc'");
