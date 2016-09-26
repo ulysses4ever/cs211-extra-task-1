cb22a253e98b5478c26ab7d492cbcce0837f19b5
BlueMadWolf <sasha_kulishova@mail.ru>
2016-09-26 12:12:04 +0300

    hours_difference

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -10,25 +10,14 @@ double seconds_difference(double time_1, double time_2)
 	return time_2 - time_1;
 }
 
-/*double hours_difference(double time_1, double time_2)
+//Return the number of hours later that a time in seconds
+//time_2 is than a time in seconds time_1.
+double hours_difference(double time_1, double time_2)
 {
-    /*
-        Return the number of hours later that a time in seconds
-        time_2 is than a time in seconds time_1.
-            
-        >>> hours_difference(1800.0, 3600.0)
-        0.5
+		return (time_2 - time_1) / 3600;
+}
 
-        >>> hours_difference(3600.0, 1800.0)
-        -0.5
-
-        >>> hours_difference(1800.0, 2160.0)
-        0.1
-
-        >>> hours_difference(1800.0, 1800.0)
-        0.0
-    */
-//}
+       
 
 /*double to_float_hours(int hours, int minutes, int seconds)
 {
@@ -165,5 +154,11 @@ int main()
 	assert((seconds_difference(3600.0, 1800.0) + 1800) < DBL_EPSILON && "test-2");
 	assert((seconds_difference(1800.0, 2160.0) - 360) < DBL_EPSILON && "test-3");
 	assert(seconds_difference(1800.0, 1800.0) < DBL_EPSILON && "test-4");
+
+	//проверяет работу hours_difference
+	assert((hours_difference(1800.0, 3600.0) - 0.5) < DBL_EPSILON && "test-5");
+	assert((hours_difference(3600.0, 1800.0) + 0.5) < DBL_EPSILON && "test-6");
+	assert((hours_difference(1800.0, 2160.0) - 0.1) < DBL_EPSILON && "test-7");
+	assert(hours_difference(1800.0, 1800.0) < DBL_EPSILON && "test-8");
 
 }
