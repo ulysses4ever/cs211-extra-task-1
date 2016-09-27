9f63eb3cf918a61b2d3991cd2232d1e658d0591e
Admin <Admin>
2016-09-27 21:51:40 +0300

    all tests were upd

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -102,49 +102,49 @@ double time_from_utc(int utc_offset, double time)
 
 int main(){
         //seconds_difference
-        assert(fabs(seconds_difference(1800.0, 3600.0) - 1800.0)  < DBL_EPS);
-        assert(fabs(seconds_difference(3600.0, 1800.0) + 1800.0)  < DBL_EPS);
-        assert(fabs(seconds_difference(1800.0, 2160.0) -  360.0)  < DBL_EPS);
-        assert(fabs(seconds_difference(1800.0, 1800.0)         )  < DBL_EPS);
+        assert(fabs(seconds_difference(1800.0, 3600.0) - 1800.0)  < DBL_EPS && "test-1");
+        assert(fabs(seconds_difference(3600.0, 1800.0) + 1800.0)  < DBL_EPS && "test-2");
+        assert(fabs(seconds_difference(1800.0, 2160.0) -  360.0)  < DBL_EPS && "test-3");
+        assert(fabs(seconds_difference(1800.0, 1800.0)         )  < DBL_EPS&& "test-4");
 
         //hours_difference
-        assert(fabs(hours_difference(1800.0, 3600.0) - 0.5)  < DBL_EPS);
-        assert(fabs(hours_difference(3600.0, 1800.0) + 0.5)  < DBL_EPS);
-        assert(fabs(hours_difference(1800.0, 2160.0) - 0.1)  < DBL_EPS);
-        assert(fabs(hours_difference(1800.0, 1800.0)      )  < DBL_EPS);
+        assert(fabs(hours_difference(1800.0, 3600.0) - 0.5)  < DBL_EPS && "test-5");
+        assert(fabs(hours_difference(3600.0, 1800.0) + 0.5)  < DBL_EPS && "test-6");
+        assert(fabs(hours_difference(1800.0, 2160.0) - 0.1)  < DBL_EPS && "test-7");
+        assert(fabs(hours_difference(1800.0, 1800.0)      )  < DBL_EPS && "test-8");
 
         //to_float_hours
-        assert(fabs(to_float_hours(0, 15, 0) -  0.25) < DBL_EPS);
-        assert(fabs(to_float_hours(2, 45, 9) -  2.7525) < DBL_EPS);
-        assert(fabs(to_float_hours(1, 0, 36) -  1.01) < DBL_EPS);
+        assert(fabs(to_float_hours(0, 15, 0) -  0.25) < DBL_EPS && "test-9");
+        assert(fabs(to_float_hours(2, 45, 9) -  2.7525) < DBL_EPS && "test-10");
+        assert(fabs(to_float_hours(1, 0, 36) -  1.01) < DBL_EPS && "test-11");
 
         //to_24_hour_clock
-        assert(fabs(to_24_hour_clock(24)) < DBL_EPS);
-        assert(fabs(to_24_hour_clock(48)) < DBL_EPS);
-        assert(fabs(to_24_hour_clock(25) - 1) < DBL_EPS);
-        assert(fabs(to_24_hour_clock(4) - 4) < DBL_EPS);
-        assert(fabs(to_24_hour_clock(28.5) - 4.5) < DBL_EPS);
+        assert(fabs(to_24_hour_clock(24)) < DBL_EPS && "test-12");
+        assert(fabs(to_24_hour_clock(48)) < DBL_EPS && "test-13");
+        assert(fabs(to_24_hour_clock(25) - 1) < DBL_EPS && "test-14");
+        assert(fabs(to_24_hour_clock(4) - 4) < DBL_EPS && "test-15");
+        assert(fabs(to_24_hour_clock(28.5) - 4.5) < DBL_EPS && "test-16");
 
         //get_hours , minutes, seconds
-        assert(1 == get_hours(3800));
-        assert(3 == get_minutes(3800));
-        assert(20 == get_seconds(3800));
+        assert(1 == get_hours(3800) && "test-17");
+        assert(3 == get_minutes(3800) && "test-18");
+        assert(20 == get_seconds(3800) && "test-19");
 
         //time_to_utc
-        assert(fabs(time_to_utc(+0, 12.0) -  12.0) < DBL_EPS);
-        assert(fabs(time_to_utc(+1, 12.0) -  11.0) < DBL_EPS);
-        assert(fabs(time_to_utc(-1, 12.0) -  13.0) < DBL_EPS);
-        assert(fabs(time_to_utc(-11, 18.0) -  5.0) < DBL_EPS);
-        assert(fabs(time_to_utc(-1, 0.0) -  1.0) < DBL_EPS);
-        assert(fabs(time_to_utc(-1, 23.0)       ) < DBL_EPS);
+        assert(fabs(time_to_utc(+0, 12.0) -  12.0) < DBL_EPS && "test-20");
+        assert(fabs(time_to_utc(+1, 12.0) -  11.0) < DBL_EPS && "test-21");
+        assert(fabs(time_to_utc(-1, 12.0) -  13.0) < DBL_EPS && "test-22");
+        assert(fabs(time_to_utc(-11, 18.0) -  5.0) < DBL_EPS && "test-23");
+        assert(fabs(time_to_utc(-1, 0.0) -  1.0) < DBL_EPS && "test-24");
+        assert(fabs(time_to_utc(-1, 23.0)       ) < DBL_EPS && "test-25");
 
         //time_from_utc
-        assert(fabs(time_from_utc(+0, 12.0) -  12.0) < DBL_EPS);
-        assert(fabs(time_from_utc(+1, 12.0) -  13.0) < DBL_EPS);
-        assert(fabs(time_from_utc(-1, 12.0) -  11.0) < DBL_EPS);
-        assert(fabs(time_from_utc(+6, 6.0) -  12.0) < DBL_EPS);
-        assert(fabs(time_from_utc(-7, 6.0) -  23.0) < DBL_EPS);
-        assert(fabs(time_from_utc(-1, 0.0) -  23.0) < DBL_EPS);
-        assert(fabs(time_from_utc(-1, 23.0) -  22.0) < DBL_EPS);
-        assert(fabs(time_from_utc(+1, 23.0) -  0.0) < DBL_EPS);
+        assert(fabs(time_from_utc(+0, 12.0) -  12.0) < DBL_EPS && "test-26");
+        assert(fabs(time_from_utc(+1, 12.0) -  13.0) < DBL_EPS && "test-27");
+        assert(fabs(time_from_utc(-1, 12.0) -  11.0) < DBL_EPS && "test-28");
+        assert(fabs(time_from_utc(+6, 6.0) -  12.0) < DBL_EPS && "test-29");
+        assert(fabs(time_from_utc(-7, 6.0) -  23.0) < DBL_EPS && "test-30");
+        assert(fabs(time_from_utc(-1, 0.0) -  23.0) < DBL_EPS && "test-31");
+        assert(fabs(time_from_utc(-1, 23.0) -  22.0) < DBL_EPS && "test-32");
+        assert(fabs(time_from_utc(+1, 23.0)         ) < DBL_EPS && "test-33");
 }
