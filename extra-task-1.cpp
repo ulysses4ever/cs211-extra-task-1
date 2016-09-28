8ff475047d4744bd9e9207295f7bcae3a3a8b9ab
Николай Моздоров < >
2016-09-28 22:40:54 +0300

    function hours_difference is done

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -11,24 +11,11 @@ double seconds_difference(double time_1, double time_2)
     return time_2 - time_1;
 }
 
+// Returns the number of hours later that a time in seconds
+// time_2 is than a time in seconds time_1.
 double hours_difference(double time_1, double time_2)
 {
-    /*
-        Return the number of hours later that a time in seconds
-        time_2 is than a time in seconds time_1.
-            
-        >>> hours_difference(1800.0, 3600.0)
-        0.5
-
-        >>> hours_difference(3600.0, 1800.0)
-        -0.5
-
-        >>> hours_difference(1800.0, 2160.0)
-        0.1
-
-        >>> hours_difference(1800.0, 1800.0)
-        0.0
-    */
+	return seconds_difference(time_1, time_2) / 3600;
 }
 
 double to_float_hours(int hours, int minutes, int seconds)
@@ -172,4 +159,10 @@ int main()
 	assert(are_equal(seconds_difference(3600.0, 1800.0), -1800.0));
 	assert(are_equal(seconds_difference(1800.0, 2160.0), 360.0));
 	assert(are_equal(seconds_difference(1800.0, 1800.0), 0.0));
+	
+	// testing hours_difference
+	assert(are_equal(hours_difference(1800.0, 3600.0), 0.5));
+	assert(are_equal(hours_difference(3600.0, 1800.0), -0.5));
+	assert(are_equal(hours_difference(1800.0, 2160.0), 0.1));
+	assert(are_equal(hours_difference(1800.0, 1800.0), 0.0));
 }
