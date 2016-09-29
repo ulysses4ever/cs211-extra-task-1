a77c4b4f4b0e48377520e33c526df4b946810d29
Dpokova <daria.ol.p@gmail.com>
2016-09-29 17:55:20 +0300

    реализация и тесты для to_float_hours

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -21,25 +21,13 @@ double hours_difference(double time_1, double time_2)
 
 }
 
+/* Return the total number of hours in the specified number
+of hours, minutes, and seconds. */
 double to_float_hours(int hours, int minutes, int seconds)
 {
-    /*
-        Return the total number of hours in the specified number
-        of hours, minutes, and seconds.
-
-        Precondition: 0 <= minutes < 60  and  0 <= seconds < 60
-
-        >>> to_float_hours(0, 15, 0)
-        0.25
-
-        >>> to_float_hours(2, 45, 9)
-        2.7525
-
-        >>> to_float_hours(1, 0, 36)
-        1.01
-    */
-	return(0);
+	return ((hours * 3600.0 + minutes * 60.0 + seconds) / 3600);
 }
+
 
 double to_24_hour_clock(double hours)
 {
@@ -170,4 +158,10 @@ int main()
 	assert(abs(hours_difference(1800.0, 2160.0) - 0.1) < DBL_EPSILON);
 	assert(abs(hours_difference(1800.0, 1800.0) - 0.0) < DBL_EPSILON);
 	cout << "Tests for hours_difference were successfully passed" << endl;
+
+	// to_float_hours
+	assert(abs(to_float_hours(0, 15, 0) - 0.25) < DBL_EPSILON);
+	assert(abs(to_float_hours(2, 45, 9) - 2.7525) < DBL_EPSILON);
+	assert(abs(to_float_hours(1, 0, 36) - 1.01) < DBL_EPSILON);
+	cout << "Tests for to_float_hours were successfully passed" << endl;
 }
