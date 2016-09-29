09f0246b29f4c85686ca6e6a02cc3eea7f3d0619
Dpokova <daria.ol.p@gmail.com>
2016-09-29 17:50:11 +0300

    реализация и тетсы для seconds_difference

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -1,23 +1,14 @@
+#include <iostream>;
+#include <locale>;
+#include <cassert>;
+
+using namespace std;
+
+const double dbl_epsilon = 0.001;
+
 double seconds_difference(double time_1, double time_2)
 {
-    // your implementation goes here...
-    
-    /*    
-        Return the number of seconds later that a time in seconds
-        time_2 is than a time in seconds time_1.
-            
-        >>> seconds_difference(1800.0, 3600.0)
-        1800.0
-
-        >>> seconds_difference(3600.0, 1800.0)
-        -1800.0
-
-        >>> seconds_difference(1800.0, 2160.0)
-        360.0
-
-        >>> seconds_difference(1800.0, 1800.0)
-        0.0
-    */
+	return (time_2 - time_1);
 }
 
 double hours_difference(double time_1, double time_2)
@@ -38,6 +29,7 @@ double hours_difference(double time_1, double time_2)
         >>> hours_difference(1800.0, 1800.0)
         0.0
     */
+	return(0);
 }
 
 double to_float_hours(int hours, int minutes, int seconds)
@@ -57,6 +49,7 @@ double to_float_hours(int hours, int minutes, int seconds)
         >>> to_float_hours(1, 0, 36)
         1.01
     */
+	return(0);
 }
 
 double to_24_hour_clock(double hours)
@@ -86,6 +79,7 @@ double to_24_hour_clock(double hours)
         with integer and fractional part of a hours separately.
         
     */
+	return(0);
 }
 
 /*
@@ -135,6 +129,7 @@ double time_to_utc(int utc_offset, double time)
         >>> time_to_utc(-1, 23.0)
         0.0
     */
+	return(0);
 }
 
 double time_from_utc(int utc_offset, double time)
@@ -166,4 +161,18 @@ double time_from_utc(int utc_offset, double time)
         >>> time_from_utc(+1, 23.0)
         0.0
     */
+	return(0);
+}
+
+int main()
+{
+	std::locale::global(std::locale(""));
+
+	// seconds_difference
+	assert(abs(seconds_difference(1800.0, 3600.0) - 1800.0) < DBL_EPSILON);
+	assert(abs(seconds_difference(3600.0, 1800.0) + 1800.0) < DBL_EPSILON);
+	assert(abs(seconds_difference(1800.0, 2160.0) - 360.0) < DBL_EPSILON);
+	assert(abs(seconds_difference(1800.0, 1800.0) - 0.0) < DBL_EPSILON);
+	cout << "Tests for seconds_difference were successfully passed";
+
 }
