ffcd8a2422425bc926d51ca4a2b93cede0ba7cf5
Dpokova <daria.ol.p@gmail.com>
2016-09-29 18:35:46 +0300

    реализация и тесты для to_24_hour_clock

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -1,6 +1,7 @@
 #include <iostream>;
 #include <locale>;
 #include <cassert>;
+#include <cmath>;
 
 using namespace std;
 
@@ -28,35 +29,11 @@ double to_float_hours(int hours, int minutes, int seconds)
 	return ((hours * 3600.0 + minutes * 60.0 + seconds) / 3600);
 }
 
-
+/* hours is a number of hours since midnight. Return the hour as seen on a 24-hour clock. */
 double to_24_hour_clock(double hours)
 {
-    /*
-        hours is a number of hours since midnight. Return the
-        hour as seen on a 24-hour clock.
-
-        Precondition: hours >= 0
-
-        >>> to_24_hour_clock(24)
-        0
-        
-        >>> to_24_hour_clock(48)
-        0
-        
-        >>> to_24_hour_clock(25)
-        1
-        
-        >>> to_24_hour_clock(4)
-        4
-        
-        >>> to_24_hour_clock(28.5)
-        4.5
-        
-        You may wish to inspect various function in <cmath> to work
-        with integer and fractional part of a hours separately.
-        
-    */
-	return(0);
+	assert(hours >= 0);
+	return fmod(hours + 24.0, 24);
 }
 
 /*
@@ -164,4 +141,12 @@ int main()
 	assert(abs(to_float_hours(2, 45, 9) - 2.7525) < DBL_EPSILON);
 	assert(abs(to_float_hours(1, 0, 36) - 1.01) < DBL_EPSILON);
 	cout << "Tests for to_float_hours were successfully passed" << endl;
+
+	// to_24_hour_clock
+	assert(abs(to_24_hour_clock(24) - 0) < DBL_EPSILON);
+	assert(abs(to_24_hour_clock(48) - 0) < DBL_EPSILON);
+	assert(abs(to_24_hour_clock(25) - 1) < DBL_EPSILON);
+	assert(abs(to_24_hour_clock(4) - 4) < DBL_EPSILON);
+	assert(abs(to_24_hour_clock(28.5) - 4.5) < DBL_EPSILON);
+	cout << "Tests for to_24_hour_clock were successfully passed" << endl;
 }
