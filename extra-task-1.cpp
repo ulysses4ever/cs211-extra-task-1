0c94c70ee51bac86f6dbc36965fe0eda9f7d751a
Влада Кривенцова < >
2016-09-29 19:54:07 +0300

    More functions

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -1,91 +1,64 @@
+#include <iostream>
 #include <cassert> 
+#include <cmath>
 
+using namespace std;
+
+
+/*
+Return the number of seconds later that a time in seconds
+time_2 is than a time in seconds time_1.
+*/
 double seconds_difference(double time_1, double time_2)
 {
-	//assert();
 	return (time_2 - time_1);
-    
-    /*    
-        Return the number of seconds later that a time in seconds
-        time_2 is than a time in seconds time_1.
-            
-        >>> seconds_difference(1800.0, 3600.0)
-        1800.0
-
-        >>> seconds_difference(3600.0, 1800.0)
-        -1800.0
-
-        >>> seconds_difference(1800.0, 2160.0)
-        360.0
-
-        >>> seconds_difference(1800.0, 1800.0)
-        0.0
-    */
 }
 
 double hours_difference(double time_1, double time_2)
 {
-	return ((time_2 - time_1) / 3600);
-
+	return ((time_2 - time_1) / 3600.0);
     /*
         Return the number of hours later that a time in seconds
         time_2 is than a time in seconds time_1.
-            
-        >>> hours_difference(1800.0, 3600.0)
-        0.5
-
-        >>> hours_difference(3600.0, 1800.0)
-        -0.5
-
-        >>> hours_difference(1800.0, 2160.0)
-        0.1
 
         >>> hours_difference(1800.0, 1800.0)
         0.0
     */
 }
 
+/*
+Return the total number of hours in the specified number
+of hours, minutes, and seconds.
+*/
 double to_float_hours(int hours, int minutes, int seconds)
 {
-    /*
-        Return the total number of hours in the specified number
-        of hours, minutes, and seconds.
+	assert((minutes >= 0) && (minutes < 60));
+	assert((seconds >= 0) && (seconds < 60));
 
-        Precondition: 0 <= minutes < 60  and  0 <= seconds < 60
-
-        >>> to_float_hours(0, 15, 0)
-        0.25
-
-        >>> to_float_hours(2, 45, 9)
-        2.7525
-
-        >>> to_float_hours(1, 0, 36)
-        1.01
-    */
+	return (hours + (minutes / 60.0) + (seconds / 3600.0));
+	
 }
 
 double to_24_hour_clock(double hours)
 {
+	assert(hours >= 0);
+
+	double fractpart, intpart;
+	fractpart = modf(hours, &intpart);
+	int intpart2 = intpart; // сужающее преобразование
+
+	if (hours < 24)
+		return hours;
+	else if ((hours > 24) && (hours < 48))
+		return (hours - 24);
+	else if (intpart2 % 12 == 0)
+		return 0;
+
     /*
         hours is a number of hours since midnight. Return the
         hour as seen on a 24-hour clock.
 
-        Precondition: hours >= 0
-
-        >>> to_24_hour_clock(24)
-        0
         
-        >>> to_24_hour_clock(48)
-        0
-        
-        >>> to_24_hour_clock(25)
-        1
-        
-        >>> to_24_hour_clock(4)
-        4
-        
-        >>> to_24_hour_clock(28.5)
-        4.5
         
         You may wish to inspect various function in <cmath> to work
         with integer and fractional part of a hours separately.
@@ -116,6 +89,7 @@ double to_24_hour_clock(double hours)
 
 double time_to_utc(int utc_offset, double time)
 {
+	return 0;
     /*
         Return time at UTC+0, where utc_offset is the number of hours away from
         UTC+0.
@@ -144,6 +118,7 @@ double time_to_utc(int utc_offset, double time)
 
 double time_from_utc(int utc_offset, double time)
 {
+	return 0;
     /*
         Return UTC time in time zone utc_offset.
 
@@ -171,4 +146,80 @@ double time_from_utc(int utc_offset, double time)
         >>> time_from_utc(+1, 23.0)
         0.0
     */
+}
+
+
+int main()
+{
+	// >>> seconds_difference(1800.0, 3600.0)
+	//     1800.0
+	assert(seconds_difference(1800.0, 3600.0) == 1800.0);
+
+	// >>> seconds_difference(3600.0, 1800.0)
+	//     -1800.0
+	assert(seconds_difference(3600.0, 1800.0) == -1800.0);
+
+	// >>> seconds_difference(1800.0, 2160.0)
+	//     360.0
+	assert(seconds_difference(1800.0, 2160.0) == 360.0);
+
+	// >>> seconds_difference(1800.0, 1800.0)
+	//     0.0
+	assert(seconds_difference(1800.0, 1800.0) == 0.0);
+
+	// -------------------------------------------------
+
+	// >>> hours_difference(1800.0, 3600.0)
+	//     0.5
+	assert(hours_difference(1800.0, 3600.0) == 0.5);
+
+	// >>> hours_difference(3600.0, 1800.0)
+	//     -0.5
+	assert(hours_difference(3600.0, 1800.0) == -0.5);
+
+	// >>> hours_difference(1800.0, 2160.0)
+	//     0.1
+	assert(hours_difference(1800.0, 2160.0) == 0.1);
+
+	// >>> hours_difference(1800.0, 1800.0)
+	//     0.0
+	assert(hours_difference(1800.0, 1800.0) == 0.0);
+
+	// -------------------------------------------------
+
+	// >>> to_float_hours(0, 15, 0)
+	//     0.25
+	assert(fabs(to_float_hours(0, 15, 0) - 0.25) < DBL_EPSILON);
+
+	// >>> to_float_hours(2, 45, 9)
+	//     2.7525
+	assert(fabs(to_float_hours(2, 45, 9) - 2.7525) < DBL_EPSILON);
+
+	// >>> to_float_hours(1, 0, 36)
+	//     1.01
+	assert(fabs(to_float_hours(1, 0, 36) - 1.01) < DBL_EPSILON);
+
+	// -------------------------------------------------
+
+	// >>> to_24_hour_clock(24)
+	//     0
+	assert(to_24_hour_clock(24) == 0);
+
+	// >>> to_24_hour_clock(48)
+	//     0
+	assert(to_24_hour_clock(48) == 0);
+
+	// >>> to_24_hour_clock(25)
+	//	   1
+	assert(to_24_hour_clock(25) == 1);
+
+	// >>> to_24_hour_clock(4)
+	// 	   4
+	assert(to_24_hour_clock(4) == 4);
+
+	// >>> to_24_hour_clock(28.5)
+	//     4.5
+	assert(fabs(to_24_hour_clock(28.5) - 4.5) < DBL_EPSILON);
+
+	// -------------------------------------------------
 }
