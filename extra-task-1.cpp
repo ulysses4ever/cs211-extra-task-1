900d440f6023d8d7f2a0d80b930ba662efc91b52
Sergey <kaptch@gmail.com>
2017-09-10 23:36:42 +0300

    seconds_difference hours_difference to_float_hours

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -1,6 +1,10 @@
+int main() {
+
+}
+
 double seconds_difference(double time_1, double time_2)
 {
-    // your implementation goes here...
+    return time_2 - time_1;
     
     /*    
         Return the number of seconds later that a time in seconds
@@ -22,6 +26,7 @@ double seconds_difference(double time_1, double time_2)
 
 double hours_difference(double time_1, double time_2)
 {
+    return (seconds_difference(time_1, time_2) / 3600.0);
     /*
         Return the number of hours later that a time in seconds
         time_2 is than a time in seconds time_1.
@@ -42,6 +47,7 @@ double hours_difference(double time_1, double time_2)
 
 double to_float_hours(int hours, int minutes, int seconds)
 {
+    return (hours + minutes / 60 + seconds / 3600);
     /*
         Return the total number of hours in the specified number
         of hours, minutes, and seconds.
@@ -61,6 +67,7 @@ double to_float_hours(int hours, int minutes, int seconds)
 
 double to_24_hour_clock(double hours)
 {
+
     /*
         hours is a number of hours since midnight. Return the
         hour as seen on a 24-hour clock.
