132484132823e0ae7ffdcc62929738b84d91f252
xamely <m.xeniya.a@ya.ru>
2017-09-12 23:38:59 +0300

    Added function time_to_utc

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -49,31 +49,7 @@ int get_seconds(int time) {
 
 double time_to_utc(int utc_offset, double time)
 {
-	return 0;
-    /*
-        Return time at UTC+0, where utc_offset is the number of hours away from
-        UTC+0.
-        You may be interested in:
-        https://en.wikipedia.org/wiki/Coordinated_Universal_Time
-
-        >>> time_to_utc(+0, 12.0)
-        12.0
- 
-        >>> time_to_utc(+1, 12.0)
-        11.0
- 
-        >>> time_to_utc(-1, 12.0)
-        13.0
- 
-        >>> time_to_utc(-11, 18.0)
-        5.0
- 
-        >>> time_to_utc(-1, 0.0)
-        1.0
- 
-        >>> time_to_utc(-1, 23.0)
-        0.0
-    */
+	return (int(floor(time)) - utc_offset + 24) % 24 + time - floor(time);
 }
 
 double time_from_utc(int utc_offset, double time)
@@ -132,4 +108,29 @@ int main() {
 	assert((get_hours(3800) == 1) && "test 17");
 	assert((get_minutes(3800) == 3) && "test 18");
 	assert((get_seconds(3800) == 20) && "test 19");
+	/*
+	>>> time_to_utc(+0, 12.0)
+	12.0
+
+	>>> time_to_utc(+1, 12.0)
+	11.0
+
+	>>> time_to_utc(-1, 12.0)
+	13.0
+
+	>>> time_to_utc(-11, 18.0)
+	5.0
+
+	>>> time_to_utc(-1, 0.0)
+	1.0
+
+	>>> time_to_utc(-1, 23.0)
+	0.0
+	*/
+	assert(are_equal(time_to_utc(+0, 12.0), 12.0) && "test 20");
+	assert(are_equal(time_to_utc(+1, 12.0), 11.0) && "test 21");
+	assert(are_equal(time_to_utc(-1, 12.0), 13.0) && "test 22");
+	assert(are_equal(time_to_utc(-11, 18.0), 5.0) && "test 23");
+	assert(are_equal(time_to_utc(-1, 0.0), 1.0) && "test 24");
+	assert(are_equal(time_to_utc(-1, 23.0), 0.0) && "test 25");
 }
