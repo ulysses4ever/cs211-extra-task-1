a688ced8eed6e6022be1a7c7b639b1ccfe0c671a
Antonzo <tony.zamay@mail.ru>
2017-09-13 21:59:52 +0300

    to_float_hours

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -18,22 +18,7 @@ double hours_difference(double time_1, double time_2)
 
 double to_float_hours(int hours, int minutes, int seconds)
 {
-    /*
-        Return the total number of hours in the specified number
-        of hours, minutes, and seconds.
-
-        Precondition: 0 <= minutes < 60  and  0 <= seconds < 60
-
-        >>> to_float_hours(0, 15, 0)
-        0.25
-
-        >>> to_float_hours(2, 45, 9)
-        2.7525
-
-        >>> to_float_hours(1, 0, 36)
-        1.01
-    */
-	return 0;
+	return (hours * 3600 + minutes * 60 + seconds) / 3600.0;
 }
 
 double to_24_hour_clock(double hours)
@@ -158,4 +143,8 @@ int main() {
 	assert(RealEq(hours_difference(3600, 1800), -0.5) && "number 2");
 	assert(RealEq(hours_difference(1800, 2160), 0.1) && "number 3");
 	assert(RealEq(hours_difference(1800, 1800), 0) && "number 4");
+
+	assert(RealEq(to_float_hours(0, 15, 0), 0.25) && "number 1");
+	assert(RealEq(to_float_hours(2, 45, 9), 2.7525) && "number 2");
+	assert(RealEq(to_float_hours(1, 0, 36), 1.01) && "number 3");
 }
