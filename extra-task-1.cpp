75c31d590c440528ed4856c8b407140d5a410b7b
SokolovMichail <sokolovmisha44@gmail.com>
2017-09-13 23:27:27 +0300

    Added time_to_utc and its test block

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -135,7 +135,7 @@ int get_seconds(int seconds)
 
 double time_to_utc(int utc_offset, double time)
 {
-	return 0;
+	return double(lround(trunc(time - utc_offset))%24 + remainder(time, 1));
     /*
         Return time at UTC+0, where utc_offset is the number of hours away from
         UTC+0.
@@ -231,7 +231,16 @@ int main()
 	assert(get_hours(3800) == 1 && "get-t1");
 	assert(get_minutes(3800) == 3 && "get-t2");
 	assert(get_seconds(3800) == 20 && "get-t3");
-	
+
+	//time_to_utc test block
+	assert(fabs(time_to_utc(+0, 12.0) - 12.0) <= DBL_EPSILON && "time_to_utc-t1");
+	assert(fabs(time_to_utc(+1, 12.0) - 11.0) <= DBL_EPSILON && "time_to_utc-t2");
+	assert(fabs(time_to_utc(-1, 12.0) - 13.0) <= DBL_EPSILON && "time_to_utc-t3");
+	assert(fabs(time_to_utc(-11, 18.0) - 5.0) <= DBL_EPSILON && "time_to_utc-t4");
+	assert(fabs(time_to_utc(-1, 0.0) - 1.0) <= DBL_EPSILON && "time_to_utc-t5");
+	assert(fabs(time_to_utc(-1, 23.0) - 0.0) <= DBL_EPSILON && "time_to_utc-t6");
+
+		
 
 
 }
