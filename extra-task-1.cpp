37fbe115adb08f0097dcbeaaf9b270e7cf9ef320
SokolovMichail <sokolovmisha44@gmail.com>
2017-09-13 22:39:41 +0300

    Added function hours_difference and its test block

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -27,7 +27,7 @@ double seconds_difference(double time_1, double time_2)
 
 double hours_difference(double time_1, double time_2)
 {
-	//return (time_2-time_1)/3600.0;
+	return (time_2-time_1)/3600.0;
     /*
         Return the number of hours later that a time in seconds
         time_2 is than a time in seconds time_1.
@@ -48,6 +48,7 @@ double hours_difference(double time_1, double time_2)
 
 double to_float_hours(int hours, int minutes, int seconds)
 {
+	return 0;
 	//return hours + minutes/60.0 + seconds / 3600.0;
     /*
         Return the total number of hours in the specified number
@@ -180,7 +181,7 @@ double time_from_utc(int utc_offset, double time)
 
 int main()
 {
-	/*
+	
 	
 	//Seconds_Difference test block
 	assert(fabs(seconds_difference(1800.0, 3600.0) - 1800.0) <= DBL_EPSILON && "Seconds-diff-t1" );
@@ -188,15 +189,15 @@ int main()
 	assert(fabs(seconds_difference(1800.0, 2160.0) - 360.0) <= DBL_EPSILON && "Seconds-diff-t3");
 	assert(fabs(seconds_difference(1800.0, 1800.0) - 0.0) <= DBL_EPSILON && "Seconds-diff-t4");
 
-	*/
+	
 
-	/*
+	
 	//Hours_Difference test block
 	assert(fabs(hours_difference(1800.0, 3600.0) - 0.5) <= DBL_EPSILON && "hours-diff-t1" );
 	assert(fabs(hours_difference(3600.0, 1800.0) + 0.5) <= DBL_EPSILON && "hours-diff-t2");
 	assert(fabs(hours_difference(1800.0, 2160.0) - 0.1) <= DBL_EPSILON && "hours-diff-t3");
 	assert(fabs(hours_difference(1800.0, 1800.0) - 0.0) <= DBL_EPSILON && "hours-diff-t4");
-	*/
+	
 	/*
 	//ToFloatHours test block
 	assert(fabs(to_float_hours(0, 15, 0) - 0.25) <= DBL_EPSILON && "ToFloatHours-t1");
