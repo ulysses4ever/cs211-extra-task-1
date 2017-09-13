be613a180e94a2c71db7ef4520b27894e6b065ac
AlexFrost111 <skateone1@mail.ru>
2017-09-13 09:35:05 +0300

    to_float_hours

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -5,18 +5,6 @@ double seconds_difference(double time_1, double time_2)
     /*    
         Return the number of seconds later that a time in seconds
         time_2 is than a time in seconds time_1.
-            
-        >>> seconds_difference(1800.0, 3600.0)
-        1800.0
-
-        >>> seconds_difference(3600.0, 1800.0)
-        -1800.0
-
-        >>> seconds_difference(1800.0, 2160.0)
-        360.0
-
-        >>> seconds_difference(1800.0, 1800.0)
-        0.0
     */
 
 }
@@ -27,38 +15,17 @@ double hours_difference(double time_1, double time_2)
     /*
         Return the number of hours later that a time in seconds
         time_2 is than a time in seconds time_1.
-            
-        >>> hours_difference(1800.0, 3600.0)
-        0.5
-
-        >>> hours_difference(3600.0, 1800.0)
-        -0.5
-
-        >>> hours_difference(1800.0, 2160.0)
-        0.1
-
-        >>> hours_difference(1800.0, 1800.0)
-        0.0
     */
 }
 
 double to_float_hours(int hours, int minutes, int seconds)
 {
-	return 0;
+	return hours + minutes / 60.0 + seconds / 3600.0;
     /*
         Return the total number of hours in the specified number
         of hours, minutes, and seconds.
 
         Precondition: 0 <= minutes < 60  and  0 <= seconds < 60
-
-        >>> to_float_hours(0, 15, 0)
-        0.25
-
-        >>> to_float_hours(2, 45, 9)
-        2.7525
-
-        >>> to_float_hours(1, 0, 36)
-        1.01
     */
 }
 
@@ -192,4 +159,8 @@ int main()
 		assert(fabs(hours_difference(3600, 1800) - -0.5) < DBL_EPSILON && "test-2-2");
 		assert(fabs(hours_difference(1800, 2160) - 0.1) < DBL_EPSILON && "test-2-3");
 		assert(fabs(hours_difference(1800, 1800)) < DBL_EPSILON && "test-2-4");
+	// to_float_hours
+		assert(fabs(to_float_hours(0, 15, 0) - 0.25) < DBL_EPSILON && "test-3-1");
+		assert(fabs(to_float_hours(2, 45, 9) - 2.7525) < DBL_EPSILON && "test-3-2");
+		assert(fabs(to_float_hours(1, 0, 36) - 1.01) < DBL_EPSILON && "test-3-3");
 }
