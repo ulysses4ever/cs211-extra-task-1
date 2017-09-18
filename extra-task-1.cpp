99892ab16cd682ba1e14ab7c312addd25907c56a
Mirradell <olya.shtejn@yandex.ru>
2017-09-18 21:34:39 +0300

    Add realisation for function seconds_difference()

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -1,6 +1,11 @@
+#include<iostream>
+#include<cassert>
+using namespace std;
+const double DBL_EPSILN = 0.00001;
+
 double seconds_difference(double time_1, double time_2)
 {
-    // your implementation goes here...
+	return time_2 - time_1;
     
     /*    
         Return the number of seconds later that a time in seconds
