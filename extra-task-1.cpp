4549487c09ea396a7a45b23309f05065e3c27058
BayerGleb <gleb.bayer@live.ru>
2017-09-19 20:36:03 +0300

    hours_difference

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -24,6 +24,27 @@ double seconds_difference(double time_1, double time_2)
 	return time_2 - time_1;
 }
 
+double hours_difference(double time_1, double time_2)
+{
+	/*
+	Return the number of hours later that a time in seconds
+	time_2 is than a time in seconds time_1.
+
+	>>> hours_difference(1800.0, 3600.0)
+	0.5
+
+	>>> hours_difference(3600.0, 1800.0)
+	-0.5
+
+	>>> hours_difference(1800.0, 2160.0)
+	0.1
+
+	>>> hours_difference(1800.0, 1800.0)
+	0.0
+	*/
+	return seconds_difference(time_1, time_2) / 3600.0;
+}
+
 int main()
 {
 	//seconds_difference
@@ -31,4 +52,10 @@ int main()
 	assert(fabs(seconds_difference(3600.0, 1800.0) - -1800.0) < DBL_EPSILON);
 	assert(fabs(seconds_difference(1800.0, 2160.0) - 360.0) < DBL_EPSILON);
 	assert(fabs(seconds_difference(1800.0, 1800.0) - 0.0) < DBL_EPSILON);
+
+	//hours_difference
+	assert(fabs(hours_difference(1800.0, 3600.0) - 0.5) < DBL_EPSILON);
+	assert(fabs(hours_difference(3600.0, 1800.0) - -0.5) < DBL_EPSILON);
+	assert(fabs(hours_difference(1800.0, 2160.0) - 0.1) < DBL_EPSILON);
+	assert(fabs(hours_difference(1800.0, 1800.0) - 0.0) < DBL_EPSILON);
 }
