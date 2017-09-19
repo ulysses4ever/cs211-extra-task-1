14667bc4475ad68de4bbf4584d7861afd8be045d
Nikita-lys <lysenkonikita61@mail.ru>
2017-09-19 21:39:38 +0300

    function time_to_utc

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -43,16 +43,15 @@ double to_float_hours(int hours, int minutes, int seconds)
 // ¬озвразает час, как показано на 24 - часовых часах
 double to_24_hour_clock(double hours)
 {
-    /*
-        hours is a number of hours since midnight. Return the
-        hour as seen on a 24-hour clock.
-    */
-	assert(hours >= 0 && "to_24_hour_clock, hours >= 0")
-	while (hours < 24)
+	/*
+		hours is a number of hours since midnight. Return the
+		hour as seen on a 24-hour clock.
+	*/
+	assert(hours >= 0 && "to_24_hour_clock, hours >= 0");
+	while (hours >= 24)
 	{
 		hours -= 24;
 	}
-
 	return hours;
 }
 
@@ -77,33 +76,57 @@ double to_24_hour_clock(double hours)
     it is currently 01:03:20 (hh:mm:ss).
 */
 
+// ќпредел€ет целые часы по заданным секундам
+int get_hours(int seconds)
+{
+	int res = 0;
+	while (seconds <= 3600)
+	{
+		seconds -= 3600;
+		res += 1;
+	}
+	return res;
+}
+
+// ќпредел€ет оставшиес€ минуты по заданным секундам
+int get_minutes(int seconds)
+{
+	int res = 0;
+	while (seconds <= 3600)
+		seconds -= 3600;
+
+	while (seconds <= 60)
+	{
+		seconds -= 60;
+		res += 1;
+	}
+	return res;
+}
+
+// ќпредел€ет оставшиес€ секунды
+int get_seconds(int seconds)
+{
+	int res = 0;
+	while (seconds <= 3600)
+		seconds -= 3600;
+
+	while (seconds <= 60)
+		seconds -= 60;
+
+	return seconds;
+}
+
+// ѕереводит часы из UTC в местное
 double time_to_utc(int utc_offset, double time)
 {
     /*
         Return time at UTC+0, where utc_offset is the number of hours away from
         UTC+0.
-        You may be interested in:
-        https://en.wikipedia.org/wiki/Coordinated_Universal_Time
-
-        >>> time_to_utc(+0, 12.0)
-        12.0
- 
-        >>> time_to_utc(+1, 12.0)
-        11.0
- 
-        >>> time_to_utc(-1, 12.0)
-        13.0
- 
-        >>> time_to_utc(-11, 18.0)
-        5.0
- 
-        >>> time_to_utc(-1, 0.0)
-        1.0
- 
-        >>> time_to_utc(-1, 23.0)
-        0.0
     */
-	return 0;
+	double res = time - utc_offset;
+	if (res >= 24)
+		res -= 24;
+	return (res);
 }
 
 double time_from_utc(int utc_offset, double time)
@@ -211,35 +234,67 @@ int main()
 
 
 	//-------------------------------to_24_hour_clock-------------------------------------
-	{
-		/*
-		hours is a number of hours since midnight. Return the
-		hour as seen on a 24-hour clock.
+	/*
+	hours is a number of hours since midnight. Return the
+	hour as seen on a 24-hour clock.
 
-		Precondition: hours >= 0
+	Precondition: hours >= 0
 
-		>>> to_24_hour_clock(24)
-		0
+	>>> to_24_hour_clock(24)
+	0
 
-		>>> to_24_hour_clock(48)
-		0
+	>>> to_24_hour_clock(48)
+	0
 
-		>>> to_24_hour_clock(25)
-		1
+	>>> to_24_hour_clock(25)
+	1
 
-		>>> to_24_hour_clock(4)
-		4
+	>>> to_24_hour_clock(4)
+	4
 
-		>>> to_24_hour_clock(28.5)
-		4.5
+	>>> to_24_hour_clock(28.5)
+	4.5
 
-		You may wish to inspect various function in <cmath> to work
-		with integer and fractional part of a hours separately.
-		*/
-		assert(RealsAreEqual(to_24_hour_clock(24), 0) && "test-4.1");
-		assert(RealsAreEqual(to_24_hour_clock(48), 0) && "test-4.2");
-		assert(RealsAreEqual(to_24_hour_clock(25), 1) && "test-4.3");
-		assert(RealsAreEqual(to_24_hour_clock(4), 4) && "test-4.4");
-		assert(RealsAreEqual(to_24_hour_clock(28.5), 4.5) && "test-4.5");
+	You may wish to inspect various function in <cmath> to work
+	with integer and fractional part of a hours separately.
+	*/
+	assert(RealsAreEqual(to_24_hour_clock(24), 0) && "test-4.1");
+	assert(RealsAreEqual(to_24_hour_clock(48), 0) && "test-4.2");
+	assert(RealsAreEqual(to_24_hour_clock(25), 1) && "test-4.3");
+	assert(RealsAreEqual(to_24_hour_clock(4), 4) && "test-4.4");
+	assert(RealsAreEqual(to_24_hour_clock(28.5), 4.5) && "test-4.5");
 
+
+
+	//-------------------------------time_to_utc-------------------------------------
+	/*
+	Return time at UTC+0, where utc_offset is the number of hours away from
+	UTC+0.
+	You may be interested in:
+	https://en.wikipedia.org/wiki/Coordinated_Universal_Time
+
+	>>> time_to_utc(+0, 12.0)
+	12.0
+
+	>>> time_to_utc(+1, 12.0)
+	11.0
+
+	>>> time_to_utc(-1, 12.0)
+	13.0
+
+	>>> time_to_utc(-11, 18.0)
+	5.0
+
+	>>> time_to_utc(-1, 0.0)
+	1.0
+
+	>>> time_to_utc(-1, 23.0)
+	0.0
+	*/	
+	assert(RealsAreEqual(time_to_utc(+0, 12.0), 12.0) && "test-5.1");
+	assert(RealsAreEqual(time_to_utc(+1, 12.0), 11.0) && "test-5.2");
+	assert(RealsAreEqual(time_to_utc(-1, 12.0), 13.0) && "test-5.3");
+	assert(RealsAreEqual(time_to_utc(-11, 18.0), 5.0) && "test-5.4");
+	assert(RealsAreEqual(time_to_utc(-1, 0.0), 1.0) && "test-5.5");
+	assert(RealsAreEqual(time_to_utc(-1, 23.0), 0.0) && "test-5.6");
 }
