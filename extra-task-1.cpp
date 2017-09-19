67e76b715b92af1afa2c5979af6a4fc09248e783
Mirradell <olya.shtejn@yandex.ru>
2017-09-19 14:24:11 +0300

    Add realisation for to_float_hours() function

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -48,6 +48,9 @@ double hours_difference(double time_1, double time_2)
 
 double to_float_hours(int hours, int minutes, int seconds)
 {
+	assert((minutes >= 0) && (minutes < 60) && "Minutes are in wrong format!");
+	assert((seconds >= 0) && (seconds < 60) && "Seconds are in wrong format!");
+	return hours + (minutes / 60.0) + (seconds / 3600.0);
     /*
         Return the total number of hours in the specified number
         of hours, minutes, and seconds.
