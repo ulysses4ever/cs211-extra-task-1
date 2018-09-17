aaf7bae76d9959840425f7f799298a47491631a7
Lereena <lereenadem@yandex.ru>
2018-09-17 15:24:49 +0300

    Add to_24_hour_clock

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -19,31 +19,13 @@ double to_float_hours(int hours, int minutes, int seconds)
 
 double to_24_hour_clock(double hours)
 {
-    /*
-        hours is a number of hours since midnight. Return the
-        hour as seen on a 24-hour clock.
+    assert(hours >= 0);
+    auto h_part = (int)trunc(hours);
+    auto m_part = hours - h_part;
+    while (h_part >= 24)
+        h_part -= 24;
 
-        Precondition: hours >= 0
-
-        >>> to_24_hour_clock(24)
-        0
-        
-        >>> to_24_hour_clock(48)
-        0
-        
-        >>> to_24_hour_clock(25)
-        1
-        
-        >>> to_24_hour_clock(4)
-        4
-        
-        >>> to_24_hour_clock(28.5)
-        4.5
-        
-        You may wish to inspect various function in <cmath> to work
-        with integer and fractional part of a hours separately.
-        
-    */
+    return h_part + m_part;
 }
 
 /*
