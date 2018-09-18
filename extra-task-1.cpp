6cbe4aaabde9c1ee39833df0b2c53a24963044cf
dimadboss <ds-drozdov@yandex.ru>
2018-09-18 11:34:32 +0300

    Add func 2

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -23,6 +23,7 @@ double seconds_difference(double time_1, double time_2)
 
 double hours_difference(double time_1, double time_2)
 {
+	return seconds_difference(time_1, time_2) / 3600.0;
     /*
         Return the number of hours later that a time in seconds
         time_2 is than a time in seconds time_1.
@@ -174,4 +175,10 @@ int main()
 	assert(seconds_difference(3600.0, 1800.0) == -1800.0);
 	assert(seconds_difference(1800.0, 2160.0) == 360.0);
 	assert(seconds_difference(1800.0, 1800.0) == 0.0);
+
+	assert(hours_difference(1800.0, 3600.0) == 0.5);
+	assert(hours_difference(3600.0, 1800.0) == -0.5);
+	assert(hours_difference(1800.0, 2160.0) == 0.1);
+	assert(hours_difference(1800.0, 1800.0) == 0.0);
+
 }
