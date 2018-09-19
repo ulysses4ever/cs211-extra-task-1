6bf707a996e095d86c9d1d9420a051dfff59e0a8
luckynatrium <boris.wertik@gmail.com>
2018-09-19 18:21:57 +0300

    to_24_hour_clock done

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -42,6 +42,13 @@ double to_float_hours(int hours, int minutes, int seconds)
 
 double to_24_hour_clock(double hours)
 {
+	assert(hours >= 0);
+	while (true)
+	{
+		if (hours < 24)
+			return hours;
+		hours -= 24;
+	}
 	return 0;
     /*
         hours is a number of hours since midnight. Return the
@@ -49,20 +56,7 @@ double to_24_hour_clock(double hours)
 
         Precondition: hours >= 0
 
-        >>> to_24_hour_clock(24)
-        0
-        
-        >>> to_24_hour_clock(48)
-        0
-        
-        >>> to_24_hour_clock(25)
-        1
-        
-        >>> to_24_hour_clock(4)
-        4
-        
-        >>> to_24_hour_clock(28.5)
-        4.5
+       
         
         You may wish to inspect various function in <cmath> to work
         with integer and fractional part of a hours separately.
@@ -171,7 +165,11 @@ int main()
 	
 	//system("pause");
 
-	 
-
+	assert(fabs(to_24_hour_clock(24) - 0) < DBL_EPSILON);
+	assert(fabs(to_24_hour_clock(48) - 0) < DBL_EPSILON);
+	assert(fabs(to_24_hour_clock(25) - 1) < DBL_EPSILON);
+	assert(fabs(to_24_hour_clock(4) - 4) < DBL_EPSILON);
+	assert(fabs(to_24_hour_clock(28.5) - 4.5) < DBL_EPSILON);
+	
 		 
 }
