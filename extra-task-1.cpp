61f30e93338b12b0b5c06848aab35e48e8f7e334
luckynatrium <boris.wertik@gmail.com>
2018-09-19 19:21:05 +0300

    last correction befor pull-requesting

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -1,7 +1,6 @@
 #include <assert.h>
 #include <cmath>
 #include <cfloat>
-#include <iostream>
 double seconds_difference(double time_1, double time_2)
 {
 	return time_2 - time_1;
@@ -176,17 +175,17 @@ int main()
 	assert(seconds_difference(1800.0, 2160.0) == 360.0);
 	assert(seconds_difference(1800.0, 1800.0) == 0.0);
 	
+
 	assert(hours_difference(1800.0, 3600.0) == 0.5);
 	assert(hours_difference(3600.0, 1800.0) == -0.5);
 	assert(hours_difference(1800.0, 2160.0) == 0.1);
 	assert(hours_difference(1800.0, 1800.0) == 0.0);
-	
-	//std::cout << to_float_hours(0, 15, 0);
+	 
+
 	assert(fabs(to_float_hours(0, 15, 0)-  0.25)<DBL_EPSILON);
 	assert(fabs(to_float_hours(2, 45, 9) - 2.7525) < DBL_EPSILON);
 	assert(fabs(to_float_hours(1, 0, 36) - 1.01) < DBL_EPSILON);
 	
-	//system("pause");
 
 	assert(fabs(to_24_hour_clock(24) - 0) < DBL_EPSILON);
 	assert(fabs(to_24_hour_clock(48) - 0) < DBL_EPSILON);
@@ -213,5 +212,4 @@ int main()
 	assert(time_from_utc(-1, 0.0) == 23.0);
 	assert(time_from_utc(-1, 23.0) == 22.0);
 	assert(time_from_utc(+1, 23.0) == 0.0);
-
 }
