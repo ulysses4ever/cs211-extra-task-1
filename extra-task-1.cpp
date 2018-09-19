603de6baf0abeb76e788bcf25df4e10f992e8390
MihaxXx <mikes22@yandex.ru>
2018-09-20 00:40:13 +0300

    time_from_utc implementation

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -56,35 +56,10 @@ double time_to_utc(int utc_offset, double time)
     return to_24_hour_clock((time - utc_offset) > 0 ? (time - utc_offset) : 24 + (time - utc_offset));
 }
 
+///Return UTC time in time zone utc_offset.
 double time_from_utc(int utc_offset, double time)
 {
-    /*
-        Return UTC time in time zone utc_offset.
-
-        >>> time_from_utc(+0, 12.0)
-        12.0
- 
-        >>> time_from_utc(+1, 12.0)
-        13.0
- 
-        >>> time_from_utc(-1, 12.0)
-        11.0
- 
-        >>> time_from_utc(+6, 6.0)
-        12.0
- 
-        >>> time_from_utc(-7, 6.0)
-        23.0
- 
-        >>> time_from_utc(-1, 0.0)
-        23.0
- 
-        >>> time_from_utc(-1, 23.0)
-        22.0
- 
-        >>> time_from_utc(+1, 23.0)
-        0.0
-    */
+    return to_24_hour_clock((time + utc_offset) > 0 ? (time + utc_offset) : 24 + (time + utc_offset));
 }
 
 int main()
@@ -130,6 +105,15 @@ int main()
     assert(fabs(time_to_utc(+6, 5.0) - 23.0) < DBL_EPSILON);  //Additional test to check passing to previous day
     assert(fabs(time_to_utc(-6, 23.0) - 5.0) < DBL_EPSILON);
 
-
+    //time_from_utc tests
+    assert(fabs(time_from_utc(+0, 12.0) - 12.0) < DBL_EPSILON);
+    assert(fabs(time_from_utc(+1, 12.0) - 13.0) < DBL_EPSILON);
+    assert(fabs(time_from_utc(-1, 12.0) - 11.0) < DBL_EPSILON);
+    assert(fabs(time_from_utc(+6, 6.0) - 12.0) < DBL_EPSILON);
+    assert(fabs(time_from_utc(-7, 6.0) - 23.0) < DBL_EPSILON);
+    assert(fabs(time_from_utc(-1, 0.0) - 23.0) < DBL_EPSILON);
+    assert(fabs(time_from_utc(-1, 23.0) - 22.0) < DBL_EPSILON);
+    assert(fabs(time_from_utc(+1, 23.0) - 0.0) < DBL_EPSILON);
+    assert(fabs(time_from_utc(-2, 1.0) - 23.0) < DBL_EPSILON);  //Additional test to check passing to previous day
 
 }
