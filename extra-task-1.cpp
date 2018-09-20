12a21655e42eb283239a4f3e1fdd94acfcb3dfbf
DdiavaLL <43447259+DdiavaLL@users.noreply.github.com>
2018-09-20 22:16:13 +0300

    Update extra-task-1.cpp

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -6,16 +6,16 @@ double seconds_difference(double time_1, double time_2)
         /*Return the number of seconds later that a time in seconds
         time_2 is than a time in seconds time_1.*/
             
-        assert (fabs(seconds_difference(1800.0, 3600.0)) == DBL_EPSILON);
+        assert (fabs(seconds_difference(1800.0, 3600.0)) == 1800.0);
         //1800.0
 
-        assert (fabs(seconds_difference(3600.0, 1800.0)) == DBL_EPSILON);
+        assert (fabs(seconds_difference(3600.0, 1800.0)) == -1800.0);
         //-1800.0
 
-        assert (fabs(seconds_difference(1800.0, 2160.0)) == DBL_EPSILON);
+        assert (fabs(seconds_difference(1800.0, 2160.0)) == 360.0);
         //360.0
 
-        assert (fabs(seconds_difference(1800.0, 1800.0)) == DBL_EPSILON);
+        assert (fabs(seconds_difference(1800.0, 1800.0)) == 0.0);
         //0.0
     
 }
