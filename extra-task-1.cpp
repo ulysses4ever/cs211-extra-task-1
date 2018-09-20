00bb36281265152b3afb321b2c8465dc9543461c
DdiavaLL <43447259+DdiavaLL@users.noreply.github.com>
2018-09-20 22:51:33 +0300

    Update extra-task-1.cpp

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -22,43 +22,45 @@ double seconds_difference(double time_1, double time_2)
 
 double hours_difference(double time_1, double time_2)
 {
-    return (time_2 - time_1) / 3600.0;
+    return (fabs(time_2 - time_1)) / 3600.0;
     /*
         Return the number of hours later that a time in seconds
         time_2 is than a time in seconds time_1.
-            
+    */      
+       
         assert (hours_difference(1800.0, 3600.0) == 0.5 );
-        0.5
+        //0.5
 
         assert (hours_difference(3600.0, 1800.0) == -0.5);
-        -0.5
+        //-0.5
 
-        >>> hours_difference(1800.0, 2160.0)
-        0.1
+        assert (hours_difference(1800.0, 2160.0) == 0.1);
+        //0.1
 
-        >>> hours_difference(1800.0, 1800.0)
-        0.0
-    */
+        assert (hours_difference(1800.0, 1800.0) == 0.0);
+        //0.0
+    
 }
 
 double to_float_hours(int hours, int minutes, int seconds)
 {
-    
+    return (hourz + minutes/60 + seconds/3600);
     /*
         Return the total number of hours in the specified number
         of hours, minutes, and seconds.
 
         Precondition: 0 <= minutes < 60  and  0 <= seconds < 60
-
-        >>> to_float_hours(0, 15, 0)
-        0.25
-
-        >>> to_float_hours(2, 45, 9)
-        2.7525
-
-        >>> to_float_hours(1, 0, 36)
-        1.01
     */
+        assert( to_float_hours(0, 15, 0) == 0.25);
+        //0.25
+
+        assert( to_float_hours(2, 45, 9) == 2.7525);
+        //2.7525
+
+        assert( to_float_hours(1, 0, 36) == 1.01);
+        //>>> to_float_hours(1, 0, 36)
+        //1.01
+   
 }
 
 double to_24_hour_clock(double hours)
