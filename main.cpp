cd5b8c8aa5a56ef114762fdad2ddb1aa609b0802
AtianCode <AtianCode@gmail.com>
2018-09-21 18:43:38 +0300

    Add assert for to_24 hour_clock

--- main.cpp
+++ main.cpp
@@ -22,10 +22,12 @@ int main()
 	assert(to_float_hours(2, 45, 9) - 2.7525 < DBL_EPSILON);
 	assert(to_float_hours(1, 0, 36) - 1.01 < DBL_EPSILON);
 
-	cout << to_24_hour_clock(25) << endl;
-	cout << to_24_hour_clock(24) << endl;
-	cout << to_24_hour_clock(48) << endl;
-	cout << to_24_hour_clock(4) << endl;
-	cout << to_24_hour_clock(28.5) << endl;
+	assert(to_24_hour_clock(24) - 0.0 < DBL_EPSILON);
+	assert(to_24_hour_clock(48) - 0.0 < DBL_EPSILON);
+	assert(to_24_hour_clock(25) - 1.0 < DBL_EPSILON);
+	assert(to_24_hour_clock(4) - 4.0 < DBL_EPSILON);
+	assert(to_24_hour_clock(28.5) - 4.5 < DBL_EPSILON);
+
+	cout << "All tests completed" << endl;
 	system("pause");
 }
