c225c79c2befadaa68ca0b445c3e2feb8609dc88
AtianCode <AtianCode@gmail.com>
2018-09-21 18:29:20 +0300

    relised header files with functions

---
+++ extra-task-1.h
@@ -0,0 +1,20 @@
+#ifndef EXTRATASK_H
+#define EXTRATASK_H
+
+double seconds_difference(double time_1, double time_2);
+
+double hours_difference(double time_1, double time_2);
+
+double to_float_hours(int hours, int minutes, int seconds);
+
+double to_24_hour_clock(double hours);
+
+int get_hours(double time);
+
+int get_minutes(double time);
+
+int get_seconds(double time);
+
+double time_to_utc(int utc_offset, double time);
+
+#endif EXTRATASK_H
