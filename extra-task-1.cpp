b47b86e30b99dd9e1b2eb6bffa3f9f0e2f910635
AtianCode <AtianCode@gmail.com>
2018-09-21 20:21:17 +0300

    File completed!

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -1,6 +1,18 @@
-#include <math.h>
+#include <cmath>
 #include <float.h>
 #include <assert.h>
+
+/*
+Implement three functions
+* get_hours
+* get_minutes
+* get_seconds
+They are used to determine the hours part, minutes part and seconds part
+of a time in seconds. E.g.:
+
+In other words, if 3800 seconds have elapsed since midnight,
+it is currently 01:03:20 (hh:mm:ss).
+*/
 
 int get_hours(double time)
 {
@@ -25,17 +37,6 @@ double seconds_difference(double time_1, double time_2)
         Return the number of seconds later that a time in seconds
         time_2 is than a time in seconds time_1.
             
-        >>> seconds_difference(1800.0, 3600.0)
-        1800.0
-
-        >>> seconds_difference(3600.0, 1800.0)
-        -1800.0
-
-        >>> seconds_difference(1800.0, 2160.0)
-        360.0
-
-        >>> seconds_difference(1800.0, 1800.0)
-        0.0
     */
 	return time_2 - time_1;
 }
@@ -48,14 +49,6 @@ double to_float_hours(int hours, int minutes, int seconds)
 
 	Precondition: 0 <= minutes < 60  and  0 <= seconds < 60
 
-	>>> to_float_hours(0, 15, 0)
-	0.25
-
-	>>> to_float_hours(2, 45, 9)
-	2.7525
-
-	>>> to_float_hours(1, 0, 36)
-	1.01
 	*/
 	assert(minutes >= 0);
 	assert(minutes < 60);
@@ -70,17 +63,6 @@ double hours_difference(double time_1, double time_2)
         Return the number of hours later that a time in seconds
         time_2 is than a time in seconds time_1.
             
-        >>> hours_difference(1800.0, 3600.0)
-        0.5
-
-        >>> hours_difference(3600.0, 1800.0)
-        -0.5
-
-        >>> hours_difference(1800.0, 2160.0)
-        0.1
-
-        >>> hours_difference(1800.0, 1800.0)
-        0.0
     */
 	int h1 = get_hours(time_1);
 	int m1 = get_minutes(time_1);
@@ -102,21 +84,6 @@ double to_24_hour_clock(double hours)
 
         Precondition: hours >= 0
 
-        >>> to_24_hour_clock(24)
-        0
-        
-        >>> to_24_hour_clock(48)
-        0
-        
-        >>> to_24_hour_clock(25)
-        1
-        
-        >>> to_24_hour_clock(4)
-        4
-        
-        >>> to_24_hour_clock(28.5)
-        4.5
-        
         You may wish to inspect various function in <cmath> to work
         with integer and fractional part of a hours separately.
         
@@ -128,27 +95,6 @@ double to_24_hour_clock(double hours)
 }
 
 
-/*
-    Implement three functions
-        * get_hours
-        * get_minutes
-        * get_seconds
-    They are used to determine the hours part, minutes part and seconds part 
-    of a time in seconds. E.g.:
-
-    >>> get_hours(3800)
-    1
-
-    >>> get_minutes(3800)
-    3
-
-    >>> get_seconds(3800)
-    20
-
-    In other words, if 3800 seconds have elapsed since midnight, 
-    it is currently 01:03:20 (hh:mm:ss).
-*/
-
 double time_to_utc(int utc_offset, double time)
 {
     /*
@@ -156,56 +102,16 @@ double time_to_utc(int utc_offset, double time)
         UTC+0.
         You may be interested in:
         https://en.wikipedia.org/wiki/Coordinated_Universal_Time
-
-        >>> time_to_utc(+0, 12.0)
-        12.0
- 
-        >>> time_to_utc(+1, 12.0)
-        11.0
- 
-        >>> time_to_utc(-1, 12.0)
-        13.0
- 
-        >>> time_to_utc(-11, 18.0)
-        5.0
- 
-        >>> time_to_utc(-1, 0.0)
-        1.0
- 
-        >>> time_to_utc(-1, 23.0)
-        0.0
     */
-	return 0;
+	auto t = abs(time - utc_offset);
+	return to_24_hour_clock(t);
 }
 
 double time_from_utc(int utc_offset, double time)
 {
     /*
         Return UTC time in time zone utc_offset.
-
-        >>> time_from_utc(+0, 12.0)
-        12.0
- 
-        >>> time_from_utc(+1, 12.0)
-        13.0
- 
-        >>> time_from_utc(-1, 12.0)
-        11.0
- 
-        >>> time_from_utc(+6, 6.0)
-        12.0
- 
-        >>> time_from_utc(-7, 6.0)
-        23.0
- 
-        >>> time_from_utc(-1, 0.0)
-        23.0
- 
-        >>> time_from_utc(-1, 23.0)
-        22.0
- 
-        >>> time_from_utc(+1, 23.0)
-        0.0
     */
-	return 0;
+	auto t = abs(time + utc_offset);
+	return to_24_hour_clock(t);
 }
