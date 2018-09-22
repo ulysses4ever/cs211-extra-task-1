33cf07696ab169e9414c6159f5a28a9939709648
Shiba <shibarome@gmail.com>
2018-09-22 19:55:02 +0300

    time_from_uts

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -1,4 +1,5 @@
 #include <cassert>
+#include <iostream>
 #include <cfloat>
 #include <cmath>
 
@@ -64,36 +65,10 @@ double time_to_utc(int utc_offset, double time)
 	return ((int)floor(time) - utc_offset + 24) % 24 + (time - floor(time));
 }
 
+//Return UTC time in time zone utc_offset.
 double time_from_utc(int utc_offset, double time)
 {
-    /*
-        Return UTC time in time zone utc_offset.
-
-        >>> time_from_utc(+0, 12.0)
-        12.0
- 
-        >>> time_from_utc(+1, 12.0)
-        13.0
- 
-        >>> time_from_utc(-1, 12.0)
-        11.0
- 
-        >>> time_from_utc(+6, 6.0)
-        12.0
- 
-        >>> time_from_utc(-7, 6.0)
-        23.0
- 
-        >>> time_from_utc(-1, 0.0)
-        23.0
- 
-        >>> time_from_utc(-1, 23.0)
-        22.0
- 
-        >>> time_from_utc(+1, 23.0)
-        0.0
-    */
-	return 0;
+	return ((int)floor(time) + utc_offset + 24) % 24 + (time - floor(time));
 }
 
 int main()
@@ -129,4 +104,16 @@ int main()
 	assert(areEquel(time_to_utc(-11, 18.0), 5.0));
 	assert(areEquel(time_to_utc(-1, 0.0), 1.0));
 	assert(areEquel(time_to_utc(-1, 23.0), 0.0));
+	//time_from_utc
+	assert(areEquel(time_from_utc(0, 12.0), 12.0));
+	assert(areEquel(time_from_utc(1, 12.0), 13.0));
+	assert(areEquel(time_from_utc(-1, 12.0), 11.0));
+	assert(areEquel(time_from_utc(6, 6.0), 12.0));
+	assert(areEquel(time_from_utc(-7, 6.0), 23.0));
+	assert(areEquel(time_from_utc(-1, 0.0), 23.0));
+	assert(areEquel(time_from_utc(-1, 23.0), 22.0));
+	assert(areEquel(time_from_utc(1, 23.0), 0.0));
+	
+	cout << "All tests is done" << endl;
+	system("pause");
 }
