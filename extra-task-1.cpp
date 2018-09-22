51b554a9fe4816b90b59af8cbe87a446379b26e3
RobolabGs2 <32936783+RobolabGs2@users.noreply.github.com>
2018-09-22 19:28:10 +0300

    hours_difference

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -13,28 +13,13 @@ bool are_equals(double a, double b)
 ///time_2 is than a time in seconds time_1.
 double seconds_difference(double time_1, double time_2)
 {
-    return time_2-time_1;
+	return time_2-time_1;
 }
-
+///Return the number of hours later that a time in seconds
+///time_2 is than a time in seconds time_1.
 double hours_difference(double time_1, double time_2)
 {
-    /*
-        Return the number of hours later that a time in seconds
-        time_2 is than a time in seconds time_1.
-            
-        >>> hours_difference(1800.0, 3600.0)
-        0.5
-
-        >>> hours_difference(3600.0, 1800.0)
-        -0.5
-
-        >>> hours_difference(1800.0, 2160.0)
-        0.1
-
-        >>> hours_difference(1800.0, 1800.0)
-        0.0
-    */
-	return 0;
+	return seconds_difference(time_1, time_2)/3600;
 }
 
 double to_float_hours(int hours, int minutes, int seconds)
@@ -175,6 +160,11 @@ int main()
         assert(are_equals(seconds_difference(3600.0, 1800.0),-1800.0));
 	assert(are_equals(seconds_difference(1800.0, 2160.0),360.0));
 	assert(are_equals(seconds_difference(1800.0, 1800.0), 0.0));
+
+	assert(are_equals(hours_difference(1800.0, 3600.0), 0.5));
+	assert(are_equals(hours_difference(3600.0, 1800.0), -0.5));
+	assert(are_equals(hours_difference(1800.0, 2160.0), 0.1));
+	assert(are_equals(hours_difference(1800.0, 1800.0), 0.0));
 
 	cout<<"Tests comleted"<<endl;	
 }
