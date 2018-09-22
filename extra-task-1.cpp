8800335d2d17d3eed0a4f88c397fe433afa2a541
SID37 <33577875+SID37@users.noreply.github.com>
2018-09-22 21:19:18 +0300

    time_to_utc

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -39,31 +39,7 @@ int get_seconds(int seconds)
 
 double time_to_utc(int utc_offset, double time)
 {
-    /*
-        Return time at UTC+0, where utc_offset is the number of hours away from
-        UTC+0.
-        You may be interested in:
-        https://en.wikipedia.org/wiki/Coordinated_Universal_Time
-
-        >>> time_to_utc(+0, 12.0)
-        12.0
- 
-        >>> time_to_utc(+1, 12.0)
-        11.0
- 
-        >>> time_to_utc(-1, 12.0)
-        13.0
- 
-        >>> time_to_utc(-11, 18.0)
-        5.0
- 
-        >>> time_to_utc(-1, 0.0)
-        1.0
- 
-        >>> time_to_utc(-1, 23.0)
-        0.0
-    */
-	return 0;
+	return to_24_hour_clock(24 + time - utc_offset);
 }
 
 double time_from_utc(int utc_offset, double time)
@@ -136,9 +112,17 @@ void main()
 	{
 		assert(get_hours(3800) == 1);
 		assert(get_minutes(3800) == 3);
-		int x = get_seconds(3800);
 		assert(get_seconds(3800) == 20);
 	}
 
+	//	time_to_utc tests
+	{
+		assert(time_to_utc(+0, 12.0) == 12.0);
+		assert(time_to_utc(+1, 12.0) == 11.0);
+		assert(time_to_utc(-1, 12.0) == 13.0);
+		assert(time_to_utc(-11, 18.0) == 5.0);
+		assert(time_to_utc(-1, 0.0) == 1.0);
+		assert(time_to_utc(-1, 23.0) == 0.0);
+	}
 
 }
