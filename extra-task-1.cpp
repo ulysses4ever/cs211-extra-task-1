a410780378e131e4a6d2edaa47c2066171a034f2
RobolabGs2 <32936783+RobolabGs2@users.noreply.github.com>
2018-09-22 21:44:57 +0300

    time_from_utc

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -84,37 +84,10 @@ double time_to_utc(int utc_offset, double time)
 {
     	return fmod(time-utc_offset, 24);
 }
-
+///Return UTC time in time zone utc_offset.
 double time_from_utc(int utc_offset, double time)
 {
-    /*
-        Return UTC time in time zone utc_offset.
-
-        >>> time_from_utc(+0, 12.0)
-        12.0
- 
-        >>> time_from_utc(+1, 12.0)
-        13.0
- 
-        >>> time_from_utc(-1, 12.0)
-        11.0
- 
-        >>> time_from_utc(+6, 6.0)
-        12.0
- 
-        >>> time_from_utc(-7, 6.0)
-        23.0
- 
-        >>> time_from_utc(-1, 0.0)
-        23.0
- 
-        >>> time_from_utc(-1, 23.0)
-        22.0
- 
-        >>> time_from_utc(+1, 23.0)
-        0.0
-    */
-	return 0;
+   	return fmod(24+time+utc_offset, 24);
 }
 
 int main() 
@@ -158,6 +131,15 @@ int main()
       	assert(are_equals(time_to_utc(-11, 18.0), 5.0));
 	assert(are_equals(time_to_utc(-1, 0.0), 1.0));
 	assert(are_equals(time_to_utc(-1, 23.0), 0.0));
+
+	assert(are_equals(time_from_utc(+0, 12.0), 12.0));
+	assert(are_equals(time_from_utc(+1, 12.0), 13.0));
+	assert(are_equals(time_from_utc(-1, 12.0), 11.0));
+ 	assert(are_equals(time_from_utc(+6, 6.0), 12.0));
+	assert(are_equals(time_from_utc(-7, 6.0), 23.0));
+	assert(are_equals(time_from_utc(-1, 0.0), 23.0));
+ 	assert(are_equals(time_from_utc(-1, 23.0), 22.0));
+	assert(are_equals(time_from_utc(+1, 23.0), 0.0));
 
 	cout<<"Tests comleted"<<endl;	
 }
