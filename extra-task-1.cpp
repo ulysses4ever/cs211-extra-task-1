4a987cb05b7dea2ac48403d37c87c49693c37c71
RobolabGs2 <32936783+RobolabGs2@users.noreply.github.com>
2018-09-22 20:21:41 +0300

    to_24_hour_clock

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -32,34 +32,12 @@ double to_float_hours(int hours, int minutes, int seconds)
         return hours+minutes/60.0+seconds/3600.0;
 }
 
+//hours is a number of hours since midnight. Return the
+//hour as seen on a 24-hour clock.
 double to_24_hour_clock(double hours)
 {
-    /*
-        hours is a number of hours since midnight. Return the
-        hour as seen on a 24-hour clock.
-
-        Precondition: hours >= 0
-
-        >>> to_24_hour_clock(24)
-        0
-        
-        >>> to_24_hour_clock(48)
-        0
-        
-        >>> to_24_hour_clock(25)
-        1
-        
-        >>> to_24_hour_clock(4)
-        4
-        
-        >>> to_24_hour_clock(28.5)
-        4.5
-        
-        You may wish to inspect various function in <cmath> to work
-        with integer and fractional part of a hours separately.
-        
-    */
-	return 0;
+	assert(hours>=0);
+    	return fmod(hours, 24);
 }
 
 /*
@@ -160,5 +138,12 @@ int main()
 	assert(are_equals(to_float_hours(0, 15, 0), 0.25));
 	assert(are_equals(to_float_hours(2, 45, 9), 2.7525));
 	assert(are_equals(to_float_hours(1, 0, 36), 1.01));
+
+        assert(are_equals(to_24_hour_clock(24), 0));
+        assert(are_equals(to_24_hour_clock(48), 0));
+        assert(are_equals(to_24_hour_clock(25), 1));
+        assert(are_equals(to_24_hour_clock(4),  4));
+        assert(are_equals(to_24_hour_clock(28.5), 4.5));
+
 	cout<<"Tests comleted"<<endl;	
 }
