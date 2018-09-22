51afcf1f9ed3a4c96a4351dce831a28eb1fc3921
SID37 <33577875+SID37@users.noreply.github.com>
2018-09-22 20:59:49 +0300

    seconds_difference сделал

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -8,23 +8,7 @@ double seconds_difference(double time_1, double time_2)
 
 double hours_difference(double time_1, double time_2)
 {
-    /*
-        Return the number of hours later that a time in seconds
-        time_2 is than a time in seconds time_1.
-            
-        >>> hours_difference(1800.0, 3600.0)
-        0.5
-
-        >>> hours_difference(3600.0, 1800.0)
-        -0.5
-
-        >>> hours_difference(1800.0, 2160.0)
-        0.1
-
-        >>> hours_difference(1800.0, 1800.0)
-        0.0
-    */
-	return 0;
+	return seconds_difference(time_1, time_2) / 3600.0;
 }
 
 double to_float_hours(int hours, int minutes, int seconds)
@@ -161,24 +145,18 @@ double time_from_utc(int utc_offset, double time)
 
 void main()
 {
-	assert(seconds_difference(1800.0, 3600.0) == 1800.0);
-	assert(seconds_difference(3600.0, 1800.0) == -1800.0);
-	assert(seconds_difference(1800.0, 2160.0) == 360.0);
-	assert(seconds_difference(1800.0, 1800.0) == 0.0);
-	/*
-	Return the number of seconds later that a time in seconds
-	time_2 is than a time in seconds time_1.
-
-	>>> seconds_difference(1800.0, 3600.0)
-	1800.0
-
-	>>> seconds_difference(3600.0, 1800.0)
-	-1800.0
-
-	>>> seconds_difference(1800.0, 2160.0)
-	360.0
-
-	>>> seconds_difference(1800.0, 1800.0)
-	0.0
-	*/
+	//	seconds_difference tests
+	{
+		assert(seconds_difference(1800.0, 3600.0) == 1800.0);
+		assert(seconds_difference(3600.0, 1800.0) == -1800.0);
+		assert(seconds_difference(1800.0, 2160.0) == 360.0);
+		assert(seconds_difference(1800.0, 1800.0) == 0.0);
+	}
+	//	hours_difference tests
+	{
+		assert(hours_difference(1800.0, 3600.0) == 0.5);
+		assert(hours_difference(3600.0, 1800.0) == -0.5);
+		assert(hours_difference(1800.0, 2160.0) == 0.1);
+		assert(hours_difference(1800.0, 1800.0) == 0.0);
+	}
 }
