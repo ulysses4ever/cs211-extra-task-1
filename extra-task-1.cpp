da111ab6bc1112d022c5dd1e9297346057bb4a02
DdiavaLL <ddiavall@yandex.ru>
2018-09-22 18:47:35 +0300

    Third function

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -10,23 +10,9 @@ double hours_difference(double time_1, double time_2)
 
 double to_float_hours(int hours, int minutes, int seconds)
 {
-    return (hourz + minutes/60 + seconds/3600);
-    /*
-        Return the total number of hours in the specified number
-        of hours, minutes, and seconds.
-
-        Precondition: 0 <= minutes < 60  and  0 <= seconds < 60
-    */
-        assert( to_float_hours(0, 15, 0) == 0.25);
-        //0.25
-
-        assert( to_float_hours(2, 45, 9) == 2.7525);
-        //2.7525
-
-        assert( to_float_hours(1, 0, 36) == 1.01);
-        //>>> to_float_hours(1, 0, 36)
-        //1.01
-   
+    assert((minutes < 60) && (minutes >= 0));
+	assert((seconds < 60) && (seconds >= 0));
+	return (hours + (minutes / 60.0) + (seconds / 3600.0));  
 }
 
 double to_24_hour_clock(double hours)
