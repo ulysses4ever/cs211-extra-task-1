a1854775a433da4069e437f6ce50ccd211277da4
asp1208 <43145108+asp1208@users.noreply.github.com>
2019-09-26 16:40:27 +0300

    Update extra-task-1.cpp

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -2,6 +2,7 @@ double seconds_difference(double time_1, double time_2)
 {
     // your implementation goes here...
     
+    
     /*    
         Return the number of seconds later that a time in seconds
         time_2 is than a time in seconds time_1.
