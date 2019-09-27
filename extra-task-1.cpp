b45d70b9bd31a4173e88a71badf4de4e3fb3f809
Amalgam-Cloud <52462529+Amalgam-Cloud@users.noreply.github.com>
2019-09-27 22:00:32 +0300

    to_24_hour_clock

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -66,31 +66,32 @@ double to_float_hours(int hours, int minutes, int seconds)
 
 double to_24_hour_clock(double hours)
 {
+    assert((hours - DBL_EPSILION) >=0);
     /*
         hours is a number of hours since midnight. Return the
         hour as seen on a 24-hour clock.
-
         Precondition: hours >= 0
-
         >>> to_24_hour_clock(24)
         0
-        
+        assert((to_24_hour_clock(24)-0)< DBL_EPSILON);
         >>> to_24_hour_clock(48)
         0
-        
+        assert((to_24_hour_clock(48)-0)< DBL_EPSILON);
         >>> to_24_hour_clock(25)
         1
-        
+        assert((to_24_hour_clock(25)-1)< DBL_EPSILON);
         >>> to_24_hour_clock(4)
         4
-        
+        assert((to_24_hour_clock(4)-4)< DBL_EPSILON);
         >>> to_24_hour_clock(28.5)
         4.5
-        
+        assert((to_24_hour_clock(28.5)-4.5)< DBL_EPSILON);
+
         You may wish to inspect various function in <cmath> to work
         with integer and fractional part of a hours separately.
         
     */
+    return hours%24;
 }
 
 /*
