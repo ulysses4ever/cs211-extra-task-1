a6be41958d08d05c88282f37f54fec28680e12da
Amalgam-Cloud <52462529+Amalgam-Cloud@users.noreply.github.com>
2019-09-27 21:56:39 +0300

    to_float_hours

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -46,21 +46,22 @@ double hours_difference(double time_1, double time_2)
 
 double to_float_hours(int hours, int minutes, int seconds)
 {
+    assert( minutes >=0 && minutes < 60  &&  seconds >= 0 && seconds < 60);
     /*
         Return the total number of hours in the specified number
         of hours, minutes, and seconds.
-
         Precondition: 0 <= minutes < 60  and  0 <= seconds < 60
-
         >>> to_float_hours(0, 15, 0)
         0.25
-
+	assert((to_float_hours(0, 15, 0)-0.25)<DBL_EPSILON);
         >>> to_float_hours(2, 45, 9)
         2.7525
-
+	assert((to_float_hours(2, 45, 9)-2.7525)<DBL_EPSILON);
         >>> to_float_hours(1, 0, 36)
         1.01
+	assert((to_float_hours(1, 0, 36)-1.01)<DBL_EPSILON);
     */
+    return hours+minutes*(1/60)+seconds*(1/60);
 }
 
 double to_24_hour_clock(double hours)
