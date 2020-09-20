6c779a941383c13bce4438cf4503c0436ca89da4
robsta7 <meremeev2001@gmail.com>
2020-09-20 13:52:04 +0300

    Add time_to_utc

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -158,6 +158,7 @@ double time_to_utc(int utc_offset, double time)
         >>> time_to_utc(-1, 23.0)
         0.0
     */
+    return fmod(time - utc_offset, 24);
 }
 
 double time_from_utc(int utc_offset, double time)
