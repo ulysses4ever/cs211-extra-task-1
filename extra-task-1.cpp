06650196d6037bb42f994fc548f7e01f8169f7af
COOLIRON2311 <ignatenko.ivan.23112000@gmail.com>
2020-09-21 22:24:40 +0300

    Implemented time_to_utc

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -133,6 +133,7 @@ int get_seconds(int time)
 
 double time_to_utc(int utc_offset, double time)
 {
+    return time - utc_offset % 24;
     /*
         Return time at UTC+0, where utc_offset is the number of hours away from
         UTC+0.
