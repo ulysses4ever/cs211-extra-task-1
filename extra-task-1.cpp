5e0da799f6651c567de6c19dd9911d96af7c8779
Mr_Wolf080 <jenia.kozhanoff@yandex.ru>
2020-09-24 18:34:51 +0300

    get_hours, get_minutes, get_seconds

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -54,20 +54,28 @@ double to_24_hour_clock(double hours)
         * get_hours
         * get_minutes
         * get_seconds
-    They are used to determine the hours part, minutes part and seconds part 
+    They are used to determine the hours part, minutes part and seconds part
     of a time in seconds. E.g.:
+*/
 
-    >>> get_hours(3800)
-    1
+int get_hours(int seconds)
+{
+    assert(seconds >= 0);
+    return seconds / 3600;
+}
 
-    >>> get_minutes(3800)
-    3
+int get_minutes(int seconds)
+{
+    assert(seconds >= 0);
+    return (seconds - get_hours(seconds)*3600) / 60;
+}
 
-    >>> get_seconds(3800)
-    20
-
-    In other words, if 3800 seconds have elapsed since midnight, 
-    it is currently 01:03:20 (hh:mm:ss).
+int get_seconds(int seconds)
+{
+    assert(seconds >= 0);
+    return seconds - get_hours(seconds) * 3600 - get_minutes(seconds) * 60;
+}
+/*
 
 
 double time_to_utc(int utc_offset, double time)
@@ -168,6 +176,14 @@ int main()
     assert(fabs(to_24_hour_clock(25) - 1.0) < DBL_EPSILON);
     assert(fabs(to_24_hour_clock(4) - 4.0) < DBL_EPSILON);
     assert(fabs(to_24_hour_clock(28.5) - 4.5) < DBL_EPSILON);
+    cout << "Tests passed" << endl;
+    cout << endl;
+
+    //5
+    cout << "Task 5:" << endl;
+    assert(get_hours(3800) == 1);
+    assert(get_minutes(3800) == 3);
+    assert(get_seconds(3800) == 20);
     cout << "Tests passed" << endl;
     cout << endl;
 }
