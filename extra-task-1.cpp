f65f8b2c55eb33fe9dc36214b74e8d1fe7e1728a
Mr_Wolf080 <jenia.kozhanoff@yandex.ru>
2020-09-24 17:42:49 +0300

    2-я функция

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -12,23 +12,12 @@ double seconds_difference(double time_1, double time_2)
         Return the number of seconds later that a time in seconds
         time_2 is than a time in seconds time_1.
             
-        >>> seconds_difference(1800.0, 3600.0)
-        1800.0
-
-        >>> seconds_difference(3600.0, 1800.0)
-        -1800.0
-
-        >>> seconds_difference(1800.0, 2160.0)
-        360.0
-
-        >>> seconds_difference(1800.0, 1800.0)
-        0.0
     */
 }
 
 double hours_difference(double time_1, double time_2)
 {
-    return 0.0;
+    return seconds_difference(time_1,time_2)/3600;
     /*
         Return the number of hours later that a time in seconds
         time_2 is than a time in seconds time_1.
@@ -187,6 +176,15 @@ int main()
     assert(fabs(seconds_difference(3600.0, 1800.0) - (-1800.0)) < DBL_EPSILON);
     assert(fabs(seconds_difference(1800.0, 2160.0) - 360.0) < DBL_EPSILON);
     assert(fabs(seconds_difference(1800.0, 1800.0) - 0.0) < DBL_EPSILON);
+    cout << "Tests passed" << endl;
+    cout << endl;
+
+    //2
+    cout << "Task 2:" << endl;
+    assert(fabs(hours_difference(1800.0, 3600.0) - 0.5) < DBL_EPSILON);
+    assert(fabs(hours_difference(3600.0, 1800.0) - (-0.5)) < DBL_EPSILON);
+    assert(fabs(hours_difference(1800.0, 2160.0) - 0.1) < DBL_EPSILON);
+    assert(fabs(hours_difference(1800.0, 1800.0) - 0.0) < DBL_EPSILON);
     cout << "Tests passed" << endl;
     cout << endl;
 }
