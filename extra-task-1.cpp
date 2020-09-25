2262522b9528b446f70e10c6df79f3dcb565b88b
Alterehov <terekhov01@inbox.ru>
2020-09-25 18:44:40 +0300

    Функция  seconds_difference сделана

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -18,6 +18,7 @@ double seconds_difference(double time_1, double time_2)
         >>> seconds_difference(1800.0, 1800.0)
         0.0
     */
+    return time_1 - time_2;
 }
 
 double hours_difference(double time_1, double time_2)
