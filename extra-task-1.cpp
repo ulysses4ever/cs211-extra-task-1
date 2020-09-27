d86a0c73c600920a8c37c8c7b9b27cc4314e9e6f
elon_marks <elijah.gichev@gmail.com>
2020-09-27 18:21:46 +0300

    реализована time_to_utc

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -18,7 +18,7 @@ double to_float_hours(int hours, int minutes, int seconds)
 
     return (seconds / 3600.0) + (minutes / 60.0) + hours;
 }
-
+//
 double to_24_hour_clock(double hours)
 {
     assert(hours >= 0);
@@ -48,34 +48,19 @@ int get_seconds(int seconds)
 {
     return ((seconds % 3600) % 60) % 60;
 }
-
+//Return time at UTC+0, where utc_offset is the number of hours away from UTC + 0.
 double time_to_utc(int utc_offset, double time)
 {
-    /*
-        Return time at UTC+0, where utc_offset is the number of hours away from
-        UTC+0.
-        You may be interested in:
-        https://en.wikipedia.org/wiki/Coordinated_Universal_Time
-
-        >>> time_to_utc(+0, 12.0)
-        12.0
- 
-        >>> time_to_utc(+1, 12.0)
-        11.0
- 
-        >>> time_to_utc(-1, 12.0)
-        13.0
- 
-        >>> time_to_utc(-11, 18.0)
-        5.0
- 
-        >>> time_to_utc(-1, 0.0)
-        1.0
- 
-        >>> time_to_utc(-1, 23.0)
-        0.0
-    */
-    return 0;
+    double resTime = 0;
+    if ((time - utc_offset) > 0)
+    {
+        resTime = (time - utc_offset);
+    }
+    else
+    {
+        resTime = 24 + (time - utc_offset);
+    }
+    return to_24_hour_clock(resTime);
 }
 
 double time_from_utc(int utc_offset, double time)
