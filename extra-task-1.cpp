0ef4d85ffe6368745428a082482ca6ed9ab4fcb0
yonesurprise <yonesurprise@gmail.com>
2020-09-27 17:42:57 +0300

    seconds_difference

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -1,11 +1,14 @@
+#include "extra-task-1.h"
+
 double seconds_difference(double time_1, double time_2)
 {
+    return time_2 - time_1;
     // your implementation goes here...
-    
-    /*    
+
+    /*
         Return the number of seconds later that a time in seconds
         time_2 is than a time in seconds time_1.
-            
+
         >>> seconds_difference(1800.0, 3600.0)
         1800.0
 
@@ -22,10 +25,11 @@ double seconds_difference(double time_1, double time_2)
 
 double hours_difference(double time_1, double time_2)
 {
+
     /*
         Return the number of hours later that a time in seconds
         time_2 is than a time in seconds time_1.
-            
+
         >>> hours_difference(1800.0, 3600.0)
         0.5
 
@@ -69,22 +73,22 @@ double to_24_hour_clock(double hours)
 
         >>> to_24_hour_clock(24)
         0
-        
+
         >>> to_24_hour_clock(48)
         0
-        
+
         >>> to_24_hour_clock(25)
         1
-        
+
         >>> to_24_hour_clock(4)
         4
-        
+
         >>> to_24_hour_clock(28.5)
         4.5
-        
+
         You may wish to inspect various function in <cmath> to work
         with integer and fractional part of a hours separately.
-        
+
     */
 }
 
@@ -93,7 +97,7 @@ double to_24_hour_clock(double hours)
         * get_hours
         * get_minutes
         * get_seconds
-    They are used to determine the hours part, minutes part and seconds part 
+    They are used to determine the hours part, minutes part and seconds part
     of a time in seconds. E.g.:
 
     >>> get_hours(3800)
@@ -105,7 +109,7 @@ double to_24_hour_clock(double hours)
     >>> get_seconds(3800)
     20
 
-    In other words, if 3800 seconds have elapsed since midnight, 
+    In other words, if 3800 seconds have elapsed since midnight,
     it is currently 01:03:20 (hh:mm:ss).
 */
 
@@ -119,19 +123,19 @@ double time_to_utc(int utc_offset, double time)
 
         >>> time_to_utc(+0, 12.0)
         12.0
- 
+
         >>> time_to_utc(+1, 12.0)
         11.0
- 
+
         >>> time_to_utc(-1, 12.0)
         13.0
- 
+
         >>> time_to_utc(-11, 18.0)
         5.0
- 
+
         >>> time_to_utc(-1, 0.0)
         1.0
- 
+
         >>> time_to_utc(-1, 23.0)
         0.0
     */
@@ -144,25 +148,25 @@ double time_from_utc(int utc_offset, double time)
 
         >>> time_from_utc(+0, 12.0)
         12.0
- 
+
         >>> time_from_utc(+1, 12.0)
         13.0
- 
+
         >>> time_from_utc(-1, 12.0)
         11.0
- 
+
         >>> time_from_utc(+6, 6.0)
         12.0
- 
+
         >>> time_from_utc(-7, 6.0)
         23.0
- 
+
         >>> time_from_utc(-1, 0.0)
         23.0
- 
+
         >>> time_from_utc(-1, 23.0)
         22.0
- 
+
         >>> time_from_utc(+1, 23.0)
         0.0
     */
