3e00a4b8559f661ad5f29562555e0053e2372803
Ilya Ocheretov <ocheretov.ilya@yandex.ru>
2020-09-27 16:26:59 +0300

    time_to_utc

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -44,30 +44,7 @@ int get_seconds(int seconds)
 
 double time_to_utc(int utc_offset, double time)
 {
-    /*
-        Return time at UTC+0, where utc_offset is the number of hours away from
-        UTC+0.
-        You may be interested in:
-        https://en.wikipedia.org/wiki/Coordinated_Universal_Time
-
-        >>> time_to_utc(+0, 12.0)
-        12.0
- 
-        >>> time_to_utc(+1, 12.0)
-        11.0
- 
-        >>> time_to_utc(-1, 12.0)
-        13.0
- 
-        >>> time_to_utc(-11, 18.0)
-        5.0
- 
-        >>> time_to_utc(-1, 0.0)
-        1.0
- 
-        >>> time_to_utc(-1, 23.0)
-        0.0
-    */
+    return to_24_hour_clock(time - utc_offset);
 }
 
 double time_from_utc(int utc_offset, double time)
@@ -130,6 +107,14 @@ int main(){
     assert(get_minutes(3800) == 3);
     assert(get_seconds(3800) == 20);
     std::cout << "Tests get_hours get_minutes get_seconds completed" << std::endl;
+
+    assert(fabs(time_to_utc(+0, 12.0) - 12.0) < DBL_EPSILON);
+    assert(fabs(time_to_utc(+1, 12.0) - 11.0) < DBL_EPSILON);
+    assert(fabs(time_to_utc(-1, 12.0) - 13.0) < DBL_EPSILON);
+    assert(fabs(time_to_utc(-11, 18.0) - 5.0) < DBL_EPSILON);
+    assert(fabs(time_to_utc(-1, 0.0) - 1.0) < DBL_EPSILON);
+    assert(fabs(time_to_utc(-1, 23.0) - 0.0) < DBL_EPSILON);
+    std::cout << "Tests time_to_utc completed" << std::endl;
 
     return 0;
 }
