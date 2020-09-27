fa502bde341e69a461b81dee74c29c6c5536bd4e
elon_marks <elijah.gichev@gmail.com>
2020-09-27 17:53:05 +0300

    реализована seconds_difference

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -1,23 +1,8 @@
+#include <cmath>
+
 double seconds_difference(double time_1, double time_2)
 {
-    // your implementation goes here...
-    
-    /*    
-        Return the number of seconds later that a time in seconds
-        time_2 is than a time in seconds time_1.
-            
-        >>> seconds_difference(1800.0, 3600.0)
-        1800.0
-
-        >>> seconds_difference(3600.0, 1800.0)
-        -1800.0
-
-        >>> seconds_difference(1800.0, 2160.0)
-        360.0
-
-        >>> seconds_difference(1800.0, 1800.0)
-        0.0
-    */
+    return time_2 - time_1;
 }
 
 double hours_difference(double time_1, double time_2)
@@ -38,6 +23,7 @@ double hours_difference(double time_1, double time_2)
         >>> hours_difference(1800.0, 1800.0)
         0.0
     */
+    return 0;
 }
 
 double to_float_hours(int hours, int minutes, int seconds)
@@ -57,6 +43,7 @@ double to_float_hours(int hours, int minutes, int seconds)
         >>> to_float_hours(1, 0, 36)
         1.01
     */
+    return 0;
 }
 
 double to_24_hour_clock(double hours)
@@ -86,6 +73,7 @@ double to_24_hour_clock(double hours)
         with integer and fractional part of a hours separately.
         
     */
+    return 0;
 }
 
 /*
@@ -135,6 +123,7 @@ double time_to_utc(int utc_offset, double time)
         >>> time_to_utc(-1, 23.0)
         0.0
     */
+    return 0;
 }
 
 double time_from_utc(int utc_offset, double time)
@@ -166,4 +155,5 @@ double time_from_utc(int utc_offset, double time)
         >>> time_from_utc(+1, 23.0)
         0.0
     */
+    return 0;
 }
