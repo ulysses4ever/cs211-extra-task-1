ffced4b655177e62917f1b20642c991f4d05f3dc
elia-morrison <andsimof@gmail.com>
2020-09-27 21:45:29 +0300

    to_24_hour_clock implemented

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -23,32 +23,10 @@ double to_float_hours(int hours, int minutes, int seconds)
 
 double to_24_hour_clock(double hours)
 {
-    /*
-        hours is a number of hours since midnight. Return the
-        hour as seen on a 24-hour clock.
-
-        Precondition: hours >= 0
-
-        >>> to_24_hour_clock(24)
-        0
-        
-        >>> to_24_hour_clock(48)
-        0
-        
-        >>> to_24_hour_clock(25)
-        1
-        
-        >>> to_24_hour_clock(4)
-        4
-        
-        >>> to_24_hour_clock(28.5)
-        4.5
-        
-        You may wish to inspect various function in <cmath> to work
-        with integer and fractional part of a hours separately.
-        
-    */
-    return 0;
+    double intP;
+    double fractP = modf(hours, &intP);
+    long intpart = intP;
+    return intpart % 24 + fractP;
 }
 
 /*
@@ -153,5 +131,12 @@ int main()
     assert(fabs(to_float_hours(0, 15, 0) - 0.25) < DBL_EPSILON);
     assert(fabs(to_float_hours(2, 45, 9) - 2.7525) < DBL_EPSILON);
     assert(fabs(to_float_hours(1, 0, 36) - 1.01) < DBL_EPSILON);
+
+    //to_24_hour_clock
+    assert(fabs(to_24_hour_clock(24)) < DBL_EPSILON);
+    assert(fabs(to_24_hour_clock(48)) < DBL_EPSILON);
+    assert(fabs(to_24_hour_clock(25) - 1.0) < DBL_EPSILON);
+    assert(fabs(to_24_hour_clock(4) - 4.0) < DBL_EPSILON);
+    assert(fabs(to_24_hour_clock(28.5) - 4.5) < DBL_EPSILON);
     return 0;
 }
