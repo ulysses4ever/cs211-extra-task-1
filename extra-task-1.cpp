f5f7e0d558a9095c6adc3e22eb92012c906337ea
Ilya Ocheretov <ocheretov.ilya@yandex.ru>
2020-09-27 16:09:44 +0300

    hours_difference

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -12,22 +12,7 @@ double seconds_difference(double time_1, double time_2)
 
 double hours_difference(double time_1, double time_2)
 {
-    /*
-        Return the number of hours later that a time in seconds
-        time_2 is than a time in seconds time_1.
-            
-        >>> hours_difference(1800.0, 3600.0)
-        0.5
-
-        >>> hours_difference(3600.0, 1800.0)
-        -0.5
-
-        >>> hours_difference(1800.0, 2160.0)
-        0.1
-
-        >>> hours_difference(1800.0, 1800.0)
-        0.0
-    */
+    return (time_2 - time_1) / 3600;
 }
 
 double to_float_hours(int hours, int minutes, int seconds)
@@ -164,7 +149,12 @@ int main(){
     assert(fabs(seconds_difference(1800.0, 2160.0) - 360.0) < DBL_EPSILON);
     assert(fabs(seconds_difference(1800.0, 1800.0) - 0.0) < DBL_EPSILON);
     std::cout << "Tests seconds_difference completed" << std::endl; 
-
+    
+    assert(fabs(hours_difference(1800.0, 3600.0) - 0.5) < DBL_EPSILON);
+    assert(fabs(hours_difference(3600.0, 1800.0) - (-0.5)) < DBL_EPSILON);
+    assert(fabs(hours_difference(1800.0, 2160.0) - 0.1) < DBL_EPSILON);
+    assert(fabs(hours_difference(1800.0, 1800.0) - 0.0) < DBL_EPSILON);
+    std::cout << "Tests hours_difference completed" << std::endl;
     
     return 0;
 }
