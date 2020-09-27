26aa2d2eff972d66fe7b1cadcb040c2c17891596
Ilya Ocheretov <ocheretov.ilya@yandex.ru>
2020-09-27 16:38:53 +0300

    time_from_utc + fixes

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -44,38 +44,14 @@ int get_seconds(int seconds)
 
 double time_to_utc(int utc_offset, double time)
 {
-    return to_24_hour_clock(time - utc_offset);
+    int hours = (time - utc_offset) > 0 ? (time - utc_offset) : 24 + (time - utc_offset);
+    return to_24_hour_clock(hours);
 }
 
 double time_from_utc(int utc_offset, double time)
-{
-    /*
-        Return UTC time in time zone utc_offset.
-
-        >>> time_from_utc(+0, 12.0)
-        12.0
- 
-        >>> time_from_utc(+1, 12.0)
-        13.0
- 
-        >>> time_from_utc(-1, 12.0)
-        11.0
- 
-        >>> time_from_utc(+6, 6.0)
-        12.0
- 
-        >>> time_from_utc(-7, 6.0)
-        23.0
- 
-        >>> time_from_utc(-1, 0.0)
-        23.0
- 
-        >>> time_from_utc(-1, 23.0)
-        22.0
- 
-        >>> time_from_utc(+1, 23.0)
-        0.0
-    */
+{   
+    int hours = (time + utc_offset) > 0 ? (time + utc_offset) : 24 + (time + utc_offset);
+    return to_24_hour_clock(hours);
 }
 
 int main(){
@@ -115,6 +91,17 @@ int main(){
     assert(fabs(time_to_utc(-1, 0.0) - 1.0) < DBL_EPSILON);
     assert(fabs(time_to_utc(-1, 23.0) - 0.0) < DBL_EPSILON);
     std::cout << "Tests time_to_utc completed" << std::endl;
+
+    assert(fabs(time_from_utc(+0, 12.0) - 12.0) < DBL_EPSILON);
+    assert(fabs(time_from_utc(+1, 12.0) - 13.0) < DBL_EPSILON);
+    assert(fabs(time_from_utc(-1, 12.0) - 11.0) < DBL_EPSILON);
+    assert(fabs(time_from_utc(+6, 6.0) - 12.0) < DBL_EPSILON);
+    assert(fabs(time_from_utc(-7, 6.0) - 23.0) < DBL_EPSILON);
+    assert(fabs(time_from_utc(-1, 0.0) - 23.0) < DBL_EPSILON);
+    assert(fabs(time_from_utc(-1, 23.0) - 22.0) < DBL_EPSILON);
+    assert(fabs(time_from_utc(+1, 23.0) - 0.0) < DBL_EPSILON);
+    std::cout << "Tests time_from_utc completed" << std::endl;
+
 
     return 0;
 }
