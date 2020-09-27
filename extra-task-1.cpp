b606b39f6799c924d4dbadbf4e62748582659c34
handlessdude <handlessdude@gmail.com>
2020-09-27 17:31:12 +0300

    completed fourth task's implementation

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -1,4 +1,5 @@
 #include "extra-task-1.h"
+#include <cmath>
 
 //Return the number of seconds later that a time in seconds time_2 is than a time in seconds time_1.
 double seconds_difference(double time_1, double time_2)
@@ -25,31 +26,9 @@ double to_float_hours(int hours, int minutes, int seconds)
 //hours is a number of hours since midnight.Return the hour as seen on a 24 - hour clock.
 double to_24_hour_clock(double hours)
 {
-    /*
-        hours is a number of hours since midnight. Return the
-        hour as seen on a 24-hour clock.
-
-        Precondition: hours >= 0
-
-        >>> to_24_hour_clock(24)
-        0
-        
-        >>> to_24_hour_clock(48)
-        0
-        
-        >>> to_24_hour_clock(25)
-        1
-        
-        >>> to_24_hour_clock(4)
-        4
-        
-        >>> to_24_hour_clock(28.5)
-        4.5
-        
-        You may wish to inspect various function in <cmath> to work
-        with integer and fractional part of a hours separately.
-        
-    */
+    assert(hours >= 0);
+    int int_part = floor(hours);
+    return int_part % 24 + (hours - int_part);
 }
 
 /*
@@ -74,62 +53,62 @@ double to_24_hour_clock(double hours)
 */
 
 //Return time at UTC+0, where utc_offset is the number of hours away from UTC + 0.
-double time_to_utc(int utc_offset, double time)
-{
-    /*
-        Return time at UTC+0, where utc_offset is the number of hours away from
-        UTC+0.
-        You may be interested in:
-        https://en.wikipedia.org/wiki/Coordinated_Universal_Time
-
-        >>> time_to_utc(+0, 12.0)
-        12.0
- 
-        >>> time_to_utc(+1, 12.0)
-        11.0
- 
-        >>> time_to_utc(-1, 12.0)
-        13.0
- 
-        >>> time_to_utc(-11, 18.0)
-        5.0
- 
-        >>> time_to_utc(-1, 0.0)
-        1.0
- 
-        >>> time_to_utc(-1, 23.0)
-        0.0
-    */
-}
-
-//Return UTC time in time zone utc_offset.
-double time_from_utc(int utc_offset, double time)
-{
-    /*
-        Return UTC time in time zone utc_offset.
-
-        >>> time_from_utc(+0, 12.0)
-        12.0
- 
-        >>> time_from_utc(+1, 12.0)
-        13.0
- 
-        >>> time_from_utc(-1, 12.0)
-        11.0
- 
-        >>> time_from_utc(+6, 6.0)
-        12.0
- 
-        >>> time_from_utc(-7, 6.0)
-        23.0
- 
-        >>> time_from_utc(-1, 0.0)
-        23.0
- 
-        >>> time_from_utc(-1, 23.0)
-        22.0
- 
-        >>> time_from_utc(+1, 23.0)
-        0.0
-    */
-}
+//double time_to_utc(int utc_offset, double time)
+//{
+//    /*
+//        Return time at UTC+0, where utc_offset is the number of hours away from
+//        UTC+0.
+//        You may be interested in:
+//        https://en.wikipedia.org/wiki/Coordinated_Universal_Time
+//
+//        >>> time_to_utc(+0, 12.0)
+//        12.0
+// 
+//        >>> time_to_utc(+1, 12.0)
+//        11.0
+// 
+//        >>> time_to_utc(-1, 12.0)
+//        13.0
+// 
+//        >>> time_to_utc(-11, 18.0)
+//        5.0
+// 
+//        >>> time_to_utc(-1, 0.0)
+//        1.0
+// 
+//        >>> time_to_utc(-1, 23.0)
+//        0.0
+//    */
+//}
+//
+////Return UTC time in time zone utc_offset.
+//double time_from_utc(int utc_offset, double time)
+//{
+//    /*
+//        Return UTC time in time zone utc_offset.
+//
+//        >>> time_from_utc(+0, 12.0)
+//        12.0
+// 
+//        >>> time_from_utc(+1, 12.0)
+//        13.0
+// 
+//        >>> time_from_utc(-1, 12.0)
+//        11.0
+// 
+//        >>> time_from_utc(+6, 6.0)
+//        12.0
+// 
+//        >>> time_from_utc(-7, 6.0)
+//        23.0
+// 
+//        >>> time_from_utc(-1, 0.0)
+//        23.0
+// 
+//        >>> time_from_utc(-1, 23.0)
+//        22.0
+// 
+//        >>> time_from_utc(+1, 23.0)
+//        0.0
+//    */
+//}
