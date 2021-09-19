77973ab8f23ce4056c4245ae203a1fecac436d63
mybtw <ilyabigk@gmail.com>
2021-09-19 12:09:08 +0300

    added header and main

---
+++ header.h
@@ -0,0 +1,8 @@
+#ifndef header_
+#define header_
+
+//Return the number of seconds later that a time in seconds time_2 is than a time in seconds time_1.
+double seconds_difference(double , double );
+
+
+#endif  /* header_ */
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include"header.h"
+#include<cassert>
+int main()
+{
+	assert(seconds_difference(1800.0, 3600.0) == 1800.0);
+	assert(seconds_difference(3600.0, 1800.0) == -1800.0);
+	assert(seconds_difference(1800.0, 2160.0) == 360.0);
+	assert(seconds_difference(1800.0, 1800.0) == 0.0);
+}
