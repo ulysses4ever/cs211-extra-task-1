6b38452361a163ba680c504fc066f0a50e05caea
mybtw <ilyabigk@gmail.com>
2021-09-19 12:26:33 +0300

    to float hours

--- header.h
+++ header.h
@@ -1,3 +1,4 @@
+#include<cassert>
 #ifndef header_
 #define header_
 
@@ -5,6 +6,9 @@
 double seconds_difference(double , double );
 
 // Return the number of hours later that a time in seconds time_2 is than a time in seconds time_1.
-double hours_difference(double, double t);
+double hours_difference(double, double );
+
+//Return the total number of hours in the specified number of hours, minutes, and seconds.
+double to_float_hours(int, int, int);
 
 #endif  /* header_ */
--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include<iostream>
 #include"header.h"
-#include<cassert>
 int main()
 {
 	assert(seconds_difference(1800.0, 3600.0) == 1800.0);
@@ -12,5 +11,11 @@ int main()
 	assert(hours_difference(3600.0, 1800.0) == -0.5);
 	assert(hours_difference(1800.0, 2160.0) == 0.1);
 	assert(hours_difference(1800.0, 1800.0) == 0.0);
+
+
+	assert(to_float_hours(0, 15, 0) == 0.25);
+	assert(to_float_hours(2, 45, 9) == 2.7525);
+	assert(to_float_hours(1, 0, 36) == 1.01);
+
 
 }
