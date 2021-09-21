3cca6bb4091dcdf145ed0d06a91cf401c0ec87f4
Spoky_Loki <viter02@inbox.ru>
2021-09-21 21:20:16 +0300

    Last functiom Time_from_UTC

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <cassert>
 #include <cmath>
 #include <cfloat>
@@ -58,36 +59,11 @@ double time_to_utc(int utc_offset, double time)
     return to_24_hour_clock(((time - utc_offset) > 0) ? (time - utc_offset) : (24 + (time - utc_offset)));
 }
 
-//double time_from_utc(int utc_offset, double time)
-//{
-//    /*
-//        Return UTC time in time zone utc_offset.
-//
-//        >>> time_from_utc(+0, 12.0)
-//        12.0
-// 
-//        >>> time_from_utc(+1, 12.0)
-//        13.0
-// 
-//        >>> time_from_utc(-1, 12.0)
-//        11.0
-// 
-//        >>> time_from_utc(+6, 6.0)
-//        12.0
-// 
-//        >>> time_from_utc(-7, 6.0)
-//        23.0
-// 
-//        >>> time_from_utc(-1, 0.0)
-//        23.0
-// 
-//        >>> time_from_utc(-1, 23.0)
-//        22.0
-// 
-//        >>> time_from_utc(+1, 23.0)
-//        0.0
-//    */
-//}
+//Return UTC time in time zone utc_offset.
+double time_from_utc(int utc_offset, double time)
+{
+    return to_24_hour_clock((time + utc_offset) > 0 ? (time + utc_offset) : 24 + (time + utc_offset));
+}
 
 int main()
 {
@@ -98,6 +74,7 @@ int main()
     assert(fabs(seconds_difference(1800.0, 2160.0) - 360.0) < DBL_EPSILON);
     assert(fabs(seconds_difference(1800.0, 1800.0) - 0.0) < DBL_EPSILON);
 
+
     //Return the number of hours later that a time in seconds
     //time_2 is than a time in seconds time_1.
     assert(fabs(hours_difference(1800.0, 3600.0) - 0.5) < DBL_EPSILON);
@@ -105,11 +82,13 @@ int main()
     assert(fabs(hours_difference(1800.0, 2160.0) - 0.1) < DBL_EPSILON);
     assert(fabs(hours_difference(1800.0, 1800.0) - 0.0) < DBL_EPSILON);
 
+
     //Return the total number of hours in the specified number
     //of hours, minutes, and seconds.
     assert(fabs(to_float_hours(0, 15, 0) - 0.25) < DBL_EPSILON);
     assert(fabs(to_float_hours(2, 45, 9) - 2.7525) < DBL_EPSILON);
     assert(fabs(to_float_hours(1, 0, 36) - 1.01) < DBL_EPSILON);
+
 
     //hours is a number of hours since midnight. Return the
     //hour as seen on a 24-hour clock.
@@ -118,6 +97,7 @@ int main()
     assert(fabs(to_24_hour_clock(25) - 1) < DBL_EPSILON);
     assert(fabs(to_24_hour_clock(4) - 4) < DBL_EPSILON);
     assert(fabs(to_24_hour_clock(28.5) - 4.5) < DBL_EPSILON);
+
 
     /*Implement three functions
         * get_hours
@@ -129,6 +109,7 @@ int main()
     assert(get_minutes(3800) == 3);
     assert(get_seconds(3800) == 20);
 
+
     //Return time at UTC + 0, where utc_offset is the number of hours away from UTC + 0.
     assert(fabs(time_to_utc(+0, 12.0) - 12.0) < DBL_EPSILON);
     assert(fabs(time_to_utc(+1, 12.0) - 11.0) < DBL_EPSILON);
@@ -138,4 +119,15 @@ int main()
     assert(fabs(time_to_utc(-1, 23.0) - 0.0) < DBL_EPSILON);
 
 
+    //Return UTC time in time zone utc_offset.
+    assert(fabs(time_from_utc(+0, 12.0) - 12.0) < DBL_EPSILON);
+    assert(fabs(time_from_utc(+1, 12.0) - 13.0) < DBL_EPSILON);
+    assert(fabs(time_from_utc(-1, 12.0) - 11.0) < DBL_EPSILON);
+    assert(fabs(time_from_utc(+6, 6.0) - 12.0) < DBL_EPSILON);
+    assert(fabs(time_from_utc(-7, 6.0) - 23.0) < DBL_EPSILON);
+    assert(fabs(time_from_utc(-1, 0.0) - 23.0) < DBL_EPSILON);
+    assert(fabs(time_from_utc(-1, 23.0) - 22.0) < DBL_EPSILON);
+    assert(fabs(time_from_utc(+1, 23.0) - 0.0) < DBL_EPSILON);
+
+    std::cout << "All tests were successful!" << std::endl;
 }
