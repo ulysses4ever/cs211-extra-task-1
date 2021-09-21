1b0f26f262fbfc3804fd762206a544afc923fdd4
Spoky_Loki <viter02@inbox.ru>
2021-09-21 21:11:54 +0300

    Time_to_UTC function

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -52,34 +52,12 @@ int get_seconds(int seconds)
     return ((seconds % 3600) % 60) % 60;
 }
 
-//double time_to_utc(int utc_offset, double time)
-//{
-//    /*
-//        Return time at UTC+0, where utc_offset is the number of hours away from
-//        UTC+0.
-//        You may be interested in:
-//        https://en.wikipedia.org/wiki/Coordinated_Universal_Time
-//
-//        >>> time_to_utc(+0, 12.0)
-//        12.0
-// 
-//        >>> time_to_utc(+1, 12.0)
-//        11.0
-// 
-//        >>> time_to_utc(-1, 12.0)
-//        13.0
-// 
-//        >>> time_to_utc(-11, 18.0)
-//        5.0
-// 
-//        >>> time_to_utc(-1, 0.0)
-//        1.0
-// 
-//        >>> time_to_utc(-1, 23.0)
-//        0.0
-//    */
-//}
-//
+//Return time at UTC+0, where utc_offset is the number of hours away from UTC + 0.
+double time_to_utc(int utc_offset, double time)
+{
+    return to_24_hour_clock(((time - utc_offset) > 0) ? (time - utc_offset) : (24 + (time - utc_offset)));
+}
+
 //double time_from_utc(int utc_offset, double time)
 //{
 //    /*
@@ -150,4 +128,14 @@ int main()
     assert(get_hours(3800) == 1);
     assert(get_minutes(3800) == 3);
     assert(get_seconds(3800) == 20);
+
+    //Return time at UTC + 0, where utc_offset is the number of hours away from UTC + 0.
+    assert(fabs(time_to_utc(+0, 12.0) - 12.0) < DBL_EPSILON);
+    assert(fabs(time_to_utc(+1, 12.0) - 11.0) < DBL_EPSILON);
+    assert(fabs(time_to_utc(-1, 12.0) - 13.0) < DBL_EPSILON);
+    assert(fabs(time_to_utc(-11, 18.0) - 5.0) < DBL_EPSILON);
+    assert(fabs(time_to_utc(-1, 0.0) - 1.0) < DBL_EPSILON);
+    assert(fabs(time_to_utc(-1, 23.0) - 0.0) < DBL_EPSILON);
+
+
 }
