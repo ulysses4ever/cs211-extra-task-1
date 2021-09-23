b47f8abb439b2f76b05649c51b48491e134287ad
Aleksandr Kalinin <akalinin@sfedu.ru>
2021-09-23 13:38:42 +0300

    3) Добавил реализацию функции to_float_hours и тесты к ней

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -1,4 +1,6 @@
-
+#include <cfloat>
+#include <cassert>
+#include <cmath>
 //Return the number of seconds later that a time in seconds
 //time_2 is than a time in seconds time_1.
 double seconds_difference(double time_1, double time_2)
@@ -13,23 +15,14 @@ double hours_difference(double time_1, double time_2)
     return (time_2 - time_1) / 3600;
 }
 
+
+// Return the total number of hours in the specified number
+//of hours, minutes, and seconds.
 double to_float_hours(int hours, int minutes, int seconds)
 {
-    /*
-        Return the total number of hours in the specified number
-        of hours, minutes, and seconds.
-
-        Precondition: 0 <= minutes < 60  and  0 <= seconds < 60
-
-        >>> to_float_hours(0, 15, 0)
-        0.25
-
-        >>> to_float_hours(2, 45, 9)
-        2.7525
-
-        >>> to_float_hours(1, 0, 36)
-        1.01
-    */
+    assert(0 <= minutes && minutes < 60);
+    assert(0 <= seconds && seconds < 60);
+    return  (minutes / 60.0) + (seconds / 3600.0) + hours;
 }
 
 double to_24_hour_clock(double hours)
@@ -154,4 +147,9 @@ int main()
     assert(fabs(hours_difference(3600.0, 1800.0) - (-0.5)) < DBL_EPSILON);
     assert(fabs(hours_difference(1800.0, 2160.0) - 0.1) < DBL_EPSILON);
     assert(fabs(hours_difference(1800.0, 1800.0) - 0.0) < DBL_EPSILON);
+
+    //to_float_hours Tests
+    assert(fabs(to_float_hours(0, 15, 0) - 0.25) < DBL_EPSILON);
+    assert(fabs(to_float_hours(2, 45, 9) - 2.7525) < DBL_EPSILON);
+    assert(fabs(to_float_hours(1, 0, 36) - 1.01) < DBL_EPSILON);
 }
