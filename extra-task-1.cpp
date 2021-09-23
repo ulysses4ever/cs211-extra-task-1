906955312add80ff7e2b1cef1da3c9b6cb5c53c9
Aleksandr Kalinin <akalinin@sfedu.ru>
2021-09-23 11:46:39 +0300

    2) Добавил реализацию функции hours_difference

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -6,24 +6,11 @@ double seconds_difference(double time_1, double time_2)
     return time_2 - time_1;
 }
 
+//Return the number of hours later that a time in seconds
+//time_2 is than a time in seconds time_1.
 double hours_difference(double time_1, double time_2)
 {
-    /*
-        Return the number of hours later that a time in seconds
-        time_2 is than a time in seconds time_1.
-            
-        >>> hours_difference(1800.0, 3600.0)
-        0.5
-
-        >>> hours_difference(3600.0, 1800.0)
-        -0.5
-
-        >>> hours_difference(1800.0, 2160.0)
-        0.1
-
-        >>> hours_difference(1800.0, 1800.0)
-        0.0
-    */
+    return (time_2 - time_1) / 3600;
 }
 
 double to_float_hours(int hours, int minutes, int seconds)
