41d367af2571450b4cc74af9f05a715a01e45d97
Aleksandr Kalinin <akalinin@sfedu.ru>
2021-09-23 13:10:35 +0300

    Добавил main с тестами к двум функциям

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -140,3 +140,18 @@ double time_from_utc(int utc_offset, double time)
         0.0
     */
 }
+
+int main()
+{
+    //seconds_difference Tests
+    assert(fabs(seconds_difference(1800.0, 3600.0) - 1800.0) < DBL_EPSILON);
+    assert(fabs(seconds_difference(3600.0, 1800.0) - (-1800.0)) < DBL_EPSILON);
+    assert(fabs(seconds_difference(1800.0, 2160.0) - 360.0) < DBL_EPSILON);
+    assert(fabs(seconds_difference(1800.0, 1800.0) - 0.0) < DBL_EPSILON);
+
+    //hours_difference Tests
+    assert(fabs(hours_difference(1800.0, 3600.0) - 0.5) < DBL_EPSILON);
+    assert(fabs(hours_difference(3600.0, 1800.0) - (-0.5)) < DBL_EPSILON);
+    assert(fabs(hours_difference(1800.0, 2160.0) - 0.1) < DBL_EPSILON);
+    assert(fabs(hours_difference(1800.0, 1800.0) - 0.0) < DBL_EPSILON);
+}
