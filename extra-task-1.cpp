fc0bcf985729d31c26b84c77b40a3c369fed5869
Ekaterina0801 <ds.katrin@mail.ru>
2021-09-26 22:37:02 +0300

    Добавлена to_float_hours

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -44,6 +44,10 @@ double hours_difference(double time_1, double time_2)
 
 double to_float_hours(int hours, int minutes, int seconds)
 {
+    assert(minutes >= 0);
+    assert(minutes < 60);
+    assert(seconds >= 0 && seconds < 60);
+    return hours + (minutes / 60.0) + seconds / 3600.0;
     /*
         Return the total number of hours in the specified number
         of hours, minutes, and seconds.
