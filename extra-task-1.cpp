633221cd2ece755d3c273e85a38ecf7987c5f628
Danbka523 <gordonanet12@gmail.com>
2021-09-26 19:21:34 +0300

    Сделано задание 4

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -116,27 +116,21 @@ double to_24_hour_clock(double hours)
 >>>>>>> 5471b41 (Сделано задание 3 и удалены лишние комментарии)
 }
 
-/*
-    Implement three functions
-        * get_hours
-        * get_minutes
-        * get_seconds
-    They are used to determine the hours part, minutes part and seconds part 
-    of a time in seconds. E.g.:
+int get_hours(int seconds) {
+    assert(seconds >= 0);
+    return seconds / 3600;
+}
 
-    >>> get_hours(3800)
-    1
+int get_minutes(int seconds) {
+    assert(seconds >= 0);
+    return (seconds % 3600) / 60;
 
-    >>> get_minutes(3800)
-    3
+}
 
-    >>> get_seconds(3800)
-    20
-
-    In other words, if 3800 seconds have elapsed since midnight, 
-    it is currently 01:03:20 (hh:mm:ss).
-*/
-
+int get_seconds(int seconds) {
+    assert(seconds >= 0);
+    return  seconds % 3600 % 60 % 60;
+}
 double time_to_utc(int utc_offset, double time)
 {
     /*
--- extra-task-1.h
+++ extra-task-1.h
@@ -7,6 +7,13 @@ double seconds_difference(double time_1, double time_2);
 //Return the number of hours later that a time in seconds time_2 is than a time in seconds time_1.
 double hours_difference(double time_1, double time_2);
 //task3
-// hours is a number of hours since midnight. Return the hour as seen on a 24 - hour clock.
+//hours is a number of hours since midnight. Return the hour as seen on a 24 - hour clock.
 double to_24_hour_clock(double hours);
+//task4
+//Determine hours part
+int get_hours(int seconds);
+//determine minutes part
+int get_minutes(int seconds);
+//determine seconds part
+int get_seconds(int seconds);
 #endif
