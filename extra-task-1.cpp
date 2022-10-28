fc9b99eb1dcd0401fc46a85467542be0446616fa
mpustovoi <pustovoy31032003@mail.ru>
2022-10-28 16:44:14 +0300

    Добавлена функция main с тестами для всех функций

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -206,3 +206,69 @@ double time_from_utc(int utc_offset, double time)
         0.0
     */
 }
+
+//Тесты для функций
+int main()
+{
+    setlocale(LC_ALL, "RUSSIAN");
+
+    //Тест для seconds_difference 
+    assert(fabs(seconds_difference(1800.0, 3600.0) - 1800.0) < DBL_EPSILON);
+    assert(fabs(seconds_difference(3600.0, 1800.0) - (-1800.0)) < DBL_EPSILON);
+    assert(fabs(seconds_difference(1800.0, 2160.0) - 360.0) < DBL_EPSILON);
+    assert(fabs(seconds_difference(1800.0, 1800.0) - 0.0) < DBL_EPSILON);
+    cout << "Тесты для seconds_difference пройдены" << endl << endl;
+
+    //Тест для hours_difference 
+    assert(fabs(hours_difference(1800.0, 3600.0) - 0.5) < DBL_EPSILON);
+    assert(fabs(hours_difference(3600.0, 1800.0) - (-0.5)) < DBL_EPSILON);
+    assert(fabs(hours_difference(1800.0, 2160.0) - 0.1) < DBL_EPSILON);
+    assert(fabs(hours_difference(1800.0, 1800.0) - 0.0) < DBL_EPSILON);
+    cout << "Тесты для hours_difference пройдены" << endl << endl;
+
+    //Тест для to_float_hours
+    assert(fabs(to_float_hours(0, 15, 0) - 0.25) < DBL_EPSILON);
+    assert(fabs(to_float_hours(2, 45, 9) - 2.7525) < DBL_EPSILON);
+    assert(fabs(to_float_hours(1, 0, 36) - 1.01) < DBL_EPSILON);
+    cout << "Тесты для to_float_hours пройдены" << endl << endl;
+
+    //Тест для to_24_hour_clock tests
+    assert(fabs(to_24_hour_clock(24) - 0) < DBL_EPSILON);
+    assert(fabs(to_24_hour_clock(48) - 0) < DBL_EPSILON);
+    assert(fabs(to_24_hour_clock(25) - 1) < DBL_EPSILON);
+    assert(fabs(to_24_hour_clock(4) - 4) < DBL_EPSILON);
+    assert(fabs(to_24_hour_clock(28.5) - 4.5) < DBL_EPSILON);
+    cout << "Тесты для to_24_hour_clock пройдены" << endl << endl;
+
+    //Тест для get_hours
+    assert(get_hours(3800) == 1);
+    cout << "Тесты для get_hours пройдены" << endl << endl;
+
+    //Тест для get_minutes
+    assert(get_minutes(3800) == 3);
+    cout << "Тесты для get_minutes пройдены" << endl << endl;
+
+    //Тест для get_seconds 
+    assert(get_seconds(3800) == 20);
+    cout << "Тесты для get_seconds пройдены" << endl << endl;
+
+    //Тест для time_to_utc
+    assert(fabs(time_to_utc(+0, 12.0) - 12.0) < DBL_EPSILON);
+    assert(fabs(time_to_utc(+1, 12.0) - 11.0) < DBL_EPSILON);
+    assert(fabs(time_to_utc(-1, 12.0) - 13.0) < DBL_EPSILON);
+    assert(fabs(time_to_utc(-11, 18.0) - 5.0) < DBL_EPSILON);
+    assert(fabs(time_to_utc(-1, 0.0) - 1.0) < DBL_EPSILON);
+    assert(fabs(time_to_utc(-1, 23.0) - 0.0) < DBL_EPSILON);
+    cout << "Тесты для time_to_utc пройдены" << endl << endl;
+
+    //Тест для time_from_utc
+    assert(fabs(time_from_utc(+0, 12.0) - 12.0) < DBL_EPSILON);
+    assert(fabs(time_from_utc(+1, 12.0) - 13.0) < DBL_EPSILON);
+    assert(fabs(time_from_utc(-1, 12.0) - 11.0) < DBL_EPSILON);
+    assert(fabs(time_from_utc(+6, 6.0) - 12.0) < DBL_EPSILON);
+    assert(fabs(time_from_utc(-7, 6.0) - 23.0) < DBL_EPSILON);
+    assert(fabs(time_from_utc(-1, 0.0) - 23.0) < DBL_EPSILON);
+    assert(fabs(time_from_utc(-1, 23.0) - 22.0) < DBL_EPSILON);
+    assert(fabs(time_from_utc(1, 23.0) - 0.0) < DBL_EPSILON);
+    cout << "Тесты для time_from_utc пройдены" << endl;
+}
