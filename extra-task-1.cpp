e2c5984f16db80cd2f1b33bc25df167a64ec47b6
AlexanderZemlyak <zemlyaksanya@mail.ru>
2022-10-29 22:11:20 +0300

    Implement get_seconds function

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -58,26 +58,14 @@ inline int get_minutes(int seconds)
 	return seconds / 60 % 60;
 }
 
-/*
-	Implement three functions
-		* get_hours
-		* get_minutes
-		* get_seconds
-	They are used to determine the hours part, minutes part and seconds part
-	of a time in seconds. E.g.:
-
-	>>> get_hours(3800)
-	1
-
-	>>> get_minutes(3800)
-	3
-
-	>>> get_seconds(3800)
-	20
-
-	In other words, if 3800 seconds have elapsed since midnight,
-	it is currently 01:03:20 (hh:mm:ss).
-*/
+/// <summary>
+/// returns number of seconds passed after the closest integer number of minutes
+/// </summary>
+inline int get_seconds(int seconds)
+{
+	assert(seconds >= 0);
+	return seconds % 60;
+}
 
 double time_to_utc(int utc_offset, double time)
 {
@@ -177,4 +165,11 @@ int main()
 	assert(get_minutes(100) == 1);
 	assert(get_minutes(7200) == 0);
 	assert(get_minutes(19500) == 25);
+
+	// Тесты функции get_seconds
+	assert(get_seconds(3800) == 20);
+	assert(get_seconds(250) == 10);
+	assert(get_seconds(7200) == 0);
+	assert(get_seconds(19500) == 0);
+	assert(get_seconds(90942) == 42);
 }
