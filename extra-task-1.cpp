4a10f046fe512aebcf7c9925a886d3a625754a57
Grigoriy <grigoriymirosedi@gmail.com>
2022-10-30 23:00:49 +0300

    Created get_hours, get_minutes, get_seconds and tested them

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -33,26 +33,20 @@ double to_24_hour_clock(double hours)
     return ((int)(hours) % 24) + (hours - floor(hours));
 }
 
-/*
-    Implement three functions
-        * get_hours
-        * get_minutes
-        * get_seconds
-    They are used to determine the hours part, minutes part and seconds part 
-    of a time in seconds. E.g.:
+//getting hours from seconds
+int get_hours(int seconds) {
+    return seconds / 3600 % 24;
+}
 
-    >>> get_hours(3800)
-    1
+//getting minutes from seconds
+int get_minutes(int seconds) {
+    return seconds / 60 % 60;
+}
 
-    >>> get_minutes(3800)
-    3
-
-    >>> get_seconds(3800)
-    20
-
-    In other words, if 3800 seconds have elapsed since midnight, 
-    it is currently 01:03:20 (hh:mm:ss).
-*/
+//getting seconds from seconds 
+int get_seconds(int seconds) {
+    return seconds % 60;
+}
 
 double time_to_utc(int utc_offset, double time)
 {
@@ -140,4 +134,23 @@ int main() {
     assert(fabs(to_24_hour_clock(25) - 1) < DBL_EPSILON);
     assert(fabs(to_24_hour_clock(4) - 4) < DBL_EPSILON);
     assert(fabs(to_24_hour_clock(28.5) - 4.5) < DBL_EPSILON);
+
+    //testing get_hours...
+    assert(get_hours(3800) == 1);
+    assert(get_hours(7200) == 2);
+    assert(get_hours(90000) == 1);
+    assert(get_hours(0) == 0);
+
+    //testing get_minutes...
+    assert(get_minutes(3800) == 3);
+    assert(get_minutes(100) == 1);
+    assert(get_minutes(7200) == 0);
+    assert(get_minutes(19500) == 25);
+
+    //testing get_seconds
+    assert(get_seconds(3800) == 20);
+    assert(get_seconds(250) == 10);
+    assert(get_seconds(7200) == 0);
+    assert(get_seconds(19500) == 0);
+    assert(get_seconds(90942) == 42);
 }
