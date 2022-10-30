9a7868a282f98c75850cf2f134e392a308ae4ed0
Grigoriy <grigoriymirosedi@gmail.com>
2022-10-30 22:30:49 +0300

    Now seconds_difference works great!

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -3,27 +3,11 @@
 #include <cmath>
 #include <iostream>
 
+//Return the number of seconds later that a time in seconds
+//time_2 is than a time in seconds time_1.
 double seconds_difference(double time_1, double time_2)
 {
-    // your implementation goes here...
-
-    /*
-        Return the number of seconds later that a time in seconds
-        time_2 is than a time in seconds time_1.
-
-        >>> seconds_difference(1800.0, 3600.0)
-        1800.0
-
-        >>> seconds_difference(3600.0, 1800.0)
-        -1800.0
-
-        >>> seconds_difference(1800.0, 2160.0)
-        360.0
-
-        >>> seconds_difference(1800.0, 1800.0)
-        0.0
-    */
-    return 0;
+    return time_2 - time_1;
 }
 
 double hours_difference(double time_1, double time_2)
@@ -182,13 +166,13 @@ double time_from_utc(int utc_offset, double time)
 
 int main() {
 
-    /*
+    
     //testing seconds_diffrerence...
     assert(fabs(seconds_difference(1800.0, 3600.0) - 1800.0) < DBL_EPSILON);
 	assert(fabs(seconds_difference(3600.0, 1800.0) - (-1800.0)) < DBL_EPSILON);
 	assert(fabs(seconds_difference(1800.0, 2160.0) - 360.0) < DBL_EPSILON);
 	assert(fabs(seconds_difference(1800.0, 1800.0) - 0.0) < DBL_EPSILON);
-
+    /*
     //testing hours_difference...
     assert(fabs(hours_difference(1800.0, 3600.0) - 0.5) < DBL_EPSILON);
     assert(fabs(hours_difference(3600.0, 1800.0) - (-0.5)) < DBL_EPSILON);
