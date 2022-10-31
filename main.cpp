665b64482653d3b316dc7ec66559b8af497855e5
MovchanGitHub <92666028+MovchanGitHub@users.noreply.github.com>
2022-10-31 11:51:18 +0300

    add first function asserts

---
+++ main.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <assert.h>
+#include <cmath>
+#include "Header.h"
+
+using namespace std;
+
+int main()
+{
+	assert(abs(seconds_difference(1800.0, 3600.0) - 1800) == 0);
+	assert(abs(seconds_difference(3600.0, 1800.0) + 1800) == 0);
+	assert(abs(seconds_difference(1800.0, 2160.0) - 360) == 0);
+	assert(abs(seconds_difference(1800.0, 1800.0)) == 0);
+}
