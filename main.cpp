6cdd1c90bd74406da4f08b10e2fdd911b608169d
MovchanGitHub <92666028+MovchanGitHub@users.noreply.github.com>
2022-10-31 11:53:08 +0300

    add second function asserts

--- main.cpp
+++ main.cpp
@@ -11,4 +11,10 @@ int main()
 	assert(abs(seconds_difference(3600.0, 1800.0) + 1800) == 0);
 	assert(abs(seconds_difference(1800.0, 2160.0) - 360) == 0);
 	assert(abs(seconds_difference(1800.0, 1800.0)) == 0);
+
+
+	assert(abs(hours_difference(1800.0, 3600.0) - 0.5) == 0);
+	assert(abs(hours_difference(3600.0, 1800.0) + 0.5) == 0);
+	assert(abs(hours_difference(1800.0, 2160.0) - 0.1) == 0);
+	assert(abs(hours_difference(1800.0, 1800.0)) == 0);
 }
