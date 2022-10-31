491993954e8a608d7a7a0abced470ace52c27ef4
MovchanGitHub <92666028+MovchanGitHub@users.noreply.github.com>
2022-10-31 12:02:19 +0300

    add third function asserts

--- main.cpp
+++ main.cpp
@@ -17,4 +17,9 @@ int main()
 	assert(abs(hours_difference(3600.0, 1800.0) + 0.5) == 0);
 	assert(abs(hours_difference(1800.0, 2160.0) - 0.1) == 0);
 	assert(abs(hours_difference(1800.0, 1800.0)) == 0);
+
+
+	assert(abs(to_float_hours(0, 15, 0) - 0.25) <= DBL_EPSILON);
+	assert(abs(to_float_hours(2, 45, 9) - 2.7525) <= DBL_EPSILON);
+	assert(abs(to_float_hours(1, 0, 36) - 1.01) <= DBL_EPSILON);
 }
