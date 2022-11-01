10baeea3f0487bd47823a9b8c5afb28643ee0748
ana_sta_r <anastasiastar0611@gmail.com>
2022-11-01 18:26:48 +0300

    implement to_24_hour_clock

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -1,4 +1,5 @@
 #include <assert.h>
+#include <cmath>
 
 double seconds_difference(double time_1, double time_2)
 {
@@ -68,6 +69,8 @@ double to_float_hours(int hours, int minutes, int seconds)
 
 double to_24_hour_clock(double hours)
 {
+    assert(hours >= 0);
+    return std::fmod(hours, 24);
     /*
         hours is a number of hours since midnight. Return the
         hour as seen on a 24-hour clock.
