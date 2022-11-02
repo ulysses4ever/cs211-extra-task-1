aab71485f1387209abecf8a68bf647809b8160c4
Katrina0373 <mocalovak6@gmail.com>
2022-11-02 13:56:29 +0300

    implemented seconds_difference

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -1,7 +1,8 @@
 double seconds_difference(double time_1, double time_2)
 {
-    // your implementation goes here...
-    
+
+    return time_2 - time_1;
+
     /*    
         Return the number of seconds later that a time in seconds
         time_2 is than a time in seconds time_1.
@@ -22,6 +23,7 @@ double seconds_difference(double time_1, double time_2)
 
 double hours_difference(double time_1, double time_2)
 {
+    return 0;
     /*
         Return the number of hours later that a time in seconds
         time_2 is than a time in seconds time_1.
@@ -42,6 +44,7 @@ double hours_difference(double time_1, double time_2)
 
 double to_float_hours(int hours, int minutes, int seconds)
 {
+    return 0;
     /*
         Return the total number of hours in the specified number
         of hours, minutes, and seconds.
@@ -61,6 +64,7 @@ double to_float_hours(int hours, int minutes, int seconds)
 
 double to_24_hour_clock(double hours)
 {
+    return 0;
     /*
         hours is a number of hours since midnight. Return the
         hour as seen on a 24-hour clock.
@@ -111,6 +115,7 @@ double to_24_hour_clock(double hours)
 
 double time_to_utc(int utc_offset, double time)
 {
+    return 0;
     /*
         Return time at UTC+0, where utc_offset is the number of hours away from
         UTC+0.
@@ -139,6 +144,7 @@ double time_to_utc(int utc_offset, double time)
 
 double time_from_utc(int utc_offset, double time)
 {
+    return 0;
     /*
         Return UTC time in time zone utc_offset.
 
