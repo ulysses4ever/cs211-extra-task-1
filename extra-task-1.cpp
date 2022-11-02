293d2d1a07b830cddd0a204e0d1f4989ca32d326
Anastasia <keleshyan.nastya@bk.ru>
2022-11-02 21:11:01 +0300

    to_float_hours

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -12,7 +12,7 @@ double hours_difference(double time_1, double time_2)
 
 double to_float_hours(int hours, int minutes, int seconds)
 {
-    return hours + (minutes / 60) + (seconds / 3600);
+    return hours + (minutes / 60.0) + (seconds / 3600.0);
 }
 
 double to_24_hour_clock(double hours)
