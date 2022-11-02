01e01762306492905806e24e00f01a49bb288194
stashaKaktus <anna_konoplyanova@mail.ru>
2022-11-02 17:06:35 +0300

    Add to_float_hours

--- extra-task-1.h
+++ extra-task-1.h
@@ -1,3 +1,8 @@
+#pragma once
+#include <assert.h>
+
 double seconds_difference(double time_1, double time_2);
 
 double hours_difference(double time_1, double time_2);
+
+double to_float_hours(int hours, int minutes, int seconds);
--- main.cpp
+++ main.cpp
@@ -15,4 +15,9 @@ int main()
     assert(fabs(hours_difference(3600.0, 1800.0) + 0.5) <= DBL_EPSILON);
     assert(fabs(hours_difference(1800.0, 2160.0) - 0.1) <= DBL_EPSILON);
     assert(fabs(hours_difference(1800.0, 1800.0) - 0.0) <= DBL_EPSILON);
+
+    // ==== test to_float_hours ====
+    assert(fabs(to_float_hours(0, 15, 0) - 0.25) <= DBL_EPSILON);
+    assert(fabs(to_float_hours(2, 45, 9) - 2.7525) <= DBL_EPSILON);
+    assert(fabs(to_float_hours(1, 0, 36) - 1.01) <= DBL_EPSILON);
 }
