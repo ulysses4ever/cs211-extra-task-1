2033261f55502a5fb9859cd2527080e3b99acf77
Stanislav Leonchik <leonstas1@mail.ru>
2022-11-04 21:09:28 +0300

    Solve to_float_hours()

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -48,6 +48,8 @@ double hours_difference(double time_1, double time_2)
 
 double to_float_hours(int hours, int minutes, int seconds)
 {
+    return hours + (double)minutes / 60 + (double)seconds / 3600;
+
     /*
         Return the total number of hours in the specified number
         of hours, minutes, and seconds.
@@ -185,6 +187,9 @@ int main(){
     assert(fabs(hours_difference(1800.0, 2160.0) - 0.1) < DBL_EPSILON);
     assert(fabs(hours_difference(1800.0, 1800.0) - 0.0) < DBL_EPSILON);
 
+    assert(fabs(to_float_hours(0, 15, 0) - 0.25) < DBL_EPSILON);
+    assert(fabs(to_float_hours(2, 45, 9) - 2.7525) < DBL_EPSILON);
+    assert(fabs(to_float_hours(1, 0, 36) - 1.01) < DBL_EPSILON);
 
 
 
