1c3ae5514fd9158ecd369bc5f7f75310ef633c08
Stanislav Leonchik <leonstas1@mail.ru>
2022-11-04 21:07:02 +0300

    Solve seconds_difference()

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -1,7 +1,10 @@
+#include <cmath>
+
 double seconds_difference(double time_1, double time_2)
 {
     // your implementation goes here...
-    
+    return time_2 - time_1;
+
     /*    
         Return the number of seconds later that a time in seconds
         time_2 is than a time in seconds time_1.
@@ -166,4 +169,17 @@ double time_from_utc(int utc_offset, double time)
         >>> time_from_utc(+1, 23.0)
         0.0
     */
+}
+
+int main(){
+    assert(fabs(seconds_difference(1800.0, 3600.0) - 1800.0) < DBL_EPSILON);
+    assert(fabs(seconds_difference(3600.0, 1800.0) - -1800.0) < DBL_EPSILON);
+    assert(fabs(seconds_difference(1800.0, 2160.0) - 360.0) < DBL_EPSILON);
+    assert(fabs(seconds_difference(1800.0, 1800.0) - 0.0) < DBL_EPSILON);
+
+
+
+
+
+    return 0;
 }
