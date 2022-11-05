ef94315e0349b33df51b7e6e25a71309da6f346a
Dexight <92735754+Dexight@users.noreply.github.com>
2022-11-05 19:57:21 +0300

    +hours_difference

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -8,25 +8,11 @@ double seconds_difference(double time_1, double time_2)
     return time_2 - time_1;
 }
 
+/*Return the number of hours later that a time in seconds
+time_2 is than a time in seconds time_1.*/
 double hours_difference(double time_1, double time_2)
 {
-    return 0;
-    /*
-        Return the number of hours later that a time in seconds
-        time_2 is than a time in seconds time_1.
-            
-        >>> hours_difference(1800.0, 3600.0)
-        0.5
-
-        >>> hours_difference(3600.0, 1800.0)
-        -0.5
-
-        >>> hours_difference(1800.0, 2160.0)
-        0.1
-
-        >>> hours_difference(1800.0, 1800.0)
-        0.0
-    */
+    return seconds_difference(time_1, time_2) / 3600;
 }
 
 double to_float_hours(int hours, int minutes, int seconds)
@@ -169,4 +155,10 @@ int main()
     assert(fabs(seconds_difference(1800.0, 2160.0) - (360.0)) < DBL_EPSILON);
     assert(fabs(seconds_difference(1800.0, 1800.0)) < DBL_EPSILON);
     std::cout << "Tests #1 passed." << std::endl;
+    //task 2
+    assert(fabs(hours_difference(1800.0, 3600.0) - 0.5) < DBL_EPSILON);
+    assert(fabs(hours_difference(3600.0, 1800.0) - (-0.5)) < DBL_EPSILON);
+    assert(fabs(hours_difference(1800.0, 2160.0) - 0.1) < DBL_EPSILON);
+    assert(fabs(hours_difference(1800.0, 1800.0)) < DBL_EPSILON);
+    std::cout << "Tests #2 passed." << std::endl;
 }
