77756bc5daf1ff4afb484bd1b8849c913f2f784a
Dexight <92735754+Dexight@users.noreply.github.com>
2022-11-05 19:47:11 +0300

    +seconds_difference; +main.

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -1,27 +1,16 @@
+#include <iostream>
+#include <cmath>
+#include <cassert>
+/*Return the number of seconds later that a time in seconds
+time_2 is than a time in seconds time_1.*/
 double seconds_difference(double time_1, double time_2)
 {
-    // your implementation goes here...
-    
-    /*    
-        Return the number of seconds later that a time in seconds
-        time_2 is than a time in seconds time_1.
-            
-        >>> seconds_difference(1800.0, 3600.0)
-        1800.0
-
-        >>> seconds_difference(3600.0, 1800.0)
-        -1800.0
-
-        >>> seconds_difference(1800.0, 2160.0)
-        360.0
-
-        >>> seconds_difference(1800.0, 1800.0)
-        0.0
-    */
+    return time_2 - time_1;
 }
 
 double hours_difference(double time_1, double time_2)
 {
+    return 0;
     /*
         Return the number of hours later that a time in seconds
         time_2 is than a time in seconds time_1.
@@ -42,6 +31,7 @@ double hours_difference(double time_1, double time_2)
 
 double to_float_hours(int hours, int minutes, int seconds)
 {
+    return 0;
     /*
         Return the total number of hours in the specified number
         of hours, minutes, and seconds.
@@ -61,6 +51,7 @@ double to_float_hours(int hours, int minutes, int seconds)
 
 double to_24_hour_clock(double hours)
 {
+    return 0;
     /*
         hours is a number of hours since midnight. Return the
         hour as seen on a 24-hour clock.
@@ -111,6 +102,7 @@ double to_24_hour_clock(double hours)
 
 double time_to_utc(int utc_offset, double time)
 {
+    return 0;
     /*
         Return time at UTC+0, where utc_offset is the number of hours away from
         UTC+0.
@@ -139,6 +131,7 @@ double time_to_utc(int utc_offset, double time)
 
 double time_from_utc(int utc_offset, double time)
 {
+    return 0;
     /*
         Return UTC time in time zone utc_offset.
 
@@ -166,4 +159,14 @@ double time_from_utc(int utc_offset, double time)
         >>> time_from_utc(+1, 23.0)
         0.0
     */
+}
+
+int main()
+{
+    //task 1
+    assert(fabs(seconds_difference(1800.0, 3600.0) - 1800.0) < DBL_EPSILON);
+    assert(fabs(seconds_difference(3600.0, 1800.0) - (-1800.0)) < DBL_EPSILON);
+    assert(fabs(seconds_difference(1800.0, 2160.0) - (360.0)) < DBL_EPSILON);
+    assert(fabs(seconds_difference(1800.0, 1800.0)) < DBL_EPSILON);
+    std::cout << "Tests #1 passed." << std::endl;
 }
