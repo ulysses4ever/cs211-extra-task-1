b1e1bf3d125218360509aad2b8254f176febde93
DmytryPan <92853484+DmytryPan@users.noreply.github.com>
2022-11-05 16:03:35 +0300

    add seconds_difference

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -1,27 +1,13 @@
+#include<iostream>
+#include<cassert>
 double seconds_difference(double time_1, double time_2)
 {
-    // your implementation goes here...
-    
-    /*    
-        Return the number of seconds later that a time in seconds
-        time_2 is than a time in seconds time_1.
-            
-        >>> seconds_difference(1800.0, 3600.0)
-        1800.0
-
-        >>> seconds_difference(3600.0, 1800.0)
-        -1800.0
-
-        >>> seconds_difference(1800.0, 2160.0)
-        360.0
-
-        >>> seconds_difference(1800.0, 1800.0)
-        0.0
-    */
+    return time_2 - time_1;
 }
 
 double hours_difference(double time_1, double time_2)
 {
+    return 0.0;
     /*
         Return the number of hours later that a time in seconds
         time_2 is than a time in seconds time_1.
@@ -42,6 +28,8 @@ double hours_difference(double time_1, double time_2)
 
 double to_float_hours(int hours, int minutes, int seconds)
 {
+    return 0.0;
+
     /*
         Return the total number of hours in the specified number
         of hours, minutes, and seconds.
@@ -61,6 +49,8 @@ double to_float_hours(int hours, int minutes, int seconds)
 
 double to_24_hour_clock(double hours)
 {
+    return 0.0;
+
     /*
         hours is a number of hours since midnight. Return the
         hour as seen on a 24-hour clock.
@@ -111,6 +101,8 @@ double to_24_hour_clock(double hours)
 
 double time_to_utc(int utc_offset, double time)
 {
+    return 0.0;
+
     /*
         Return time at UTC+0, where utc_offset is the number of hours away from
         UTC+0.
@@ -139,6 +131,8 @@ double time_to_utc(int utc_offset, double time)
 
 double time_from_utc(int utc_offset, double time)
 {
+    return 0.0;
+
     /*
         Return UTC time in time zone utc_offset.
 
@@ -166,4 +160,14 @@ double time_from_utc(int utc_offset, double time)
         >>> time_from_utc(+1, 23.0)
         0.0
     */
+}
+
+int main() {
+    //task - 1
+    assert(seconds_difference(1800.0, 3600.0) == 1800.0);
+    assert(seconds_difference(3600.0, 1800.0) == -1800.0);
+    assert(seconds_difference(1800.0, 2160.0) == 360.0);
+    assert(seconds_difference(1800.0, 1800.0) == 0.0);
+    //task - 2
+
 }
