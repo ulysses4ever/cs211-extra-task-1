27a0dcec6e94e050ba0eac59f2faf8bd3d0e99d1
DmytryPan <92853484+DmytryPan@users.noreply.github.com>
2022-11-05 16:29:26 +0300

    add hours_difference

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -7,23 +7,8 @@ double seconds_difference(double time_1, double time_2)
 
 double hours_difference(double time_1, double time_2)
 {
-    return 0.0;
-    /*
-        Return the number of hours later that a time in seconds
-        time_2 is than a time in seconds time_1.
-            
-        >>> hours_difference(1800.0, 3600.0)
-        0.5
-
-        >>> hours_difference(3600.0, 1800.0)
-        -0.5
-
-        >>> hours_difference(1800.0, 2160.0)
-        0.1
-
-        >>> hours_difference(1800.0, 1800.0)
-        0.0
-    */
+    return (time_2 - time_1)/3600;
+   
 }
 
 double to_float_hours(int hours, int minutes, int seconds)
@@ -169,5 +154,25 @@ int main() {
     assert(seconds_difference(1800.0, 2160.0) == 360.0);
     assert(seconds_difference(1800.0, 1800.0) == 0.0);
     //task - 2
+     /*
+        Return the number of hours later that a time in seconds
+        time_2 is than a time in seconds time_1.
+
+        >>> hours_difference(1800.0, 3600.0)
+        0.5
+
+        >>> hours_difference(3600.0, 1800.0)
+        -0.5
+
+        >>> hours_difference(1800.0, 2160.0)
+        0.1
+
+        >>> hours_difference(1800.0, 1800.0)
+        0.0
+    */
+    assert(hours_difference(1800.0, 3600.0) == 0.5);
+    assert(hours_difference(3600.0, 1800.0) == -0.5);
+    assert(hours_difference(1800.0, 2160.0) == 0.1);
+    assert(hours_difference(1800.0, 1800.0) == 0.0);
 
 }
