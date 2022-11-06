209eaceb139d78463a2604f3a268e5846b18dfa9
Lola0122 <96561376+Lola0122@users.noreply.github.com>
2022-11-06 22:28:10 +0300

    проверка диапазона минут и секунд в to_float_hours

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -1,3 +1,4 @@
+#include <assert.h>
 double seconds_difference(double time_1, double time_2)
 {
     return time_2 - time_1;
@@ -10,6 +11,8 @@ double hours_difference(double time_1, double time_2)
 
 double to_float_hours(int hours, int minutes, int seconds)
 {
+    assert(minutes >= 0 && minutes < 60);
+    assert(seconds >= 0 && seconds < 60);
     return (hours + minutes * 1.0 / 60 + seconds * 1.0 / 3600);
 }
 
