28db3f31364ef4208c264d03b602265a8a63fb1d
Oleg Demianenko <odimianenko@sfedu.ru>
2022-11-06 21:12:41 +0300

    Implement function to_24_hour_clock

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -1,3 +1,5 @@
+#include <cmath>
+
 double seconds_difference(double time_1, double time_2)
 {
     return (time_2 - time_1);
@@ -65,6 +67,9 @@ double to_float_hours(int hours, int minutes, int seconds)
 
 double to_24_hour_clock(double hours)
 {
+
+    return (fmod(hours, 24.0));
+
     /*
         hours is a number of hours since midnight. Return the
         hour as seen on a 24-hour clock.
@@ -73,22 +78,22 @@ double to_24_hour_clock(double hours)
 
         >>> to_24_hour_clock(24)
         0
-        
+
         >>> to_24_hour_clock(48)
         0
-        
+
         >>> to_24_hour_clock(25)
         1
-        
+
         >>> to_24_hour_clock(4)
         4
-        
+
         >>> to_24_hour_clock(28.5)
         4.5
-        
+
         You may wish to inspect various function in <cmath> to work
         with integer and fractional part of a hours separately.
-        
+
     */
 }
 
