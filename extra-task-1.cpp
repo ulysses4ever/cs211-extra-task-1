42d924c267865ad9d0a32b68184155364fe9210d
UnicornTsar <92906941+UnicornTsar@users.noreply.github.com>
2022-11-07 00:07:29 +0300

    completed and tested to_float_hours function

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -12,22 +12,10 @@ double hours_difference(double time_1, double time_2)
 
 double to_float_hours(int hours, int minutes, int seconds)
 {
-    return 0;
-    /*
-        Return the total number of hours in the specified number
-        of hours, minutes, and seconds.
+    assert(0 <= minutes && minutes < 60);
+    assert(0 <= seconds && seconds < 60);
 
-        Precondition: 0 <= minutes < 60  and  0 <= seconds < 60
-
-        >>> to_float_hours(0, 15, 0)
-        0.25
-
-        >>> to_float_hours(2, 45, 9)
-        2.7525
-
-        >>> to_float_hours(1, 0, 36)
-        1.01
-    */
+    return hours+(minutes/60.0)+(seconds/3600.0);
 }
 
 double to_24_hour_clock(double hours)
