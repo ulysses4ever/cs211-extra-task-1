9939dadde6747deb3b7d4139b97624ca2a46bd20
Lisa <edombaian@sfedu.ru>
2022-11-06 21:29:21 +0300

    add asserts for time_from_utc func

--- main.cpp
+++ main.cpp
@@ -40,5 +40,14 @@ int main()
 	assert(fabs(time_to_utc(-1, 0.0) - 1.0) <= DBL_EPSILON);
 	assert(fabs(time_to_utc(-1, 23.0) - 0.0) <= DBL_EPSILON);
 
+	//asserts for time_from_utc func
+	assert(fabs(time_from_utc(+0, 12.0) - 12.0) <= DBL_EPSILON);
+	assert(fabs(time_from_utc(+1, 12.0) - 13.0) <= DBL_EPSILON);
+	assert(fabs(time_from_utc(-1, 12.0) - 11.0) <= DBL_EPSILON);
+	assert(fabs(time_from_utc(+6, 6.0)  - 12.0) <= DBL_EPSILON);
+	assert(fabs(time_from_utc(-7, 6.0)  - 23.0) <= DBL_EPSILON);
+	assert(fabs(time_from_utc(-1, 0.0)  - 23.0) <= DBL_EPSILON);
+	assert(fabs(time_from_utc(-1, 23.0) - 22.0) <= DBL_EPSILON);
+	assert(fabs(time_from_utc(+1, 23.0) - 0.0)  <= DBL_EPSILON);
 	std::cout << "end.";
 }
