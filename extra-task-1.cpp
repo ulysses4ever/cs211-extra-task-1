a465faf7193f5214ee94fb624f2f363ce85a2408
Oleg Demianenko <odimianenko@sfedu.ru>
2022-11-06 21:21:04 +0300

    Implement function time_to_utc

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -120,6 +120,8 @@ double to_24_hour_clock(double hours)
 
 double time_to_utc(int utc_offset, double time)
 {
+    return (fmod((time - utc_offset), 24.0));
+
     /*
         Return time at UTC+0, where utc_offset is the number of hours away from
         UTC+0.
@@ -128,19 +130,19 @@ double time_to_utc(int utc_offset, double time)
 
         >>> time_to_utc(+0, 12.0)
         12.0
- 
+
         >>> time_to_utc(+1, 12.0)
         11.0
- 
+
         >>> time_to_utc(-1, 12.0)
         13.0
- 
+
         >>> time_to_utc(-11, 18.0)
         5.0
- 
+
         >>> time_to_utc(-1, 0.0)
         1.0
- 
+
         >>> time_to_utc(-1, 23.0)
         0.0
     */
