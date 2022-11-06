a5d2112827d643097504c1d4e61eb9007d84ab6c
Lola0122 <96561376+Lola0122@users.noreply.github.com>
2022-11-06 23:03:40 +0300

    Реализовали time_to_utc

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -37,53 +37,10 @@ int get_seconds(int sec)
 {
     return (sec - 3600 * get_hours(sec) - 60 * get_minutes(sec)) / 60;
 }
-/*
-    Implement three functions
-        * get_hours
-        * get_minutes
-        * get_seconds
-    They are used to determine the hours part, minutes part and seconds part 
-    of a time in seconds. E.g.:
-
-    >>> get_hours(3800)
-    1
-
-    >>> get_minutes(3800)
-    3
-
-    >>> get_seconds(3800)
-    20
-
-    In other words, if 3800 seconds have elapsed since midnight, 
-    it is currently 01:03:20 (hh:mm:ss).
-*/
 
 double time_to_utc(int utc_offset, double time)
 {
-    /*
-        Return time at UTC+0, where utc_offset is the number of hours away from
-        UTC+0.
-        You may be interested in:
-        https://en.wikipedia.org/wiki/Coordinated_Universal_Time
-
-        >>> time_to_utc(+0, 12.0)
-        12.0
- 
-        >>> time_to_utc(+1, 12.0)
-        11.0
- 
-        >>> time_to_utc(-1, 12.0)
-        13.0
- 
-        >>> time_to_utc(-11, 18.0)
-        5.0
- 
-        >>> time_to_utc(-1, 0.0)
-        1.0
- 
-        >>> time_to_utc(-1, 23.0)
-        0.0
-    */
+    return to_24_hour_clock(time - utc_offset * 1.0);
 }
 
 double time_from_utc(int utc_offset, double time)
