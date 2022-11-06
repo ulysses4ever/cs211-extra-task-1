071a2f7928e415559501d3ffd54ac3f557aae221
cherevatovm <dop.work@yandex.ru>
2022-11-06 22:37:09 +0300

    Added seconds_difference

---
+++ main.cpp
@@ -0,0 +1,11 @@
+﻿#include <assert.h>
+#include "extra-task-1.h"
+#include <iostream>
+
+int main()
+{
+    assert(fabs(seconds_difference(1800.0, 3600.0) - 1800.0) < DBL_EPSILON);
+    assert(fabs(seconds_difference(3600.0, 1800.0) + 1800.0) < DBL_EPSILON);
+    assert(fabs(seconds_difference(1800.0, 2160.0) - 360.0) < DBL_EPSILON);
+    assert(fabs(seconds_difference(1800.0, 1800.0)) < DBL_EPSILON);
+}
