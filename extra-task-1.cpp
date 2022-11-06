d92f4fdc9942a90c65520a156998827ceff5c9db
XiennM <kmochalova@sfedu.ru>
2022-11-06 22:46:26 +0300

    complete time_from_utc

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -171,6 +171,8 @@ double time_to_utc(int utc_offset, double time)
 
 double time_from_utc(int utc_offset, double time)
 {
+    return to_24_hour_clock(utc_offset + time + 24);
+
     /*
         Return UTC time in time zone utc_offset.
 
@@ -237,4 +239,14 @@ void main()
     assert(fabs(time_to_utc(-11, 18.0) - 5) < DBL_EPSILON);
     assert(fabs(time_to_utc(-1, 0.0) - 1) < DBL_EPSILON);
     assert(fabs(time_to_utc(-1, 23.0) - 0) < DBL_EPSILON);
+
+
+    assert(fabs(time_from_utc(+0, 12.0) - 12) < DBL_EPSILON);
+    assert(fabs(time_from_utc(+1, 12.0) - 13) < DBL_EPSILON);
+    assert(fabs(time_from_utc(-1, 12.0) - 11) < DBL_EPSILON);
+    assert(fabs(time_from_utc(+6, 6.0) - 12) < DBL_EPSILON);
+    assert(fabs(time_from_utc(-7, 6.0) - 23) < DBL_EPSILON);
+    assert(fabs(time_from_utc(-1, 0.0) - 23) < DBL_EPSILON);
+    assert(fabs(time_from_utc(-1, 23.0) - 22) < DBL_EPSILON);
+    assert(fabs(time_from_utc(+1, 23.0) - 0) < DBL_EPSILON);
 }
