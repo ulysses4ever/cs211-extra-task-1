01a64c59051b8d86b7fa76c0449b716f12b307ff
XiennM <kmochalova@sfedu.ru>
2022-11-06 22:29:40 +0300

    complete to_24_hour_clock

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -73,6 +73,8 @@ double to_float_hours(int hours, int minutes, int seconds)
 
 double to_24_hour_clock(double hours)
 {
+    return hours - (int)(hours / 24) * 24;
+
     /*
         hours is a number of hours since midnight. Return the
         hour as seen on a 24-hour clock.
@@ -197,4 +199,11 @@ void main()
     assert(fabs(to_float_hours(0, 15, 0) - 0.25) < DBL_EPSILON);
     assert(fabs(to_float_hours(2, 45, 9) - 2.7525) < DBL_EPSILON);
     assert(fabs(to_float_hours(1, 0, 36) - 1.01) < DBL_EPSILON);
+
+
+    assert(fabs(to_24_hour_clock(24) - 0.0) < DBL_EPSILON);
+    assert(fabs(to_24_hour_clock(48) - 0.0) < DBL_EPSILON);
+    assert(fabs(to_24_hour_clock(25) - 1.0) < DBL_EPSILON);
+    assert(fabs(to_24_hour_clock(4) - 4.0) < DBL_EPSILON);
+    assert(fabs(to_24_hour_clock(28.5) - 4.5) < DBL_EPSILON);
 }
