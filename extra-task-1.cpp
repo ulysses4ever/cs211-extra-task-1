654154ce04ca515872cb273b33168ecc8678527f
cherevatovm <dop.work@yandex.ru>
2022-11-06 22:42:51 +0300

    Added to_float_hours

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -19,3 +19,13 @@ double hours_difference(double time_1, double time_2)
         time_2 is than a time in seconds time_1.
     */
 }
+
+double to_float_hours(int hours, int minutes, int seconds)
+{
+    assert(0 <= minutes < 60 && 0 <= seconds < 60);
+    return hours + (minutes / 60.0) + (seconds / 3600.0);
+    /*
+        Return the total number of hours in the specified number
+        of hours, minutes, and seconds.
+    */
+}
--- extra-task-1.h
+++ extra-task-1.h
@@ -2,3 +2,5 @@
 double seconds_difference(double, double);
 
 double hours_difference(double, double);
+
+double to_float_hours(int, int, int);
