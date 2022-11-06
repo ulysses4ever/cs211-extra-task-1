91d57baa5e24b66db59ee61f3d0211b11f28e320
Anton <antoniotetris150@gmail.com>
2022-11-06 11:06:01 +0300

    сделал Бонусное задание 2

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -1,3 +1,8 @@
+#include <iostream>
+#include <assert.h>
+using namespace std;
+
+
 double seconds_difference(double time_1, double time_2)
 {
     // your implementation goes here...
@@ -18,6 +23,7 @@ double seconds_difference(double time_1, double time_2)
         >>> seconds_difference(1800.0, 1800.0)
         0.0
     */
+	return time_2 - time_1;
 }
 
 double hours_difference(double time_1, double time_2)
@@ -38,6 +44,7 @@ double hours_difference(double time_1, double time_2)
         >>> hours_difference(1800.0, 1800.0)
         0.0
     */
+	return (time_2 / 3600) - (time_1 / 3600);
 }
 
 double to_float_hours(int hours, int minutes, int seconds)
@@ -57,6 +64,7 @@ double to_float_hours(int hours, int minutes, int seconds)
         >>> to_float_hours(1, 0, 36)
         1.01
     */
+	return hours + (minutes / 60) + (seconds / 3600);
 }
 
 double to_24_hour_clock(double hours)
@@ -86,8 +94,25 @@ double to_24_hour_clock(double hours)
         with integer and fractional part of a hours separately.
         
     */
+	while (hours >= 24)
+	{
+		hours -= 24;
+	}
+	return hours;
+	 
 }
 
+int get_hours(int seconds) {
+	return (seconds - (seconds % 3600)) / 3600;;
+}
+
+int get_minutes(int seconds) {
+	return (seconds - (get_hours(seconds) * 3600)) / 60;;
+}
+
+int get_seconds(int seconds) {
+	return seconds - (get_hours(seconds) * 3600) - (get_minutes(seconds) * 60);
+}
 /*
     Implement three functions
         * get_hours
@@ -135,6 +160,12 @@ double time_to_utc(int utc_offset, double time)
         >>> time_to_utc(-1, 23.0)
         0.0
     */
+	double result = time - utc_offset;
+	if (result >= 24.0)
+	{
+		result -= 24.0;
+	}
+	return result;
 }
 
 double time_from_utc(int utc_offset, double time)
@@ -166,4 +197,63 @@ double time_from_utc(int utc_offset, double time)
         >>> time_from_utc(+1, 23.0)
         0.0
     */
+	double result = time + utc_offset;
+	if (result >= 24.0)
+	{
+		result -= 24.0;
+	}
+	return result;
+}
+
+void main()
+{
+	setlocale(LC_ALL, "RUSSIAN");
+
+
+	assert(seconds_difference(1800.0, 3600.0) - 1800.0 < DBL_EPSILON);
+	assert(seconds_difference(3600.0, 1800.0) - -1800.0 < DBL_EPSILON);
+	assert(seconds_difference(1800.0, 2160.0) - 360.0 < DBL_EPSILON);
+	assert(seconds_difference(1800.0, 1800.0) - 0.0 < DBL_EPSILON);
+
+	assert(hours_difference(1800.0, 3600.0) - 0.5 < DBL_EPSILON);
+	assert(hours_difference(3600.0, 1800.0) - -0.5 < DBL_EPSILON);
+	assert(hours_difference(1800.0, 2160.0) - 0.1 < DBL_EPSILON);
+	assert(hours_difference(1800.0, 1800.0) - 0.0 < DBL_EPSILON);
+
+	assert(to_float_hours(0, 15, 0) - 0.25 < DBL_EPSILON);
+	assert(to_float_hours(2, 45, 9) - 2.7525 < DBL_EPSILON);
+	assert(to_float_hours(1, 0, 36) - 1.01 < DBL_EPSILON);
+
+	assert(to_24_hour_clock(24) - 0 < DBL_EPSILON);
+	assert(to_24_hour_clock(48) - 0 < DBL_EPSILON);
+	assert(to_24_hour_clock(25) - 1 < DBL_EPSILON);
+	assert(to_24_hour_clock(4) - 4 < DBL_EPSILON);
+	assert(to_24_hour_clock(28.5) - 4.5 < DBL_EPSILON);
+
+	assert(get_hours(3800) == 1);
+	assert(get_minutes(3800) == 3);
+	assert(get_seconds(3800) == 20);
+
+
+
+
+	assert(time_to_utc(+0, 12.0) - 12 < DBL_EPSILON);
+	assert(time_to_utc(+1, 12.0) - 11.0 < DBL_EPSILON);
+	assert(time_to_utc(-1, 12.0) - 13.0 < DBL_EPSILON);
+	assert(time_to_utc(-11, 18.0) - 5.0 < DBL_EPSILON);
+	assert(time_to_utc(-1, 0.0) - 1.0 < DBL_EPSILON);
+	assert(time_to_utc(-1, 23.0) - 0.0 < DBL_EPSILON);
+
+	assert(time_from_utc(+0, 12.0) - 12 < DBL_EPSILON);
+	assert(time_from_utc(+1, 12.0) - 13.0 < DBL_EPSILON);
+	assert(time_from_utc(-1, 12.0) - 11.0 < DBL_EPSILON);
+	assert(time_from_utc(+6, 6.0) - 12.0 < DBL_EPSILON);
+	assert(time_from_utc(-7, 6.0) - 23.0 < DBL_EPSILON);
+	assert(time_from_utc(-1, 0.0) - 23.0 < DBL_EPSILON);
+	assert(time_from_utc(-1, 23.0) - 22.0 < DBL_EPSILON);
+	assert(time_from_utc(+1, 23.0) - 0.0 < DBL_EPSILON);
+
+	
+
+	system("pause");
 }
