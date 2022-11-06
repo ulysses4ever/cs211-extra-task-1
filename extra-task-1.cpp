2efcbdba4fe83af1d580cf5740f03b80f095ce61
Lisa <edombaian@sfedu.ru>
2022-11-06 14:36:14 +0300

    add implement for hours_difference

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -5,20 +5,6 @@
 double seconds_difference(double time_1, double time_2)
 {
     return time_2 - time_1;
-    /*    
-            
-        >>> seconds_difference(1800.0, 3600.0)
-        1800.0
-
-        >>> seconds_difference(3600.0, 1800.0)
-        -1800.0
-
-        >>> seconds_difference(1800.0, 2160.0)
-        360.0
-
-        >>> seconds_difference(1800.0, 1800.0)
-        0.0
-    */
 }
 
 double hours_difference(double time_1, double time_2)
@@ -39,7 +25,7 @@ double hours_difference(double time_1, double time_2)
         >>> hours_difference(1800.0, 1800.0)
         0.0
     */
-    return 0;
+    return seconds_difference(time_1, time_2) / 3600;
 }
 
 double to_float_hours(int hours, int minutes, int seconds)
