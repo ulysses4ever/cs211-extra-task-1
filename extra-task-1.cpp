40d02c86356224718ed0b227bdd91675beb05522
cherevatovm <dop.work@yandex.ru>
2022-11-06 22:44:07 +0300

    Added to_24_hour_clock

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -29,3 +29,16 @@ double to_float_hours(int hours, int minutes, int seconds)
         of hours, minutes, and seconds.
     */
 }
+
+double to_24_hour_clock(double hours)
+{
+    assert(hours >= 0);
+    double fract_part;
+    double int_part;
+    fract_part = modf(hours, &int_part);
+    return ((int)int_part % 24) + fract_part;
+    /*
+        hours is a number of hours since midnight. Return the
+        hour as seen on a 24-hour clock.
+    */
+}
--- extra-task-1.h
+++ extra-task-1.h
@@ -4,3 +4,5 @@ double seconds_difference(double, double);
 double hours_difference(double, double);
 
 double to_float_hours(int, int, int);
+
+double to_24_hour_clock(double);
