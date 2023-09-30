0fce24fad433b25426d1b9e4daca909d7ba34017
dedicklo <145215000+dedicklo@users.noreply.github.com>
2023-09-30 14:30:27 +0300

    Исправление функции, добавление main
    Была исправлена функция seconds_difference, теперь она работает правильно для отрицательных чисел. Добавлен main, в котором прописаны assert'ы для этой же функции. Также подключены необходимые библиотеки.

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -1,6 +1,9 @@
+#include <cassert>
+#include <cmath>
+#include <cfloat>
 double seconds_difference(double time_1, double time_2)
 {
-    return time_2 - time1_;
+    return fabs(time_2 - time_1);
 }
 
 double hours_difference(double time_1, double time_2)
@@ -20,7 +23,9 @@ double hours_difference(double time_1, double time_2)
 
         >>> hours_difference(1800.0, 1800.0)
         0.0
+        
     */
+    return 0;
 }
 
 double to_float_hours(int hours, int minutes, int seconds)
@@ -40,6 +45,7 @@ double to_float_hours(int hours, int minutes, int seconds)
         >>> to_float_hours(1, 0, 36)
         1.01
     */
+    return 0;
 }
 
 double to_24_hour_clock(double hours)
@@ -69,6 +75,7 @@ double to_24_hour_clock(double hours)
         with integer and fractional part of a hours separately.
         
     */
+    return 0;
 }
 
 /*
@@ -90,6 +97,7 @@ double to_24_hour_clock(double hours)
 
     In other words, if 3800 seconds have elapsed since midnight, 
     it is currently 01:03:20 (hh:mm:ss).
+    
 */
 
 double time_to_utc(int utc_offset, double time)
@@ -118,6 +126,7 @@ double time_to_utc(int utc_offset, double time)
         >>> time_to_utc(-1, 23.0)
         0.0
     */
+    return 0;
 }
 
 double time_from_utc(int utc_offset, double time)
@@ -149,4 +158,13 @@ double time_from_utc(int utc_offset, double time)
         >>> time_from_utc(+1, 23.0)
         0.0
     */
+    return 0;
+}
+int main()
+{
+    // Тесты для seconds_difference
+    assert(fabs(seconds_difference(1800.0, 3600.0) - 1800.0) < DBL_EPSILON);
+    assert(fabs(seconds_difference(3600.0, 1800.0) - 1800.0) < DBL_EPSILON);
+    assert(fabs(seconds_difference(1800.0, 2160.0) - 360.0) < DBL_EPSILON);
+    assert(fabs(seconds_difference(1800.0, 1800.0)) < DBL_EPSILON);
 }
