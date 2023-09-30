2193c78f03719ded80939a3d9e333cb88170c421
dedicklo <145215000+dedicklo@users.noreply.github.com>
2023-09-30 14:12:46 +0300

    Добавлена реализация функции "seconds_difference"

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -1,23 +1,6 @@
 double seconds_difference(double time_1, double time_2)
 {
-    // your implementation goes here...
-    
-    /*    
-        Return the number of seconds later that a time in seconds
-        time_2 is than a time in seconds time_1.
-            
-        >>> seconds_difference(1800.0, 3600.0)
-        1800.0
-
-        >>> seconds_difference(3600.0, 1800.0)
-        -1800.0
-
-        >>> seconds_difference(1800.0, 2160.0)
-        360.0
-
-        >>> seconds_difference(1800.0, 1800.0)
-        0.0
-    */
+    return time_2 - time1_;
 }
 
 double hours_difference(double time_1, double time_2)
