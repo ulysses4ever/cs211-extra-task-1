da86f0b89d1eeccd452ce884df9fbe1afa98b9fd
Anastasia <apushkova@sfedu.ru>
2023-09-30 21:13:41 +0300

    implemented function to_24_hour_clock, added tests to this function

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -14,8 +14,15 @@ int main()
 	assert(fabs(hours_difference(1800.0, 2160.0) - 0.1) <= DBL_EPSILON);
 	assert(fabs(hours_difference(1800.0, 1800.0) - 0.0) <= DBL_EPSILON);
 
-	//Func 2
+	//Func 3
 	assert(fabs(to_float_hours(0, 15, 0) - 0.25) <= DBL_EPSILON);
 	assert(fabs(to_float_hours(2, 45, 9) - 2.7525) <= DBL_EPSILON);
 	assert(fabs(to_float_hours(1, 0, 36) - 1.01) <= DBL_EPSILON);
+
+	//Func 4
+	assert(fabs(to_24_hour_clock(24) - 0) <= DBL_EPSILON);
+	assert(fabs(to_24_hour_clock(48) - 0) <= DBL_EPSILON);
+	assert(fabs(to_24_hour_clock(25) - 1) <= DBL_EPSILON);
+	assert(fabs(to_24_hour_clock(4) - 4) <= DBL_EPSILON);
+	assert(fabs(to_24_hour_clock(28.5) - 4.5) <= DBL_EPSILON);
 }
