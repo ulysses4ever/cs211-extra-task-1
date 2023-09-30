fbade062dabed6860409581d7bf3d9f622cdc30b
Anastasia <apushkova@sfedu.ru>
2023-09-30 21:34:32 +0300

    implemented function time_to_utc, added tests to this function

--- Project1/Project1/extra-task-1.cpp
+++ Project1/Project1/extra-task-1.cpp
@@ -62,31 +62,11 @@ int get_seconds(int seconds)
 
 double time_to_utc(int utc_offset, double time)
 {
-    return 0;
-    /*
-        Return time at UTC+0, where utc_offset is the number of hours away from
-        UTC+0.
-        You may be interested in:
-        https://en.wikipedia.org/wiki/Coordinated_Universal_Time
-
-        >>> time_to_utc(+0, 12.0)
-        12.0
- 
-        >>> time_to_utc(+1, 12.0)
-        11.0
- 
-        >>> time_to_utc(-1, 12.0)
-        13.0
- 
-        >>> time_to_utc(-11, 18.0)
-        5.0
- 
-        >>> time_to_utc(-1, 0.0)
-        1.0
- 
-        >>> time_to_utc(-1, 23.0)
-        0.0
-    */
+    double x = time - utc_offset;
+    if (x >= 24)
+        x -= 24;
+    return x;
+    
 }
 
 double time_from_utc(int utc_offset, double time)
--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -31,4 +31,13 @@ int main()
 	assert(get_minutes(3800) == 3);
 	assert(get_seconds(3800) == 20);
 
+	//Func 8
+	assert(fabs(time_to_utc(+0, 12.0) - 12.0) <= DBL_EPSILON);
+	assert(fabs(time_to_utc(+1, 12.0) - 11.0) <= DBL_EPSILON);
+	assert(fabs(time_to_utc(-1, 12.0) - 13.0) <= DBL_EPSILON);
+	assert(fabs(time_to_utc(-11, 18.0) - 5.0) <= DBL_EPSILON);
+	assert(fabs(time_to_utc(-1, 0.0) - 1.0) <= DBL_EPSILON);
+	assert(fabs(time_to_utc(-1, 23.0) - 0.0) <= DBL_EPSILON);
+
+
 }
