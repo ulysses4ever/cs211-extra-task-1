e0183a4a6af43fc0df48f45763dc2a1c46e2a58a
Anastasia <apushkova@sfedu.ru>
2023-09-30 20:57:32 +0300

    implemented function hours_difference, added tests to the 1st 2 functions

--- Project1/Project1/extra-task-1.cpp
+++ Project1/Project1/extra-task-1.cpp
@@ -3,28 +3,13 @@
 
 double seconds_difference(double time_1, double time_2)
 {
-    // your implementation goes here...
     return time_2 - time_1;
-    /*    
-        Return the number of seconds later that a time in seconds
-        time_2 is than a time in seconds time_1.
-            
-        >>> seconds_difference(1800.0, 3600.0)
-        1800.0
-
-        >>> seconds_difference(3600.0, 1800.0)
-        -1800.0
-
-        >>> seconds_difference(1800.0, 2160.0)
-        360.0
-
-        >>> seconds_difference(1800.0, 1800.0)
-        0.0
-    */
+    
 }
 
 double hours_difference(double time_1, double time_2)
 {
+    return (time_2 - time_1) / 3600;
     /*
         Return the number of hours later that a time in seconds
         time_2 is than a time in seconds time_1.
@@ -45,7 +30,7 @@ double hours_difference(double time_1, double time_2)
 
 double to_float_hours(int hours, int minutes, int seconds)
 {
-    
+    return 0;
     /*
         Return the total number of hours in the specified number
         of hours, minutes, and seconds.
@@ -65,6 +50,7 @@ double to_float_hours(int hours, int minutes, int seconds)
 
 double to_24_hour_clock(double hours)
 {
+    return 0;
     /*
         hours is a number of hours since midnight. Return the
         hour as seen on a 24-hour clock.
@@ -115,6 +101,7 @@ double to_24_hour_clock(double hours)
 
 double time_to_utc(int utc_offset, double time)
 {
+    return 0;
     /*
         Return time at UTC+0, where utc_offset is the number of hours away from
         UTC+0.
@@ -143,6 +130,7 @@ double time_to_utc(int utc_offset, double time)
 
 double time_from_utc(int utc_offset, double time)
 {
+    return 0;
     /*
         Return UTC time in time zone utc_offset.
 
--- Project1/Project1/extra-task-1.h
+++ Project1/Project1/extra-task-1.h
@@ -1,5 +1,8 @@
 #pragma once
 
 #include <iostream>
+#include <assert.h>
 
 double seconds_difference(double time_1, double time_2);
+
+double hours_difference(double time_1, double time_2);
