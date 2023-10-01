45276b34e0976da191773cb5a6d6eff68999476d
dstrigin <dmitriystrigin04@gmail.com>
2023-10-01 12:57:48 +0300

    first function tests
    added tests to the first function to main file

--- extra-task-1.h
+++ extra-task-1.h
@@ -1,5 +1,7 @@
 #pragma once
 
+// checks if two double values are equal with accuracy of 10^-7
 bool equal(double, double);
 
+// returns the difference in seconds between two times given in seconds
 double seconds_difference(double time_1, double time_2);
--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 #include "extra-task-1.h"
+#include <cassert>
 
 int main() {
-	std::cout << seconds_difference(1800.0, 3600.0);
+
+	//task1
+
+	assert(equal(seconds_difference(3600.0, 1800.0), -1800.0));
+	assert(equal(seconds_difference(1800.0, 1800.0), 0.0));
+	assert(equal(seconds_difference(1800.0, 3600.0), 1800.0));
+	assert(equal(seconds_difference(256.0, 0.0), -256.0));
+	assert(equal(seconds_difference(123.1, 832.8), 709.7));
 }
