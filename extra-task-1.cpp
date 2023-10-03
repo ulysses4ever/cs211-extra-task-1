1b3a843d55c9166972f19160afab0ba44f37ba9e
Саня <ad.cherevko@gmail.com>
2023-10-03 21:54:17 +0300

    made others functions

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -22,6 +22,171 @@ double seconds_difference(double time_1, double time_2)
     */
 }
 
+double hours_difference(double time_1, double time_2)
+{
+    return (time_2 - time_1) / 3600.0;
+    /*
+        Return the number of hours later that a time in seconds
+        time_2 is than a time in seconds time_1.
+
+        >>> hours_difference(1800.0, 3600.0)
+        0.5
+
+        >>> hours_difference(3600.0, 1800.0)
+        -0.5
+
+        >>> hours_difference(1800.0, 2160.0)
+        0.1
+
+        >>> hours_difference(1800.0, 1800.0)
+        0.0
+    */
+}
+
+double to_float_hours(int hours, int minutes, int seconds)
+{
+    assert(0 <= minutes < 60 && 0<= seconds<60);
+    return hours + minutes / 60.0 + seconds / 3600.0;
+    /*
+        Return the total number of hours in the specified number
+        of hours, minutes, and seconds.
+
+        Precondition: 0 <= minutes < 60  and  0 <= seconds < 60
+
+        >>> to_float_hours(0, 15, 0)
+        0.25
+
+        >>> to_float_hours(2, 45, 9)
+        2.7525
+
+        >>> to_float_hours(1, 0, 36)
+        1.01
+    */
+}
+
+
+double to_24_hour_clock(double hours)
+{
+    assert(hours >=0);
+    return fmod(hours, 24.0);
+
+    /*
+        hours is a number of hours since midnight. Return the
+        hour as seen on a 24-hour clock.
+
+        Precondition: hours >= 0
+
+        >>> to_24_hour_clock(24)
+        0
+
+        >>> to_24_hour_clock(48)
+        0
+
+        >>> to_24_hour_clock(25)
+        1
+
+        >>> to_24_hour_clock(4)
+        4
+
+        >>> to_24_hour_clock(28.5)
+        4.5
+
+        You may wish to inspect various function in <cmath> to work
+        with integer and fractional part of a hours separately.
+
+    */
+}
+
+int get_hours(int seconds) {
+    return seconds / 3600;
+}
+
+int get_minutes(int seconds) {
+    return (seconds % 3600) / 60;
+}
+
+int get_seconds(int seconds) {
+    return seconds % 60;
+}
+
+double time_to_utc(int utc_offset, double time)
+{
+
+    double utcTime = time - utc_offset;
+    // Убедиться, что результат находится в пределах от 0 до 24 часам
+    while (utcTime < 0) {
+        utcTime += 24;
+    }
+    while (utcTime >= 24) {
+        utcTime -= 24;
+    }
+    return utcTime;
+    /*
+        Return time at UTC+0, where utc_offset is the number of hours away from
+        UTC+0.
+        You may be interested in:
+        https://en.wikipedia.org/wiki/Coordinated_Universal_Time
+
+        >>> time_to_utc(+0, 12.0)
+        12.0
+
+        >>> time_to_utc(+1, 12.0)
+        11.0
+
+        >>> time_to_utc(-1, 12.0)
+        13.0
+
+        >>> time_to_utc(-11, 18.0)
+        5.0
+
+        >>> time_to_utc(-1, 0.0)
+        1.0
+
+        >>> time_to_utc(-1, 23.0)
+        0.0
+    */
+}
+
+double time_from_utc(int utc_offset, double time)
+{
+
+    double localTime = time + utc_offset;
+    while (localTime < 0) {
+        localTime += 24;
+    }
+    while (localTime >= 24) {
+        localTime -= 24;
+    }
+    return localTime;
+    /*
+        Return UTC time in time zone utc_offset.
+
+        >>> time_from_utc(+0, 12.0)
+        12.0
+
+        >>> time_from_utc(+1, 12.0)
+        13.0
+
+        >>> time_from_utc(-1, 12.0)
+        11.0
+
+        >>> time_from_utc(+6, 6.0)
+        12.0
+
+        >>> time_from_utc(-7, 6.0)
+        23.0
+
+        >>> time_from_utc(-1, 0.0)
+        23.0
+
+        >>> time_from_utc(-1, 23.0)
+        22.0
+
+        >>> time_from_utc(+1, 23.0)
+        0.0
+    */
+}
+
 
 int main(){
 
