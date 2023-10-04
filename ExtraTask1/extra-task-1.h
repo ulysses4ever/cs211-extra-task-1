5cd63be5e9e0dd90f49ad645452f12af5f177089
Daria-Romanko <Diagic1000@gmail.com>
2023-10-05 01:08:24 +0300

    implementation of to_24_hour_clock

--- ExtraTask1/extra-task-1.h
+++ ExtraTask1/extra-task-1.h
@@ -12,4 +12,5 @@ double hours_difference(double time_1, double time_2);
 //Return the total number of hours in the specified number of hours, minutes, and seconds.
 double to_float_hours(int hours, int minutes, int seconds);
 
-
+//hours is a number of hours since midnight. Return the hour as seen on a 24 - hour clock.
+double to_24_hour_clock(double hours);
--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -19,32 +19,11 @@ double to_float_hours(int hours, int minutes, int seconds)
 
 double to_24_hour_clock(double hours)
 {
-    /*
-        hours is a number of hours since midnight. Return the
-        hour as seen on a 24-hour clock.
-
-        Precondition: hours >= 0
-
-        >>> to_24_hour_clock(24)
-        0
-        
-        >>> to_24_hour_clock(48)
-        0
-        
-        >>> to_24_hour_clock(25)
-        1
-        
-        >>> to_24_hour_clock(4)
-        4
-        
-        >>> to_24_hour_clock(28.5)
-        4.5
-        
-        You may wish to inspect various function in <cmath> to work
-        with integer and fractional part of a hours separately.
-        
-    */
-    return 0;
+    assert(hours >= 0);
+    double fractpart;
+    double intpart;
+    fractpart = modf(hours, &intpart);
+    return ((int)intpart % 24) + fractpart;
 
 }
 
