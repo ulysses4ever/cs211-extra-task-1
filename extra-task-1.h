ab2347ed26412a0424365c10f3e8fb7ae4625bd2
DanilK17 <danil.17.4.2004@gmail.com>
2023-10-05 22:04:59 +0300

    task2 done

--- extra-task-1.h
+++ extra-task-1.h
@@ -1,2 +1,6 @@
 #pragma once
+//Return the number of seconds later that a time in seconds
 double seconds_difference(double, double);
+
+//Return the number of hours later that a time in seconds
+double hours_difference(double, double);
--- main.cpp
+++ main.cpp
@@ -13,8 +13,15 @@ int main() {
     assert(abs(seconds_difference(3600.0, 1800.0) + 1800.0) < eps);
     assert(abs(seconds_difference(1800.0, 2160.0) - 360.0) < eps);
     assert(abs(seconds_difference(1800.0, 1800.0) - 0.0) < eps);
-    cout << "Тесты task1 прошли";
+    cout << "Тесты task1 прошли" << endl;
 
+    //Task2
+    assert(abs(hours_difference(1800.0, 3600.0) - 0.5) < eps);
+    assert(abs(hours_difference(3600.0, 1800.0) + 0.5) < eps);
+    assert(abs(hours_difference(1800.0, 2160.0) - 0.1) < eps);
+    assert(abs(hours_difference(1800.0, 1800.0) - 0.0) < eps);
+    cout << "Тесты task2 прошли" << endl;
+    
 
 
 
