4d80d6bb3f28ad62970e04b3b74c9d33e919d61a
Kris <kristinaschleikel@gmail.com>
2023-10-05 15:15:39 +0300

    fixed Get_Seconds

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -10,13 +10,13 @@ double seconds_difference(double time_1, double time_2)
 //Return the number of hours later that a time in seconds time_2 is than a time in seconds time_1.
 double hours_difference(double time_1, double time_2)
 {
-    return (time_2-time_1)/3600;
+    return (time_2 - time_1) / 3600;
 }
 
 //Return the total number of hours in the specified number of hours, minutes, and seconds.
 double to_float_hours(int hours, int minutes, int seconds)
 {
-    assert(0 <= minutes < 60 and 0 <= seconds < 60);
+    assert(0 <= minutes && minutes< 60 && 0 <= seconds && seconds < 60);
     return hours + (minutes / 60.0) + (seconds / 3600.0);
 }
 //hours is a number of hours since midnight. Return the hour as seen on a 24 - hour clock.
@@ -40,7 +40,7 @@ int Get_Minutes(int seconds)
 //Returns the seconds part of a time in seconds.
 int Get_Seconds(int seconds)
 {
-    return ((seconds % 3600) % 60) % 60;
+    return (seconds % 3600) % 60;
 }
 // Return time at UTC+0, where utc_offset is the number of hours away from UTC + 0.
 double time_to_utc(int utc_offset, double time)
