9cf94185d8d6e099b16aafad3bd4419608cb6e74
Игорь Кудрявцев <115657732+teachyyou@users.noreply.github.com>
2023-10-06 15:46:53 +0300

    to_24_hour_clock() implementation and tests

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <assert.h>
+#include <cmath>
 
 double seconds_difference(double time_1, double time_2)
 {
@@ -18,51 +19,15 @@ double to_float_hours(int hours, int minutes, int seconds)
     assert(seconds >= 0 && seconds <= 60);
 
     return hours + minutes / 60.0 + seconds / 3600.0;
-    /*
-        Return the total number of hours in the specified number
-        of hours, minutes, and seconds.
-
-        Precondition: 0 <= minutes < 60  and  0 <= seconds < 60
-
-        >>> to_float_hours(0, 15, 0)
-        0.25
-
-        >>> to_float_hours(2, 45, 9)
-        2.7525
-
-        >>> to_float_hours(1, 0, 36)
-        1.01
-    */
 }
 
 double to_24_hour_clock(double hours)
 {
-    return 1;
-    /*
-        hours is a number of hours since midnight. Return the
-        hour as seen on a 24-hour clock.
+    assert(hours >= 0);
 
-        Precondition: hours >= 0
-
-        >>> to_24_hour_clock(24)
-        0
-
-        >>> to_24_hour_clock(48)
-        0
-
-        >>> to_24_hour_clock(25)
-        1
-
-        >>> to_24_hour_clock(4)
-        4
-
-        >>> to_24_hour_clock(28.5)
-        4.5
-
-        You may wish to inspect various function in <cmath> to work
-        with integer and fractional part of a hours separately.
-
-    */
+    double mins = hours - floor(hours);
+    int inthour = floor(hours);
+    return (inthour % 24 + mins);
 }
 
 /*
@@ -148,22 +113,6 @@ double time_from_utc(int utc_offset, double time)
 }
 
 
-/*
-        Return the total number of hours in the specified number
-        of hours, minutes, and seconds.
-
-        Precondition: 0 <= minutes < 60  and  0 <= seconds < 60
-
-        >>> to_float_hours(0, 15, 0)
-        0.25
-
-        >>> to_float_hours(2, 45, 9)
-        2.7525
-
-        >>> to_float_hours(1, 0, 36)
-        1.01
-    */
-
 void main() {
 
     assert(abs(seconds_difference(1800.0, 3600.0) - 1800.0) < DBL_EPSILON);
@@ -179,5 +128,11 @@ void main() {
     assert(abs(to_float_hours(0, 15, 0) - 0.25) < DBL_EPSILON);
     assert(abs(to_float_hours(2,45,9) - 2.7525) < DBL_EPSILON);
     assert(abs(to_float_hours(1, 0, 36) - 1.01) < DBL_EPSILON);
+
+    assert(abs(to_24_hour_clock(24)) < DBL_EPSILON);
+    assert(abs(to_24_hour_clock(48)) < DBL_EPSILON);
+    assert(abs(to_24_hour_clock(25)-1) < DBL_EPSILON);
+    assert(abs(to_24_hour_clock(4)-4) < DBL_EPSILON);
+    assert(abs(to_24_hour_clock(28.5)-4.5) < DBL_EPSILON);
 
 }
