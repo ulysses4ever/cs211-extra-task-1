c742340abb7a1007be21e09896a7998747d441ee
L1v1ngston <115081879+L1v1ngston@users.noreply.github.com>
2023-10-06 18:09:30 +0300

    Реализована функция 4.3.

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -116,6 +116,10 @@ int get_hours(int seconds) {
     return seconds / 3600;
 }
 
+int get_minutes(int seconds) {
+    return (seconds % 3600) / 60;
+}
+
 double time_to_utc(int utc_offset, double time)
 {
     /*
