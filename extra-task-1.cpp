067aa2e7f3a35fbebf0ef32548a7a5a033bc63fa
Shkobarev <danila564321@gmail.com>
2023-10-06 23:23:44 +0300

    Сделал функцию time_from_utc и к ней assert

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -162,7 +162,12 @@ double time_to_utc(int utc_offset, double time)
 
 double time_from_utc(int utc_offset, double time)
 {
-    return 0;
+    double res = time + utc_offset;
+    if (res == 0.0)
+        return 0.0;
+    else if (res > 0)
+        return fmod(res, 24);
+    else return 24 - fmod(abs(res), 24);
     /*
         Return UTC time in time zone utc_offset.
 
@@ -233,4 +238,13 @@ int main()
     assert(fabs(time_to_utc(-11, 18.0) - 5.0) < DBL_EPSILON);
     assert(fabs(time_to_utc(-1, 0.0) - 1.0) < DBL_EPSILON);
     assert(fabs(time_to_utc(-1, 23.0) - 0.0) < DBL_EPSILON);
+
+    assert(fabs(time_from_utc(+0, 12.0) - 12.0) < DBL_EPSILON);
+    assert(fabs(time_from_utc(+1, 12.0) - 13.0) < DBL_EPSILON);
+    assert(fabs(time_from_utc(-1, 12.0) - 11.0) < DBL_EPSILON);
+    assert(fabs(time_from_utc(+6, 6.0) - 12.0) < DBL_EPSILON);
+    assert(fabs(time_from_utc(-7, 6.0) - 23.0) < DBL_EPSILON);
+    assert(fabs(time_from_utc(-1, 0.0) - 23.0) < DBL_EPSILON);
+    assert(fabs(time_from_utc(-1, 23.0) - 22.0) < DBL_EPSILON);
+    assert(fabs(time_from_utc(+1, 23.0) - 0.0) < DBL_EPSILON);
 }
