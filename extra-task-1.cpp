b912c732727167caed72bd0a901a7079d9f5f42d
Nia <108984073+kawaii-Code@users.noreply.github.com>
2023-10-06 18:11:10 +0300

    Сделал get_hours, get_minutes и get_seconds

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -68,18 +68,20 @@ double to_24_hour_clock(double hours)
     They are used to determine the hours part, minutes part and seconds part 
     of a time in seconds. E.g.:
 
-    >>> get_hours(3800)
-    1
-
-    >>> get_minutes(3800)
-    3
-
-    >>> get_seconds(3800)
-    20
-
     In other words, if 3800 seconds have elapsed since midnight, 
     it is currently 01:03:20 (hh:mm:ss).
 */
+int get_hours(int seconds) {
+    return seconds / (60 * 60);
+}
+
+int get_minutes(int seconds) {
+    return (seconds / 60) % 60;
+}
+
+int get_seconds(int seconds) {
+    return seconds % 60;
+}
 
 double time_to_utc(int utc_offset, double time)
 {
