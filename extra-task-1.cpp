c116287d4c8ffda45d2f9318383175d63ddcbcec
Linux Torbolts <114951816+MakimaGorky@users.noreply.github.com>
2023-10-06 14:11:15 +0300

    utc here

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -131,6 +131,7 @@ int get_seconds(int seconds) {
 
 double time_to_utc(int utc_offset, double time)
 {
+    return time - utc_offset;
     /*
         Return time at UTC+0, where utc_offset is the number of hours away from
         UTC+0.
