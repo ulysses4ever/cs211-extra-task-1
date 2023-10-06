7144a99432772368a31bcc5bdb61d6177e0dd3fc
Nia <108984073+kawaii-Code@users.noreply.github.com>
2023-10-06 18:10:45 +0300

    Сделал to_24_hour_clock

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -43,30 +43,20 @@ double to_float_hours(int hours, int minutes, int seconds)
 
 double to_24_hour_clock(double hours)
 {
+    assert(hours >= 0);
+
+    int integerPart = (int)hours;
+    double fractionalPart = hours - integerPart;
+    return integerPart % 24 + fractionalPart;
+
     /*
         hours is a number of hours since midnight. Return the
         hour as seen on a 24-hour clock.
 
         Precondition: hours >= 0
 
-        >>> to_24_hour_clock(24)
-        0
-        
-        >>> to_24_hour_clock(48)
-        0
-        
-        >>> to_24_hour_clock(25)
-        1
-        
-        >>> to_24_hour_clock(4)
-        4
-        
-        >>> to_24_hour_clock(28.5)
-        4.5
-        
         You may wish to inspect various function in <cmath> to work
         with integer and fractional part of a hours separately.
-        
     */
 }
 
