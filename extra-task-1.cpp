2b69588b1ff915cfb39cb74d9c597cc9a9f1a6c7
L1v1ngston <115081879+L1v1ngston@users.noreply.github.com>
2023-10-06 18:09:02 +0300

    Реализована функция 4.2.

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -112,6 +112,9 @@ double to_24_hour_clock(double hours)
     In other words, if 3800 seconds have elapsed since midnight, 
     it is currently 01:03:20 (hh:mm:ss).
 */
+int get_hours(int seconds) {
+    return seconds / 3600;
+}
 
 double time_to_utc(int utc_offset, double time)
 {
