a3c001852ef1190e8153b35eb9f98a89270d30bd
nktfmnko <nktfmnko@gmail.com>
2023-10-06 15:31:34 +0300

    add three functions to determine time part in seconds

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -117,6 +117,17 @@ double to_24_hour_clock(double hours)
     In other words, if 3800 seconds have elapsed since midnight, 
     it is currently 01:03:20 (hh:mm:ss).
 */
+double get_hours(double time) {
+    return (int)(time / 3600);
+}
+
+double get_minutes(double time) {
+    return (int)time % 3600 / 60;
+}
+
+double get_seconds(double time) {
+    return (int)time % 3600 % 60;
+}
 
 double time_to_utc(int utc_offset, double time)
 {
