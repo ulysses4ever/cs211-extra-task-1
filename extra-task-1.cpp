34f127b4cc8a05d82d95c4487e41a548d5921b2e
Shkobarev <danila564321@gmail.com>
2023-10-06 23:09:34 +0300

    Сделал функцию time_to_utc и к ней assert

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -133,7 +133,7 @@ int get_seconds(int sec)
 
 double time_to_utc(int utc_offset, double time)
 {
-    return 0;
+    return fmod(time-utc_offset,24);
     /*
         Return time at UTC+0, where utc_offset is the number of hours away from
         UTC+0.
@@ -226,4 +226,11 @@ int main()
     assert(get_seconds(3800) == 20);
     assert(get_seconds(2002) == 22);
     assert(get_seconds(7981) == 1);
+
+    assert(fabs(time_to_utc(+0, 12.0) - 12.0) < DBL_EPSILON);
+    assert(fabs(time_to_utc(+1, 12.0) - 11.0) < DBL_EPSILON);
+    assert(fabs(time_to_utc(-1, 12.0) - 13.0) < DBL_EPSILON);
+    assert(fabs(time_to_utc(-11, 18.0) - 5.0) < DBL_EPSILON);
+    assert(fabs(time_to_utc(-1, 0.0) - 1.0) < DBL_EPSILON);
+    assert(fabs(time_to_utc(-1, 23.0) - 0.0) < DBL_EPSILON);
 }
