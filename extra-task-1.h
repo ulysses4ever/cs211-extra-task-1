d634a07dbb9e1489c2b5a6ef54bd3d0c05ecf652
Antonenko Aleksandr <sa0529083@gmail.com>
2023-10-06 23:10:35 +0300

    Added implementation of the seconds_difference function, file extra-task-1.h, main.cpp and checking of the seconds_difference function in the main file.

---
+++ extra-task-1.h
@@ -0,0 +1,23 @@
+#pragma once
+
+//Return the number of seconds later that a time in seconds
+//time_2 is than a time in seconds time_1.
+double seconds_difference(double, double);
+
+//Return the number of hours later that a time in seconds
+//time_2 is than a time in seconds time_1.
+double hours_difference(double, double);
+
+//Return the total number of hours in the specified number
+//of hours, minutes, and seconds.
+double to_float_hours(int, int, int);
+
+// hours is a number of hours since midnight. Return the
+//hour as seen on a 24 - hour clock.
+double to_24_hour_clock(double);
+
+//Return time at UTC+0, where utc_offset is the number of hours away from UTC + 0.
+double time_to_utc(int, double);
+
+//Return UTC time in time zone utc_offset.
+double time_from_utc(int, double);
---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include "extra-task-1.h"
+#include <assert.h>
+#include <math.h>
+#include <float.h>
+#include <iostream>
+
+void main()
+{
+	//seconds_difference
+	assert(fabs(seconds_difference(1800.0, 3600.0) - 1800.0) <= DBL_EPSILON);
+	assert(fabs(seconds_difference(3600.0, 1800.0) - (-1800.0)) <= DBL_EPSILON);
+	assert(fabs(seconds_difference(1800.0, 2160.0) - 360.0) <= DBL_EPSILON);
+	assert(fabs(seconds_difference(1800.0, 1800.0)) <= DBL_EPSILON);
+
+	
+}
