acc553aca7b5cfb0d4f63e10e3d70a75fb6f3ae2
Nia <108984073+kawaii-Code@users.noreply.github.com>
2023-10-06 18:07:58 +0300

    Сделал seconds_difference

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -1,22 +1,15 @@
+#include <cassert>
+
 double seconds_difference(double time_1, double time_2)
 {
-    // your implementation goes here...
-    
-    /*    
+    assert(time_1 >= 0);
+    assert(time_2 >= 0);
+
+    return time_2 - time_1;
+
+    /*
         Return the number of seconds later that a time in seconds
         time_2 is than a time in seconds time_1.
-            
-        >>> seconds_difference(1800.0, 3600.0)
-        1800.0
-
-        >>> seconds_difference(3600.0, 1800.0)
-        -1800.0
-
-        >>> seconds_difference(1800.0, 2160.0)
-        360.0
-
-        >>> seconds_difference(1800.0, 1800.0)
-        0.0
     */
 }
 
