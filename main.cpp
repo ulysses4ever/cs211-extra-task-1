19c6a644ede3f7f35bb0fe383bd0182f69ea3d33
Виссарионова Анна <anna.ogneva.455@mail.ru>
2023-10-07 19:10:31 +0300

    added testd for function to_float_hours

--- main.cpp
+++ main.cpp
@@ -20,5 +20,12 @@ int main() {
 	assert(fabs(hours_difference(1800.0, 2160.0) - 0.1) < DBL_EPSILON);
 	assert(fabs(hours_difference(1800.0, 1800.0)) < DBL_EPSILON);
 
+
+	//to_float_hours tests
+	assert(fabs(to_float_hours(0, 15, 0) - 0.25) < DBL_EPSILON);
+	assert(fabs(to_float_hours(2, 45, 9) - 2.7525) < DBL_EPSILON);
+	assert(fabs(to_float_hours(1, 0, 36) - 1.01) < DBL_EPSILON);
+
+
 }
 
