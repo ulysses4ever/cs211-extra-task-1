95e66d239d81368464781cf5a2865b0a3dde31d7
Руслан Бичурин <bichurin.ruslan.romanovich@yandex.ru>
2023-10-07 22:20:58 +0300

    implementation and testing of the function time_to_unc

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -39,61 +39,45 @@ int get_seconds(int seconds)
 	return seconds - (get_hours(seconds) * 3600) - (get_minutes(seconds) * 60);
 }
 
-//double time_to_utc(int utc_offset, double time)
+double time_to_utc(int utc_offset, double time)
+{
+	double utc_time = time - utc_offset;
+	while (utc_time < 0) {
+		utc_time += 24;
+	}
+	while (utc_time >= 24) {
+		utc_time -= 24;
+	}
+	return utc_time;
+}
+
+//double time_from_utc(int utc_offset, double time)
 //{
 	/*
-		Return time at UTC+0, where utc_offset is the number of hours away from
-		UTC+0.
-		You may be interested in:
-		https://en.wikipedia.org/wiki/Coordinated_Universal_Time
+		Return UTC time in time zone utc_offset.
 
-		>>> time_to_utc(+0, 12.0)
+		>>> time_from_utc(+0, 12.0)
 		12.0
 
-		>>> time_to_utc(+1, 12.0)
-		11.0
-
-		>>> time_to_utc(-1, 12.0)
+		>>> time_from_utc(+1, 12.0)
 		13.0
 
-		>>> time_to_utc(-11, 18.0)
-		5.0
+		>>> time_from_utc(-1, 12.0)
+		11.0
 
-		>>> time_to_utc(-1, 0.0)
-		1.0
+		>>> time_from_utc(+6, 6.0)
+		12.0
 
-		>>> time_to_utc(-1, 23.0)
+		>>> time_from_utc(-7, 6.0)
+		23.0
+
+		>>> time_from_utc(-1, 0.0)
+		23.0
+
+		>>> time_from_utc(-1, 23.0)
+		22.0
+
+		>>> time_from_utc(+1, 23.0)
 		0.0
 	*/
 	//}
-
-	//double time_from_utc(int utc_offset, double time)
-	//{
-		/*
-			Return UTC time in time zone utc_offset.
-
-			>>> time_from_utc(+0, 12.0)
-			12.0
-
-			>>> time_from_utc(+1, 12.0)
-			13.0
-
-			>>> time_from_utc(-1, 12.0)
-			11.0
-
-			>>> time_from_utc(+6, 6.0)
-			12.0
-
-			>>> time_from_utc(-7, 6.0)
-			23.0
-
-			>>> time_from_utc(-1, 0.0)
-			23.0
-
-			>>> time_from_utc(-1, 23.0)
-			22.0
-
-			>>> time_from_utc(+1, 23.0)
-			0.0
-		*/
-		//}
