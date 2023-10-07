f7fbcee6d249f02958b496e59aa7dbccef21b0c9
Виссарионова Анна <anna.ogneva.455@mail.ru>
2023-10-07 19:43:10 +0300

    added tests for function time_to_utc

--- main.cpp
+++ main.cpp
@@ -32,5 +32,14 @@ int main() {
 	assert(fabs(to_24_hour_clock(48)) < DBL_EPSILON);
 	assert(fabs(to_24_hour_clock(4) - 4) < DBL_EPSILON);
 	assert(fabs(to_24_hour_clock(28.5) - 4.5) < DBL_EPSILON);
+
+
+	//time_to_utc tests
+	assert(fabs(time_to_utc(+0, 12.0) - 12.0) < DBL_EPSILON);
+	assert(fabs(time_to_utc(+1, 12.0) - 11.0) < DBL_EPSILON);
+	assert(fabs(time_to_utc(-1, 12.0) - 13.0) < DBL_EPSILON);
+	assert(fabs(time_to_utc(-11, 18.0) - 5.0) < DBL_EPSILON);
+	assert(fabs(time_to_utc(-1, 0.0) - 1.0) < DBL_EPSILON);
+	assert(fabs(time_to_utc(-1, 23.0)) < DBL_EPSILON);
 }
 
