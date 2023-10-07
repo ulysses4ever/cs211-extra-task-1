f3eebde09e932cf9f9448f6445a043d1fd91fc89
Руслан Бичурин <bichurin.ruslan.romanovich@yandex.ru>
2023-10-07 22:08:25 +0300

    implementation and testing of the functions get_hours, get_minutes and get_seconds

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -3,103 +3,97 @@
 
 double seconds_difference(double time_1, double time_2)
 {
-    return time_2 - time_1;
+	return time_2 - time_1;
 }
 
 double hours_difference(double time_1, double time_2)
 {
-    return (time_2 - time_1) / 3600;
+	return (time_2 - time_1) / 3600;
 }
 
 double to_float_hours(int hours, int minutes, int seconds)
 {
-    assert(0 <= minutes && minutes < 60);
-    assert(0 <= seconds && seconds < 60);
-    return (double)hours + (double)(minutes / 60.0) + (double)(seconds / 3600.0);
+	assert(0 <= minutes && minutes < 60);
+	assert(0 <= seconds && seconds < 60);
+	return (double)hours + (double)(minutes / 60.0) + (double)(seconds / 3600.0);
 }
 
 double to_24_hour_clock(double hours)
 {
-    assert(hours >= 0);
-    return (hours - 12 * (int)(hours / 12));
+	assert(hours >= 0);
+	return (hours - 12 * (int)(hours / 12));
 }
 
-/*
-    Implement three functions
-        * get_hours
-        * get_minutes
-        * get_seconds
-    They are used to determine the hours part, minutes part and seconds part 
-    of a time in seconds. E.g.:
+int get_hours(int seconds)
+{
+	return seconds / 3600;
+}
 
-    >>> get_hours(3800)
-    1
+int get_minutes(int seconds)
+{
+	return (seconds - (get_hours(seconds) * 3600)) / 60;
+}
 
-    >>> get_minutes(3800)
-    3
-
-    >>> get_seconds(3800)
-    20
-
-    In other words, if 3800 seconds have elapsed since midnight, 
-    it is currently 01:03:20 (hh:mm:ss).
-*/
+int get_seconds(int seconds)
+{
+	return seconds - (get_hours(seconds) * 3600) - (get_minutes(seconds) * 60);
+}
 
 //double time_to_utc(int utc_offset, double time)
 //{
-    /*
-        Return time at UTC+0, where utc_offset is the number of hours away from
-        UTC+0.
-        You may be interested in:
-        https://en.wikipedia.org/wiki/Coordinated_Universal_Time
+	/*
+		Return time at UTC+0, where utc_offset is the number of hours away from
+		UTC+0.
+		You may be interested in:
+		https://en.wikipedia.org/wiki/Coordinated_Universal_Time
 
-        >>> time_to_utc(+0, 12.0)
-        12.0
- 
-        >>> time_to_utc(+1, 12.0)
-        11.0
- 
-        >>> time_to_utc(-1, 12.0)
-        13.0
- 
-        >>> time_to_utc(-11, 18.0)
-        5.0
- 
-        >>> time_to_utc(-1, 0.0)
-        1.0
- 
-        >>> time_to_utc(-1, 23.0)
-        0.0
-    */
-//}
+		>>> time_to_utc(+0, 12.0)
+		12.0
 
-//double time_from_utc(int utc_offset, double time)
-//{
-    /*
-        Return UTC time in time zone utc_offset.
+		>>> time_to_utc(+1, 12.0)
+		11.0
 
-        >>> time_from_utc(+0, 12.0)
-        12.0
- 
-        >>> time_from_utc(+1, 12.0)
-        13.0
- 
-        >>> time_from_utc(-1, 12.0)
-        11.0
- 
-        >>> time_from_utc(+6, 6.0)
-        12.0
- 
-        >>> time_from_utc(-7, 6.0)
-        23.0
- 
-        >>> time_from_utc(-1, 0.0)
-        23.0
- 
-        >>> time_from_utc(-1, 23.0)
-        22.0
- 
-        >>> time_from_utc(+1, 23.0)
-        0.0
-    */
-//}
+		>>> time_to_utc(-1, 12.0)
+		13.0
+
+		>>> time_to_utc(-11, 18.0)
+		5.0
+
+		>>> time_to_utc(-1, 0.0)
+		1.0
+
+		>>> time_to_utc(-1, 23.0)
+		0.0
+	*/
+	//}
+
+	//double time_from_utc(int utc_offset, double time)
+	//{
+		/*
+			Return UTC time in time zone utc_offset.
+
+			>>> time_from_utc(+0, 12.0)
+			12.0
+
+			>>> time_from_utc(+1, 12.0)
+			13.0
+
+			>>> time_from_utc(-1, 12.0)
+			11.0
+
+			>>> time_from_utc(+6, 6.0)
+			12.0
+
+			>>> time_from_utc(-7, 6.0)
+			23.0
+
+			>>> time_from_utc(-1, 0.0)
+			23.0
+
+			>>> time_from_utc(-1, 23.0)
+			22.0
+
+			>>> time_from_utc(+1, 23.0)
+			0.0
+		*/
+		//}
--- extra-task-1.h
+++ extra-task-1.h
@@ -9,4 +9,10 @@ double to_float_hours(int hours, int minutes, int seconds);
 
 double to_24_hour_clock(double hours);
 
+int get_hours(int seconds);
+
+int get_minutes(int seconds);
+
+int get_seconds(int seconds);
+
 #endif
