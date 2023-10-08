e02d6ee3ac8b684bbe1185bcaf031c9bdfe1951f
Quant1co <115133527+Quant1co@users.noreply.github.com>
2023-10-08 22:07:47 +0300

    commit1
    add first task

---
+++ Bonus1/Bonus1/main.cpp
@@ -0,0 +1,16 @@
+﻿
+
+#include <iostream>
+#include "bonus.h"
+#include <cassert>
+
+void main()
+{
+	//seconds_difference tests
+	assert(fabs(seconds_difference(1800.0, 3600.0) - 1800.0) < DBL_EPSILON);
+	assert(fabs(seconds_difference(1800.0, 2160.0) - 360.0) < DBL_EPSILON);
+	assert(fabs(seconds_difference(3600.0, 1800.0) - (-1800.0)) < DBL_EPSILON);
+	assert(fabs(seconds_difference(1800.0, 1800.0) - 0.0) < DBL_EPSILON);
+}
+
+
