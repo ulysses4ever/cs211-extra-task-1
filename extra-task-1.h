3db2adb40d26205d3132c5a9864ca2bde675d9c3
kalyuzhin <67906018+kalyuzhin@users.noreply.github.com>
2023-10-08 19:02:50 +0300

    Create headers of get_hours, get_minutes, e.t.c.

--- extra-task-1.h
+++ extra-task-1.h
@@ -22,6 +22,12 @@ double to_float_hours(int hours, int minutes, int seconds);
 
 double to_24_hour_clock(double hours);
 
+double get_hours(double time);
+
+double get_minutes(double time);
+
+double get_seconds(double time);
+
 double time_to_utc(int utc_offset, double time);
 
 double time_from_utc(int utc_offset, double time);
