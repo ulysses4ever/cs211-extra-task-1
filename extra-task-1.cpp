912086032e2c0ef5237e40625fb1e545c94b5ce6
Yamat0Damashii <maksim.orlach@yandex.ru>
2023-10-08 20:39:39 +0300

    Wrote the implementation and assert for 4 task

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -1,4 +1,5 @@
 #include <cassert>
+#include <cmath>
 
 double seconds_difference(double time_1, double time_2)
 {
@@ -19,32 +20,10 @@ double to_float_hours(int hours, int minutes, int seconds)
 
 double to_24_hour_clock(double hours)
 {
-    return 0;
-    /*
-        hours is a number of hours since midnight. Return the
-        hour as seen on a 24-hour clock.
-
-        Precondition: hours >= 0
-
-        >>> to_24_hour_clock(24)
-        0
-        
-        >>> to_24_hour_clock(48)
-        0
-        
-        >>> to_24_hour_clock(25)
-        1
-        
-        >>> to_24_hour_clock(4)
-        4
-        
-        >>> to_24_hour_clock(28.5)
-        4.5
-        
-        You may wish to inspect various function in <cmath> to work
-        with integer and fractional part of a hours separately.
-        
-    */
+    assert(hours >= 0);
+    double int_part;
+    double frac_part = modf(hours, &int_part);
+    return (int)int_part % 24 + frac_part;
 }
 
 /*
