e8146cd0d40b41e0c6c6b511bad4d57c0f9459b8
HUAWEI <sanchous2810@gmail.com>
2023-10-08 16:03:51 +0300

    Add main and asserts

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -1,3 +1,7 @@
+#include <assert.h>
+#include <cmath>
+#include <cfloat>
+
 double seconds_difference(double time_1, double time_2)
 {
     // your implementation goes here...
@@ -189,4 +193,46 @@ double time_from_utc(int utc_offset, double time)
         >>> time_from_utc(+1, 23.0)
         0.0
     */
+}
+
+int main() {
+    assert(seconds_difference(1800.0, 3600.0) - 1800.0 < DBL_EPSILON);
+    assert(seconds_difference(3600.0, 1800.0) + 1800.0 < DBL_EPSILON);
+    assert(seconds_difference(1800.0, 2160.0) - 360.0 < DBL_EPSILON);
+    assert(seconds_difference(1800.0, 1800.0) < DBL_EPSILON);
+
+    assert(hours_difference(1800.0, 3600.0) - 0.5 < DBL_EPSILON);
+    assert(hours_difference(3600.0, 1800.0) + 0.5 < DBL_EPSILON);
+    assert(hours_difference(1800.0, 2160.0) - 0.1 < DBL_EPSILON);
+    assert(hours_difference(1800.0, 1800.0) < DBL_EPSILON);
+
+    assert(to_float_hours(0, 15, 0) - 0.25 < DBL_EPSILON);
+    assert(to_float_hours(2, 45, 9) - 2.7525 < DBL_EPSILON);
+    assert(to_float_hours(1, 0, 36) - 1.01 < DBL_EPSILON);
+
+    assert(to_24_hour_clock(24) < DBL_EPSILON);
+    assert(to_24_hour_clock(48) < DBL_EPSILON);
+    assert(to_24_hour_clock(25) - 1.0 < DBL_EPSILON);
+    assert(to_24_hour_clock(4) - 4.0 < DBL_EPSILON);
+    assert(to_24_hour_clock(28.5) - 4.5 < DBL_EPSILON);
+
+    assert(get_hours(3800) - 1.0 < DBL_EPSILON);
+    assert(get_minutes(3800) - 3.0 < DBL_EPSILON);
+    assert(get_seconds(3800) - 20.0 < DBL_EPSILON);
+
+    assert(time_to_utc(0, 12.0) - 12.0 < DBL_EPSILON);
+    assert(time_to_utc(1, 12.0) - 11.0 < DBL_EPSILON);
+    assert(time_to_utc(-1, 12.0) - 13.0 < DBL_EPSILON);
+    assert(time_to_utc(-11, 18.0) - 5.0 < DBL_EPSILON);
+    assert(time_to_utc(-1, 0.0) - 1.0 < DBL_EPSILON);
+    assert(time_to_utc(-1, 23.0) - 0.0 < DBL_EPSILON);
+
+    assert(time_from_utc(0, 12.0) - 12.0 < DBL_EPSILON);
+    assert(time_from_utc(1, 12.0) - 13.0 < DBL_EPSILON);
+    assert(time_from_utc(-1, 12.0) - 11.0 < DBL_EPSILON);
+    assert(time_from_utc(6, 6.0) - 12.0 < DBL_EPSILON);
+    assert(time_from_utc(-7, 6.0) - 23.0 < DBL_EPSILON);
+    assert(time_from_utc(-1, 0.0) - 23.0 < DBL_EPSILON);
+    assert(time_from_utc(-1, 23.0) - 22.0 < DBL_EPSILON);
+    assert(time_from_utc(1, 23.0) - 0.0 < DBL_EPSILON);
 }
