859f0cbd23375aa67832097c60200f4183e64001
LordPelmeha <sashasofolov@gmail.com>
2023-10-08 20:10:46 +0300

    added a function seconds_difference

---
+++ ConsoleApplication1/ConsoleApplication1/extra-task-1.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <iostream>
+#include <cassert>
+//Возвращает разницу между time_2 и time_1
+double seconds_difference(double, double);
---
+++ ConsoleApplication1/ConsoleApplication1/main.cpp
@@ -0,0 +1,12 @@
+﻿#include "extra-task-1.h"
+using namespace std;
+int main()
+{
+	{
+		assert(fabs(seconds_difference(1800.0, 3600.0) - 1800.0) < DBL_EPSILON);
+		assert(fabs(seconds_difference(3600.0, 1800.0) - (-1800.0)) < DBL_EPSILON);
+		assert(fabs(seconds_difference(1800.0, 2160.0) - 360.0) < DBL_EPSILON);
+		assert(fabs(seconds_difference(1800.0, 1800.0) - 0.0) < DBL_EPSILON);
+	}
+
+}
