0e14379c46532948ef52e4ad00de7d324c8fd954
kalyuzhin <67906018+kalyuzhin@users.noreply.github.com>
2023-10-08 18:41:25 +0300

    Updated asserts

--- main.cpp
+++ main.cpp
@@ -10,5 +10,18 @@ int main() {
         assert(seconds_difference(1800.0, 1800.0) == 0.0);
     }
 
+    {
+        // функция hours_difference
+        assert(std::fabs(hours_difference(1800.0, 3600.0) - 0.5) < DBL_EPSILON);
+        assert((hours_difference(3600.0, 1800.0) + 0.5) < DBL_EPSILON);
+        assert(std::fabs(hours_difference(1800.0, 2160.0) - 0.1) < DBL_EPSILON);
+        assert(hours_difference(1800.0, 1800.0) == 0.0);
+    }
+
+    {
+        // функция to_float_hours
+
+    }
+
     return 0;
 }
