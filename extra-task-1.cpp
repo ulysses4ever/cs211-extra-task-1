a25514f16bfe35291b5eff34bcf6af8c31eae91f
kalyuzhin <67906018+kalyuzhin@users.noreply.github.com>
2023-10-08 18:31:34 +0300

    Implemented seconds_difference, written asserts

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -6,8 +6,8 @@
 
 double seconds_difference(double time_1, double time_2)
 {
-    // your implementation goes here...
-
+    assert(time_1 >=0 && time_2>=0);
+    return time_2 - time_1;
     /*
         Return the number of seconds later that a time in seconds
         time_2 is than a time in seconds time_1.
@@ -28,6 +28,8 @@ double seconds_difference(double time_1, double time_2)
 
 double hours_difference(double time_1, double time_2)
 {
+    return 0;
+
     /*
         Return the number of hours later that a time in seconds
         time_2 is than a time in seconds time_1.
@@ -48,6 +50,8 @@ double hours_difference(double time_1, double time_2)
 
 double to_float_hours(int hours, int minutes, int seconds)
 {
+
+    return 0;
     /*
         Return the total number of hours in the specified number
         of hours, minutes, and seconds.
@@ -67,6 +71,8 @@ double to_float_hours(int hours, int minutes, int seconds)
 
 double to_24_hour_clock(double hours)
 {
+
+    return 0;
     /*
         hours is a number of hours since midnight. Return the
         hour as seen on a 24-hour clock.
@@ -117,6 +123,8 @@ double to_24_hour_clock(double hours)
 
 double time_to_utc(int utc_offset, double time)
 {
+
+    return 0;
     /*
         Return time at UTC+0, where utc_offset is the number of hours away from
         UTC+0.
@@ -145,6 +153,8 @@ double time_to_utc(int utc_offset, double time)
 
 double time_from_utc(int utc_offset, double time)
 {
+
+    return 0;
     /*
         Return UTC time in time zone utc_offset.
 
--- main.cpp
+++ main.cpp
@@ -2,5 +2,13 @@
 
 int main() {
 
+    {
+        // функция seconds_difference
+        assert(seconds_difference(1800.0, 3600.0) == 1800.0);
+        assert(seconds_difference(3600.0, 1800.0) == -1800.0);
+        assert(seconds_difference(1800.0, 2160.0) == 360.0);
+        assert(seconds_difference(1800.0, 1800.0) == 0.0);
+    }
+
     return 0;
 }
