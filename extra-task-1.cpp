126233224f9643fcd80d28f690d9919b0edb03b1
Antonenko Aleksandr <sa0529083@gmail.com>
2023-10-08 22:00:57 +0300

    Added function time_to_utc and its check in main.

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -2,21 +2,25 @@
 #include <assert.h>
 #include <math.h>
 
+//Return the number of seconds later that a time in seconds time_2 is than a time in seconds time_1.
 double seconds_difference(double time_1, double time_2)
 {
     return time_2 - time_1;
 }
 
+//Return the number of hours later that a time in seconds time_2 is than a time in seconds time_1.
 double hours_difference(double time_1, double time_2)
 {
     return seconds_difference(time_1, time_2) / 3600;
 }
 
+//Return the total number of hours in the specified number of hours, minutes, and seconds.
 double to_float_hours(int hours, int minutes, int seconds)
 {
     return hours*1.0 + minutes/60.0 + seconds/3600.0; 
 }
 
+//Return the hour as seen on a 24-hour clock. hours is a number of hours since midnight.
 double to_24_hour_clock(double hours)
 {
     return fmod(hours,24);
@@ -39,30 +43,11 @@ int get_seconds(int seconds)
 {
     return ((seconds % 3600) % 60) % 60;
 }
-/*
-    Implement three functions
-        * get_hours
-        * get_minutes
-        * get_seconds
-    They are used to determine the hours part, minutes part and seconds part 
-    of a time in seconds. E.g.:
 
-    >>> get_hours(3800)
-    1
-
-    >>> get_minutes(3800)
-    3
-
-    >>> get_seconds(3800)
-    20
-
-    In other words, if 3800 seconds have elapsed since midnight, 
-    it is currently 01:03:20 (hh:mm:ss).
-*/
-
+// Return time at UTC+0, where utc_offset is the number of hours away from UTC + 0.
 double time_to_utc(int utc_offset, double time)
 {
-    return 0;
+    return to_24_hour_clock((time - utc_offset) > 0 ? (time - utc_offset) : 24 + (time - utc_offset));
 
     /*
         Return time at UTC+0, where utc_offset is the number of hours away from
--- main.cpp
+++ main.cpp
@@ -53,4 +53,13 @@ void main()
 	assert(get_seconds(5500) == 40);
 	std::cout << "get_seconds works!" << std::endl;
 	
+	//time_to_utc
+	assert(time_to_utc(+0, 12.0) == 12.0);
+	assert(time_to_utc(+1, 12.0) == 11.0);
+	assert(time_to_utc(-1, 12.0) == 13.0);
+	assert(time_to_utc(-11, 18.0) == 5.0);
+	assert(time_to_utc(-1, 0.0) == 1.0);
+	assert(time_to_utc(-1, 23.0) == 0.0);
+	std::cout << "time_to_utc works!" << std::endl;
+
 }
