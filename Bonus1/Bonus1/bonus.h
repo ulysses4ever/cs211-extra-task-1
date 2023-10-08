6bc7606c711861df40d34ec0a7e49202b9c36622
Quant1co <115133527+Quant1co@users.noreply.github.com>
2023-10-08 22:30:17 +0300

    commit4
    add fourth task

--- Bonus1/Bonus1/bonus.h
+++ Bonus1/Bonus1/bonus.h
@@ -1,13 +1,13 @@
 #pragma once
 
-//Return the number of seconds later that a time in seconds 
-// time_2 is than a time in seconds time_1.
+//Return the number of seconds later that a time in seconds time_2 is than a time in seconds time_1
 double seconds_difference(double time_1, double time_2);
 
-//Return the number of hours later that a time in seconds
-//time_2 is than a time in seconds time_1.
+//Return the number of hours later that a time in seconds time_2 is than a time in seconds time_1
 double hours_difference(double time_1, double time_2);
 
-//Return the total number of hours in the specified number
-//of hours, minutes, and seconds.
+//Return the total number of hours in the specified number of hours, minutes, and seconds
 double to_float_hours(int hours, int minutes, int seconds);
+
+//hours is a number of hours since midnight. Return the hour as seen on a 24 - hour clock
+double to_24_hour_clock(double hours);
--- Bonus1/Bonus1/extra-task-1.cpp
+++ Bonus1/Bonus1/extra-task-1.cpp
@@ -16,73 +16,40 @@ double to_float_hours(int hours, int minutes, int seconds)
 	assert(0 <= minutes && minutes < 60 && 0 <= seconds && seconds < 60);
 
 	return hours + (minutes / 60.0) + (seconds / 3600.0);
-	/*
-		Return the total number of hours in the specified number
-		of hours, minutes, and seconds.
-
-		Precondition: 0 <= minutes < 60  and  0 <= seconds < 60
-
-		>>> to_float_hours(0, 15, 0)
-		0.25
-
-		>>> to_float_hours(2, 45, 9)
-		2.7525
-
-		>>> to_float_hours(1, 0, 36)
-		1.01
-	*/
 }
 
-//double to_24_hour_clock(double hours)
-//{
-//	/*
-//		hours is a number of hours since midnight. Return the
-//		hour as seen on a 24-hour clock.
-//
-//		Precondition: hours >= 0
-//
-//		>>> to_24_hour_clock(24)
-//		0
-//
-//		>>> to_24_hour_clock(48)
-//		0
-//
-//		>>> to_24_hour_clock(25)
-//		1
-//
-//		>>> to_24_hour_clock(4)
-//		4
-//
-//		>>> to_24_hour_clock(28.5)
-//		4.5
-//
-//		You may wish to inspect various function in <cmath> to work
-//		with integer and fractional part of a hours separately.
-//
-//	*/
-//}
-//
-///*
-//	Implement three functions
-//		* get_hours
-//		* get_minutes
-//		* get_seconds
-//	They are used to determine the hours part, minutes part and seconds part
-//	of a time in seconds. E.g.:
-//
-//	>>> get_hours(3800)
-//	1
-//
-//	>>> get_minutes(3800)
-//	3
-//
-//	>>> get_seconds(3800)
-//	20
-//
-//	In other words, if 3800 seconds have elapsed since midnight,
-//	it is currently 01:03:20 (hh:mm:ss).
-//*/
-//
+double to_24_hour_clock(double hours)
+{
+	assert(hours >= 0);
+	double result;
+	int Hours = (int)hours;
+	double hoursOnClock = hours - Hours;
+	result = Hours % 24 + hoursOnClock;
+
+	return result;
+}
+
+/*
+	Implement three functions
+		* get_hours
+		* get_minutes
+		* get_seconds
+	They are used to determine the hours part, minutes part and seconds part
+	of a time in seconds. E.g.:
+
+	>>> get_hours(3800)
+	1
+
+	>>> get_minutes(3800)
+	3
+
+	>>> get_seconds(3800)
+	20
+
+	In other words, if 3800 seconds have elapsed since midnight,
+	it is currently 01:03:20 (hh:mm:ss).
+*/
+
 //double time_to_utc(int utc_offset, double time)
 //{
 //	/*
