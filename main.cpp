d945de6a7181ba9e6c45b16783f4979461df048d
Palom-Porom <114829512+Palom-Porom@users.noreply.github.com>
2023-10-08 22:20:30 +0300

    added asserts for the first function

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include "extra-task-1.h"
+#include <cassert>
+#include <cmath>
+#include <float.h>
+
+int main()
+{
+    assert(fabs(seconds_difference(1800.0, 3600.0) - 1800.0) < DBL_EPSILON);
+    assert(fabs(seconds_difference(3600.0, 1800.0) + 1800.0) < DBL_EPSILON);
+    assert(fabs(seconds_difference(1800.0, 2160.0) - 360.0) < DBL_EPSILON);
+    assert(fabs(seconds_difference(1800.0, 1800.0) - 0.0) < DBL_EPSILON);
+    /*
+        Return the number of seconds later that a time in seconds
+        time_2 is than a time in seconds time_1.
+
+        >>> seconds_difference(1800.0, 3600.0)
+        1800.0
+
+        >>> seconds_difference(3600.0, 1800.0)
+        -1800.0
+
+        >>> seconds_difference(1800.0, 2160.0)
+        360.0
+
+        >>> seconds_difference(1800.0, 1800.0)
+        0.0
+    */
+}
