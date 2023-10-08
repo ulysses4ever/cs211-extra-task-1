8d8c5b81a38d14a0c54033d6ba82513c98e544aa
HUAWEI <sanchous2810@gmail.com>
2023-10-08 15:49:46 +0300

    Add implementation hours_difference func

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -23,6 +23,7 @@ double seconds_difference(double time_1, double time_2)
 
 double hours_difference(double time_1, double time_2)
 {
+    return seconds_difference(time_1, time_2) / 3600;
     /*
         Return the number of hours later that a time in seconds
         time_2 is than a time in seconds time_1.
