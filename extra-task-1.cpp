76a1261ddd596708c00c5a020581f8a7c861427f
Stepanych114 <danikstepanyan.2015@mail.ru>
2023-10-08 22:45:42 +0300

    add time_from_utc

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -191,6 +191,9 @@ double time_from_utc(int utc_offset, double time)
         >>> time_from_utc(+1, 23.0)
         0.0
     */
+    if (time + utc_offset < 0)
+        time += 24;
+    return   to_24_hour_clock(time + utc_offset);
 }
 
 int main()
@@ -236,4 +239,14 @@ int main()
     assert(fabs(time_to_utc(-11, 18.0) - 5.0) < DBL_EPSILON);
     assert(fabs(time_to_utc(-1, 0.0) - 1.0) < DBL_EPSILON);
     assert(fabs(time_to_utc(-1, 23.0)) < DBL_EPSILON);
+
+    /* Return UTC time in time zone utc_offset.*/
+    assert(fabs(time_from_utc(+0, 12.0) - 12.0) < DBL_EPSILON);
+    assert(fabs(time_from_utc(+1, 12.0) - 13.0) < DBL_EPSILON);
+    assert(fabs(time_from_utc(-1, 12.0) - 11.0) < DBL_EPSILON);
+    assert(fabs(time_from_utc(+6, 6.0) - 12.0) < DBL_EPSILON);
+    assert(fabs(time_from_utc(-7, 6.0) - 23.0) < DBL_EPSILON);
+    assert(fabs(time_from_utc(-1, 0.0) - 23.0) < DBL_EPSILON);
+    assert(fabs(time_from_utc(-1, 23.0) - 22.0) < DBL_EPSILON);
+    assert(fabs(time_from_utc(+1, 23.0) - 0) < DBL_EPSILON);
 }
