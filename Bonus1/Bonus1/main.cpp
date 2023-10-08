0c65118ef37aa59519ce3433f216dd441dae0d43
Quant1co <115133527+Quant1co@users.noreply.github.com>
2023-10-08 22:17:13 +0300

    commit3
    add third task

--- Bonus1/Bonus1/main.cpp
+++ Bonus1/Bonus1/main.cpp
@@ -17,6 +17,11 @@ void main()
 	assert(fabs(hours_difference(3600.0, 1800.0) - (-0.5)) < DBL_EPSILON);
 	assert(fabs(hours_difference(1800.0, 2160.0) - 0.1) < DBL_EPSILON);
 	assert(fabs(hours_difference(1800.0, 1800.0) - 0.0) < DBL_EPSILON);
+
+	//to_float_hours test
+	assert(fabs(to_float_hours(0, 15, 0) - 0.25) < DBL_EPSILON);
+	assert(fabs(to_float_hours(2, 45, 9) - 2.7525) < DBL_EPSILON);
+	assert(fabs(to_float_hours(1, 0, 36) - 1.01) < DBL_EPSILON);
 }
 
 
