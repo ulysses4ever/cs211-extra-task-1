317dba61d46997a9f5b300c1653875e2f9ebd3b8
LordPelmeha <sashasofolov@gmail.com>
2023-10-08 22:33:55 +0300

    added a functions get_hours, get_minutes and  get_seconds

--- ConsoleApplication1/ConsoleApplication1/extra-task-1.h
+++ ConsoleApplication1/ConsoleApplication1/extra-task-1.h
@@ -9,3 +9,9 @@ double hours_difference(double, double);
 double to_float_hours(int, int, int);
 //Возвращает часы на 24 часовых часах
 double to_24_hour_clock(double);
+//Возвращает часовую часть в секундах
+int get_hours(int);
+//Возвращает минутную часть в секундах
+int get_minutes(int);
+//Возвращает секундую часть
+int get_seconds(int);
