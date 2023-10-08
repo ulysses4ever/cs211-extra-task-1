dd324570bd33d039df8c3a4c93d36e6ab166fcf3
LordPelmeha <sashasofolov@gmail.com>
2023-10-08 22:12:30 +0300

    added a function to_float_hours

--- ConsoleApplication1/ConsoleApplication1/extra-task-1.h
+++ ConsoleApplication1/ConsoleApplication1/extra-task-1.h
@@ -5,3 +5,5 @@
 double seconds_difference(double, double);
 //Возвращает разницу между секундами time_2 и time_1 в часах
 double hours_difference(double, double);
+//Возвращает время в часах
+double to_float_hours(int, int, int);
