9b48e7c347306f66cc482e07e4dcdb3dc8e0c6c7
Amalia <115399488+privet-kak-dela@users.noreply.github.com>
2023-10-08 19:30:51 +0300

    add second task

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -1,12 +1,12 @@
+#include <iostream>
+#include "extra-task-1.h"
 double seconds_difference(double time_1, double time_2)
 {
     return time_2 - time_1;
-    // your implementation goes here...
-    
-    /*    
+    /*
         Return the number of seconds later that a time in seconds
         time_2 is than a time in seconds time_1.
-            
+
         >>> seconds_difference(1800.0, 3600.0)
         1800.0
 
@@ -19,14 +19,16 @@ double seconds_difference(double time_1, double time_2)
         >>> seconds_difference(1800.0, 1800.0)
         0.0
     */
+
 }
 
 double hours_difference(double time_1, double time_2)
 {
+    return (time_2 - time_1) / 3600;
     /*
         Return the number of hours later that a time in seconds
         time_2 is than a time in seconds time_1.
-            
+
         >>> hours_difference(1800.0, 3600.0)
         0.5
 
@@ -43,6 +45,7 @@ double hours_difference(double time_1, double time_2)
 
 double to_float_hours(int hours, int minutes, int seconds)
 {
+    return 0;
     /*
         Return the total number of hours in the specified number
         of hours, minutes, and seconds.
@@ -62,6 +65,7 @@ double to_float_hours(int hours, int minutes, int seconds)
 
 double to_24_hour_clock(double hours)
 {
+    return 0;
     /*
         hours is a number of hours since midnight. Return the
         hour as seen on a 24-hour clock.
@@ -70,22 +74,22 @@ double to_24_hour_clock(double hours)
 
         >>> to_24_hour_clock(24)
         0
-        
+
         >>> to_24_hour_clock(48)
         0
-        
+
         >>> to_24_hour_clock(25)
         1
-        
+
         >>> to_24_hour_clock(4)
         4
-        
+
         >>> to_24_hour_clock(28.5)
         4.5
-        
+
         You may wish to inspect various function in <cmath> to work
         with integer and fractional part of a hours separately.
-        
+
     */
 }
 
@@ -94,7 +98,7 @@ double to_24_hour_clock(double hours)
         * get_hours
         * get_minutes
         * get_seconds
-    They are used to determine the hours part, minutes part and seconds part 
+    They are used to determine the hours part, minutes part and seconds part
     of a time in seconds. E.g.:
 
     >>> get_hours(3800)
@@ -106,12 +110,13 @@ double to_24_hour_clock(double hours)
     >>> get_seconds(3800)
     20
 
-    In other words, if 3800 seconds have elapsed since midnight, 
+    In other words, if 3800 seconds have elapsed since midnight,
     it is currently 01:03:20 (hh:mm:ss).
 */
 
 double time_to_utc(int utc_offset, double time)
 {
+    return 0;
     /*
         Return time at UTC+0, where utc_offset is the number of hours away from
         UTC+0.
@@ -120,19 +125,19 @@ double time_to_utc(int utc_offset, double time)
 
         >>> time_to_utc(+0, 12.0)
         12.0
- 
+
         >>> time_to_utc(+1, 12.0)
         11.0
- 
+
         >>> time_to_utc(-1, 12.0)
         13.0
- 
+
         >>> time_to_utc(-11, 18.0)
         5.0
- 
+
         >>> time_to_utc(-1, 0.0)
         1.0
- 
+
         >>> time_to_utc(-1, 23.0)
         0.0
     */
@@ -140,30 +145,31 @@ double time_to_utc(int utc_offset, double time)
 
 double time_from_utc(int utc_offset, double time)
 {
+    return 0;
     /*
         Return UTC time in time zone utc_offset.
 
         >>> time_from_utc(+0, 12.0)
         12.0
- 
+
         >>> time_from_utc(+1, 12.0)
         13.0
- 
+
         >>> time_from_utc(-1, 12.0)
         11.0
- 
+
         >>> time_from_utc(+6, 6.0)
         12.0
- 
+
         >>> time_from_utc(-7, 6.0)
         23.0
- 
+
         >>> time_from_utc(-1, 0.0)
         23.0
- 
+
         >>> time_from_utc(-1, 23.0)
         22.0
- 
+
         >>> time_from_utc(+1, 23.0)
         0.0
     */
--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include<iostream>
+#include "cassert"
+#include <cfloat>
+#include <cmath>
 #include "extra-task-1.h"
 int main()
 {
+	//task1
+	assert(abs(1800.0 - seconds_difference(1800.0, 3600.0)) < DBL_EPSILON);
+	assert(abs(-1800.0 - seconds_difference(3600.0, 1800.0)) < DBL_EPSILON);
+	assert(abs(360.0 - seconds_difference(1800.0, 2160.0)) < DBL_EPSILON);
+	assert(abs(0 - seconds_difference(1800.0, 1800.0)) < DBL_EPSILON);
+
+	//task2
+	assert(abs(0.5 - hours_difference(1800.0, 3600.0)) < DBL_EPSILON);
+	assert(abs(-0.5 - hours_difference(3600.0, 1800.0)) < DBL_EPSILON);
+	assert(abs(0.1 - hours_difference(1800.0, 2160.0)) < DBL_EPSILON);
+	assert(abs(0 - hours_difference(1800.0, 1800.0)) < DBL_EPSILON);
 
 }
