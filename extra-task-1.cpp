4af85aa3bad56659b208d64503a47992a7616bce
ler0yyy <leroiler2004@gmail.com>
2023-10-08 14:09:59 +0300

    3 functions are added

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -40,26 +40,29 @@ double to_24_hour_clock(double hours)
     return hour_24;
 }
 
-/*
-    Implement three functions
-        * get_hours
-        * get_minutes
-        * get_seconds
-    They are used to determine the hours part, minutes part and seconds part 
-    of a time in seconds. E.g.:
+int get_hours(int seconds)
+{
+    /*
+        Return the hours part of a time in seconds.
+    */
+    return seconds / 3600;
+}
 
-    >>> get_hours(3800)
-    1
+int get_minutes(int seconds)
+{
+    /*
+        Return the minutes part of a time in seconds.
+    */
+    return (seconds % 3600) / 60;
+}
 
-    >>> get_minutes(3800)
-    3
-
-    >>> get_seconds(3800)
-    20
-
-    In other words, if 3800 seconds have elapsed since midnight, 
-    it is currently 01:03:20 (hh:mm:ss).
-*/
+int get_seconds(int seconds)
+{
+    /*
+        Return the seconds part of a time in seconds.
+    */
+    return seconds % 60;
+}
 
 double time_to_utc(int utc_offset, double time)
 {
