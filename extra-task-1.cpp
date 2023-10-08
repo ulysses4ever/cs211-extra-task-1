58ee24809372b4849777ad59c3ae1aa4d50c884d
aromansky <kirbi.zsthan@gmail.com>
2023-10-08 13:17:28 +0300

    add seconds_difference and tests

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -1,23 +1,9 @@
+#include <cassert>
+
 double seconds_difference(double time_1, double time_2)
 {
-    // your implementation goes here...
-    
-    /*    
-        Return the number of seconds later that a time in seconds
-        time_2 is than a time in seconds time_1.
-            
-        >>> seconds_difference(1800.0, 3600.0)
-        1800.0
-
-        >>> seconds_difference(3600.0, 1800.0)
-        -1800.0
-
-        >>> seconds_difference(1800.0, 2160.0)
-        360.0
-
-        >>> seconds_difference(1800.0, 1800.0)
-        0.0
-    */
+    assert(time_1 >= 0 && time_2 >= 0);
+    return time_2 - time_1;
 }
 
 double hours_difference(double time_1, double time_2)
@@ -166,4 +152,14 @@ double time_from_utc(int utc_offset, double time)
         >>> time_from_utc(+1, 23.0)
         0.0
     */
+}
+
+
+int main()
+{
+    // test seconds_difference
+    assert(std::fabs(seconds_difference(1800.0, 3600.0) - 1800.0) < DBL_EPSILON);
+    assert(std::fabs(seconds_difference(3600.0, 1800.0) + 1800.0) < DBL_EPSILON);
+    assert(std::fabs(seconds_difference(1800.0, 2160.0) - 360.0) < DBL_EPSILON);
+    assert(std::fabs(seconds_difference(1800.0, 1800.0)) < DBL_EPSILON);
 }
