5a2d779093d211dbcf480006175a2f1ae335bd0f
Palom-Porom <114829512+Palom-Porom@users.noreply.github.com>
2023-10-08 22:23:22 +0300

    added and asserted hours_difference

--- main.cpp
+++ main.cpp
@@ -25,4 +25,25 @@ int main()
         >>> seconds_difference(1800.0, 1800.0)
         0.0
     */
+
+    assert(fabs(hours_difference(1800.0, 3600.0) - 0.5) < DBL_EPSILON);
+    assert(fabs(hours_difference(3600.0, 1800.0) + 0.5) < DBL_EPSILON);
+    assert(fabs(hours_difference(1800.0, 2160.0) - 0.1) < DBL_EPSILON);
+    assert(fabs(hours_difference(1800.0, 1800.0) - 0.0) < DBL_EPSILON);
+    /*
+        Return the number of hours later that a time in seconds
+        time_2 is than a time in seconds time_1.
+
+        >>> hours_difference(1800.0, 3600.0)
+        0.5
+
+        >>> hours_difference(3600.0, 1800.0)
+        -0.5
+
+        >>> hours_difference(1800.0, 2160.0)
+        0.1
+
+        >>> hours_difference(1800.0, 1800.0)
+        0.0
+    */
 }
