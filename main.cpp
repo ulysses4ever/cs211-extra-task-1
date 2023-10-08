cf780fb8c4eedd722e377ce2068cc0bd6aefb8a3
Palom-Porom <114829512+Palom-Porom@users.noreply.github.com>
2023-10-08 22:29:00 +0300

    added and asserted to_float_hours

--- main.cpp
+++ main.cpp
@@ -5,45 +5,20 @@
 
 int main()
 {
+    //Test seconds_difference
     assert(fabs(seconds_difference(1800.0, 3600.0) - 1800.0) < DBL_EPSILON);
     assert(fabs(seconds_difference(3600.0, 1800.0) + 1800.0) < DBL_EPSILON);
     assert(fabs(seconds_difference(1800.0, 2160.0) - 360.0) < DBL_EPSILON);
     assert(fabs(seconds_difference(1800.0, 1800.0) - 0.0) < DBL_EPSILON);
-    /*
-        Return the number of seconds later that a time in seconds
-        time_2 is than a time in seconds time_1.
 
-        >>> seconds_difference(1800.0, 3600.0)
-        1800.0
-
-        >>> seconds_difference(3600.0, 1800.0)
-        -1800.0
-
-        >>> seconds_difference(1800.0, 2160.0)
-        360.0
-
-        >>> seconds_difference(1800.0, 1800.0)
-        0.0
-    */
-
+    //Test hours_difference
     assert(fabs(hours_difference(1800.0, 3600.0) - 0.5) < DBL_EPSILON);
     assert(fabs(hours_difference(3600.0, 1800.0) + 0.5) < DBL_EPSILON);
     assert(fabs(hours_difference(1800.0, 2160.0) - 0.1) < DBL_EPSILON);
     assert(fabs(hours_difference(1800.0, 1800.0) - 0.0) < DBL_EPSILON);
-    /*
-        Return the number of hours later that a time in seconds
-        time_2 is than a time in seconds time_1.
 
-        >>> hours_difference(1800.0, 3600.0)
-        0.5
-
-        >>> hours_difference(3600.0, 1800.0)
-        -0.5
-
-        >>> hours_difference(1800.0, 2160.0)
-        0.1
-
-        >>> hours_difference(1800.0, 1800.0)
-        0.0
-    */
+    //Test to_float_hours
+    assert(fabs(to_float_hours(0, 15, 0) - 0.25) < DBL_EPSILON);
+    assert(fabs(to_float_hours(2, 45, 9) - 2.7525) < DBL_EPSILON);
+    assert(fabs(to_float_hours(1, 0, 36) - 1.01) < DBL_EPSILON);
 }
