a3c1e6ebd5eb7828afb527964f9f1a8abf32e453
LordPelmeha <sashasofolov@gmail.com>
2023-10-08 22:05:25 +0300

    added a function hours_difference

--- ConsoleApplication1/ConsoleApplication1/extra-task-1.h
+++ ConsoleApplication1/ConsoleApplication1/extra-task-1.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <iostream>
 #include <cassert>
-//¬озвращает разницу между time_2 и time_1
+//¬озвращает разницу между секундами time_2 и time_1
 double seconds_difference(double, double);
+//¬озвращает разницу между секундами time_2 и time_1 в часах
+double hours_difference(double, double);
--- ConsoleApplication1/ConsoleApplication1/main.cpp
+++ ConsoleApplication1/ConsoleApplication1/main.cpp
@@ -8,5 +8,11 @@ int main()
 		assert(fabs(seconds_difference(1800.0, 2160.0) - 360.0) < DBL_EPSILON);
 		assert(fabs(seconds_difference(1800.0, 1800.0) - 0.0) < DBL_EPSILON);
 	}
+	{
+		assert(fabs(hours_difference(1800.0, 3600.0) - 0.5) < DBL_EPSILON);
+		assert(fabs(hours_difference(3600.0, 1800.0) - (-0.5)) < DBL_EPSILON);
+		assert(fabs(hours_difference(1800.0, 2160.0) - 0.1) < DBL_EPSILON);
+		assert(fabs(hours_difference(1800.0, 1800.0) - 0.0) < DBL_EPSILON);
+	}
 
 }
