48b1429e6f680492a1068c51c265ec334733aa69
SukhanDmitry <115133850+SukhanDmitry@users.noreply.github.com>
2023-10-08 21:14:41 +0300

    implemented 3 funcs

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -24,6 +24,21 @@ double to_24_hour_clock(double hours)
     return hour_24;
 }
 
+int get_hours(int seconds)
+{
+    return seconds / 3600;
+}
+
+int get_minutes(int seconds)
+{
+    return (seconds % 3600) / 60;
+}
+
+int get_seconds(int seconds)
+{
+    return seconds % 60;
+}
+
 double time_to_utc(int utc_offset, double time)
 {
     /*
