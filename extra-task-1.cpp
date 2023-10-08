09dff00b2f5e8fed89408a03820f3c2abd27a399
Palom-Porom <114829512+Palom-Porom@users.noreply.github.com>
2023-10-08 22:19:01 +0300

    realization of seconds_difference

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -1,7 +1,10 @@
+#include <cassert>
+#include <cmath>
+
 double seconds_difference(double time_1, double time_2)
 {
     // your implementation goes here...
-    
+    return time_2 - time_1;
     /*    
         Return the number of seconds later that a time in seconds
         time_2 is than a time in seconds time_1.
@@ -22,6 +25,8 @@ double seconds_difference(double time_1, double time_2)
 
 double hours_difference(double time_1, double time_2)
 {
+    //return (time_2 - time_1) / 3600;
+    return 0;
     /*
         Return the number of hours later that a time in seconds
         time_2 is than a time in seconds time_1.
@@ -42,6 +47,10 @@ double hours_difference(double time_1, double time_2)
 
 double to_float_hours(int hours, int minutes, int seconds)
 {
+    //assert(minutes < 60 && minutes >= 0);
+    //assert(minutes < 60 && seconds >= 0);
+    //return hours + (minutes / 60) + (seconds / 3600);
+    return 0;
     /*
         Return the total number of hours in the specified number
         of hours, minutes, and seconds.
@@ -61,6 +70,9 @@ double to_float_hours(int hours, int minutes, int seconds)
 
 double to_24_hour_clock(double hours)
 {
+    //assert(hours >= 0);
+    //return fmod(hours, 24);
+    return 0;
     /*
         hours is a number of hours since midnight. Return the
         hour as seen on a 24-hour clock.
@@ -88,6 +100,20 @@ double to_24_hour_clock(double hours)
     */
 }
 
+//int get_hours(double time)
+//{
+//    return (time / 3600);
+//}
+//
+//int get_minutes(double time)
+//{
+//    return ((int)time % 3600) / 60;
+//}
+//
+//double get_seconds(double time)
+//{
+//    return fmod(time, 60);
+//}
 /*
     Implement three functions
         * get_hours
@@ -111,6 +137,8 @@ double to_24_hour_clock(double hours)
 
 double time_to_utc(int utc_offset, double time)
 {
+    //return fmod((time - utc_offset), 24);
+    return 0;
     /*
         Return time at UTC+0, where utc_offset is the number of hours away from
         UTC+0.
@@ -139,6 +167,8 @@ double time_to_utc(int utc_offset, double time)
 
 double time_from_utc(int utc_offset, double time)
 {
+    //return fmod((time + utc_offset), 24);
+    return 0;
     /*
         Return UTC time in time zone utc_offset.
 
